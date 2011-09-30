f190db1b9d4f46368346ae4dd76d45217f41a5b5
Neil Drumm <drumm@delocalizedham.com>
2011-09-30 14:22:56 -0700

    We have 4 null-named users.

--- snapshot/emails.sh
+++ snapshot/emails.sh
@@ -1,4 +1,4 @@
 pass=$1
 
-echo "SELECT concat('UPDATE users SET mail = \"', mail, '\" WHERE name = \"', name, '\";') AS '' FROM users" | mysql -u dump -h db2-main-vip.drupal.org -p${pass} drupal | gzip > ${JOB_NAME}.sql.gz
+echo "SELECT concat('UPDATE users SET mail = \"', mail, '\" WHERE name = \"', name, '\";') AS '' FROM users WHERE name IS NOT NULL;" | mysql -u dump -h db2-main-vip.drupal.org -p${pass} drupal | gzip > ${JOB_NAME}.sql.gz
 mv ${JOB_NAME}.sql.gz /var/dumps/mysql/
