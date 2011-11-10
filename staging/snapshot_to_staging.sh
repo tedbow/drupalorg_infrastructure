8a40278974d67fa1cdbf03df4c43993845d61f5e
Neil Drumm <drumm@delocalizedham.com>
2011-11-09 17:54:18 -0800

    #1317402 remove delaration of one-time -use variable

--- staging/snapshot_to_staging.sh
+++ staging/snapshot_to_staging.sh
@@ -1,13 +1,12 @@
 export PATH=$PATH:/usr/local/bin
 export TERM=dumb
-domain=$(echo $JOB_NAME | sed -e 's/^snapshot_to_//') # denver2012.scratch.drupal.org
-db_site=$(echo $domain | sed -e 's/\..*$//') #denver2012
+domain=$(echo ${JOB_NAME} | sed -e 's/^snapshot_to_//') # denver2012.scratch.drupal.org
 drush="drush -r /var/www/${domain}/htdocs -l ${domain} -y"
 db=$($drush sql-conf | sed -ne 's/^\s*\[database\] => //p')
 
 # Repopulate DB
 echo "DROP DATABASE ${db}; CREATE DATABASE ${db};" | $drush sql-cli
-ssh util zcat /var/dumps/mysql/${db_site}_database_snapshot.staging-current.sql.gz | $drush sql-cli
+ssh util zcat /var/dumps/mysql/$(echo $domain | sed -e 's/\..*$//')_database_snapshot.staging-current.sql.gz | $drush sql-cli
 
 # todo configure bakery to use staging.drupal.org as a master site
 $drush pm-disable bakery
