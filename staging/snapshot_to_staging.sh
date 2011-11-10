e5bd32ac0e66f9e3d94b3d11c8a1d25fc0b2489e
Neil Drumm <drumm@delocalizedham.com>
2011-11-09 17:39:47 -0800

    #1317402 Do less when deploying a staging site.

--- staging/snapshot_to_staging.sh
+++ staging/snapshot_to_staging.sh
@@ -1,24 +1,17 @@
 export PATH=$PATH:/usr/local/bin
 export TERM=dumb
-domain=$1 # denver2012.scratch.drupal.org
+domain=$(echo $JOB_NAME | sed -e 's/^snapshot_to_//') # denver2012.scratch.drupal.org
 db_site=$(echo $domain | sed -e 's/\..*$//') #denver2012
-redesign_ro_pass=$2
 drush="drush -r /var/www/${domain}/htdocs -l ${domain} -y"
 db=$($drush sql-conf | sed -ne 's/^\s*\[database\] => //p')
 
 # Repopulate DB
 echo "DROP DATABASE ${db}; CREATE DATABASE ${db};" | $drush sql-cli
-ssh util zcat /var/dumps/mysql/${db_site}_database_snapshot-current.sql.gz | $drush sql-cli
+ssh util zcat /var/dumps/mysql/${db_site}_database_snapshot.staging-current.sql.gz | $drush sql-cli
 
-# Get ready for development
-$drush pm-enable devel
-$drush variable-set smtp_library sites/all/modules/devel/devel.module
-$drush variable-set cache 0
-$drush variable-set preprocess_css 0
-$drush variable-set preprocess_js 0
-
-# Un-sanitize the user table for bakery
-ssh util zcat /var/dumps/mysql/drupal_snapshot_emails.sql.gz | $drush sql-cli
+# todo configure bakery to use staging.drupal.org as a master site
+$drush pm-disable bakery
 
 # Prime caches
+$drush cc all
 wget -O /dev/null http://drupal:drupal@${domain}
