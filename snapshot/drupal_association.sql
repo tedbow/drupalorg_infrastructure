c0265fd37d6e269e02e30a87da00ed332ad1928d
Neil Drumm <drumm@delocalizedham.com>
2011-12-08 19:07:50 -0800

    Module was uninstalled, removing table.

--- snapshot/drupal_association.sql
+++ snapshot/drupal_association.sql
@@ -57,7 +57,6 @@ DELETE book FROM book LEFT JOIN node ON node.nid = book.nid WHERE node.nid IS NU
 DELETE comments FROM comments LEFT JOIN node ON node.nid = comments.nid WHERE node.nid IS NULL;
 DELETE comments FROM comments LEFT JOIN users ON comments.uid = users.uid WHERE users.uid IS NULL;
 DELETE comments FROM comments LEFT JOIN comments c2 ON comments.pid = c2.cid WHERE c2.cid IS NULL AND comments.pid <> 0;
-DELETE comment_alter_taxonomy FROM comment_alter_taxonomy LEFT JOIN comments ON comments.cid = comment_alter_taxonomy.cid WHERE comments.cid IS NULL;
 DELETE files FROM files LEFT JOIN users ON files.uid = users.uid WHERE users.uid IS NULL;
 DELETE files FROM files INNER JOIN upload ON files.fid = upload.fid LEFT JOIN node ON upload.nid = node.nid WHERE upload.fid IS NULL;
 DELETE upload FROM upload LEFT JOIN node ON upload.nid = node.nid WHERE node.nid IS NULL;
