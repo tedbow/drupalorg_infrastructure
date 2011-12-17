7c2d638569c0be522b398dc458071bf1662d875c
Neil Drumm <drumm@delocalizedham.com>
2011-12-16 17:34:05 -0800

    #1373570 Version control dev site templates

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -5,6 +5,11 @@
 # Exit immediately on uninitialized variable or error, and print each command.
 set -uex
 
+# Usage: write-template "template" "path/to/destination"
+function write-template {
+  sed -e "s/DB_NAME/${db_name}/g;s/NAME/${name}/g;s/SITE/${site}/g;s/DB_PASS/${db_pass}/g" "dev/${1}" > "${2}"
+}
+
 # Handle drupal.org vs. sub-domains properly
 if [ ${site} == "drupal" ]; then
   fqdn="drupal.org"
@@ -14,29 +19,21 @@ else
   snapshot="/var/dumps/mysql/${site}_database_snapshot-current.sql.gz"
 fi
 vhost_path="/etc/apache2/vhosts.d/automated-hudson"
-template="template-generic"
 web_path="/var/www/dev/${name}-${site}.redesign.devdrupal.org"
 export TERM=dumb
 drush="/usr/local/bin/drush -r ${web_path}/htdocs -l ${fqdn}"
 db_name=$(echo ${name}_${site} | sed -e "s/-/_/g" -e "s/\./_/g" | sed -e 's/^\(.\{16\}\).*/\1/') # Truncate to 16 chars
 db_pass=$(pwgen -s 16 1)
-settings_template="/var/www/dev/settings.local.template-generic"
-user_passwd=$(pwgen -s 16 1)
 
 [ -e "${web_path}" ] && echo "Project webroot already exists!" && exit 1
 
 # Create the webroot and add comment file
-echo "Creating webroot and comment file"
 mkdir -p ${web_path}/htdocs
 chown -R bender:developers ${web_path}
 echo "${COMMENT}" > ${web_path}/comment
-echo "" >> ${web_path}/comment
-echo "db user/pass: ${db_name}/${db_pass}" >> ${web_path}/comment
-echo "db name: ${db_name}" >> ${web_path}/comment
 
 # Create the vhost config
-echo "Creating vhost configuration: ${vhost_path}/${name}-${site}.conf"
-sed -e "s/NAME/${name}/g" -e "s/SITE/${site}/g" ${vhost_path}/${template} > ${vhost_path}/${name}-${site}.conf
+write-template "vhost.conf.template" "${vhost_path}/${name}-${site}.conf"
 
 # Configure the database
 echo "Configuring the database"
@@ -48,9 +45,7 @@ echo "Populating development environment with bzr checkout"
 bzr checkout bzr+ssh://util.drupal.org/bzr/${fqdn} ${web_path}/htdocs
 
 # Add settings.local.php
-sed -e "s/DB_NAME/${db_name}/g" -e "s/NAME/${name}/g" -e "s/SITE/${site}/g" -e "s/PASS/${db_pass}/g" ${settings_template} > ${web_path}/htdocs/sites/default/settings.local.php
-echo "<?php
-\$options['uri'] = 'http://${name}-${site}.redesign.devdrupal.org';" > ${web_path}/htdocs/drushrc.php
+write-template "settings.local.php.template" "${web_path}/htdocs/sites/default/settings.local.php"
 
 # Strongarm the permissions
 echo "Forcing proper permissions on ${web_path}"
@@ -58,12 +53,8 @@ find ${web_path} -type d -exec chmod g+rwx {} +
 find ${web_path} -type f -exec chmod g+rw {} +
 chgrp -R developers ${web_path}
 
-# Import mysql database
-echo "Importing latest database snapshot"
+# Import database
 ssh util zcat ${snapshot} | mysql ${db_name}
-echo "Setting all drupal user passwords to ${user_passwd}"
-mysql -e "UPDATE users SET pass = MD5('${user_passwd}')" ${db_name} 
-echo "User password: ${user_passwd}" >> ${web_path}/comment
 
 # Disable modules that don't work well in development (yet)
 ${drush} pm-disable paranoia -y
