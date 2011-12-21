1dba701ebf78dbefc0c8da2e1ddbac8df582e4ea
Neil Drumm <drumm@delocalizedham.com>
2011-12-21 15:05:34 -0800

    #1371326 Handle adding new modules.

--- vendors/update-module.sh
+++ vendors/update-module.sh
@@ -1,10 +1,20 @@
 # Exit immediately on uninitialized variable or error, and print each command.
 set -uex
 
-branch=${site}.drupal.org
-[ ${site} = 'drupal.org' ] && branch=drupal.org
+branch="${site}.drupal.org"
+[ ${site} = "drupal.org" ] && branch="drupal.org"
+vendor="/srv/bzr/vendor/${module}/${vendor_version}"
+location="sites/all/modules/${module}"
+
 rm -rf ${branch}
 bzr co /srv/bzr/${branch}/
 cd ${branch}
-bzr merge /srv/bzr/vendor/${module}/${vendor_version}
-bzr commit -m "Automatic merge from ${module} ${vendor_version}/"
+
+if [ -d ${location} ]; then
+  bzr merge ${vendor}
+  bzr commit -m "Automatic merge from ${module} ${vendor_version}."
+else
+  bzr checkout ${vendor} ${location}
+  bzr join ${location}
+  bzr commit -m "Automatic join from ${module} ${vendor_version}."
+fi
