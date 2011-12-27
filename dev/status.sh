fc88f8c835ef03609b931af878ee6542a8ac4597
Neil Drumm <drumm@delocalizedham.com>
2011-12-27 15:55:52 -0800

    #1386772 add last generated date and DB space.

--- dev/status.sh
+++ dev/status.sh
@@ -7,15 +7,23 @@ set -uex
 export TERM=dumb
 drush="/usr/local/bin/drush"
 
-{ for domain in $(ls "/var/www/dev"); do
-  echo ${domain}
-  cd "/var/www/dev/${domain}/htdocs"
-  site=$(echo "${domain}" | sed -e 's/\.redesign\.devdrupal\.org$//;s/^.*-//')
-  if [ "${site}" = 'drupal' ]; then
-    echo "SELECT from_unixtime(max(access)) AS 'Last access' FROM users_access;" | ${drush} sql-cli | xargs echo
-  else
-    echo "SELECT from_unixtime(max(access)) AS 'Last access' FROM users;" | ${drush} sql-cli | xargs echo
-  fi
-  bzr status
-  echo
-done } > "/var/www/dev-status.txt"
+{
+  echo -n "Generated "
+  date
+
+  echo "Database"
+  ssh stagingdb.drupal.org df -h /
+
+  for domain in $(ls "/var/www/dev"); do
+    echo ${domain}
+    cd "/var/www/dev/${domain}/htdocs"
+    site=$(echo "${domain}" | sed -e 's/\.redesign\.devdrupal\.org$//;s/^.*-//')
+    if [ "${site}" = 'drupal' ]; then
+      echo "SELECT from_unixtime(max(access)) AS 'Last access' FROM users_access;" | ${drush} sql-cli | xargs echo
+    else
+      echo "SELECT from_unixtime(max(access)) AS 'Last access' FROM users;" | ${drush} sql-cli | xargs echo
+    fi
+    bzr status
+    echo
+  done
+} > "/var/www/dev-status.txt"
