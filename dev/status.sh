c9237690e3404532f0657be340f22fe1be7dc824
Neil Drumm <drumm@delocalizedham.com>
2011-12-27 01:55:32 -0800

    #1386772 Make a Jenkins job for dev site status.

---
+++ dev/status.sh
@@ -0,0 +1,17 @@
+# Collect general information about dev sites.
+
+# Exit immediately on uninitialized variable or error, and print each command.
+set -uex
+
+{ for domain in $(ls "/var/www/dev"); do
+  echo ${domain}
+  cd "/var/www/dev/${domain}/htdocs"
+  site=$(echo "${domain}" | sed -e 's/\.redesign\.devdrupal\.org$//;s/^.*-//')
+  if [ "${site}" = 'drupal' ]; then
+    echo "SELECT from_unixtime(max(access)) AS 'Last access' FROM users_access;" | drush sql-cli | xargs echo
+  else
+    echo "SELECT from_unixtime(max(access)) AS 'Last access' FROM users;" | drush sql-cli | xargs echo
+  fi
+  bzr status
+  echo
+done } > "/var/www/dev-status.txt"
