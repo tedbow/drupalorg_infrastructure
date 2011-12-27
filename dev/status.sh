718141801992a23a9c1b540f030818d3285a04af
Neil Drumm <drumm@delocalizedham.com>
2011-12-27 02:00:59 -0800

    #1386772 Use drush which exists.

--- dev/status.sh
+++ dev/status.sh
@@ -3,14 +3,18 @@
 # Exit immediately on uninitialized variable or error, and print each command.
 set -uex
 
+# For easily executing drush.
+export TERM=dumb
+drush="/usr/local/bin/drush"
+
 { for domain in $(ls "/var/www/dev"); do
   echo ${domain}
   cd "/var/www/dev/${domain}/htdocs"
   site=$(echo "${domain}" | sed -e 's/\.redesign\.devdrupal\.org$//;s/^.*-//')
   if [ "${site}" = 'drupal' ]; then
-    echo "SELECT from_unixtime(max(access)) AS 'Last access' FROM users_access;" | drush sql-cli | xargs echo
+    echo "SELECT from_unixtime(max(access)) AS 'Last access' FROM users_access;" | ${drush} sql-cli | xargs echo
   else
-    echo "SELECT from_unixtime(max(access)) AS 'Last access' FROM users;" | drush sql-cli | xargs echo
+    echo "SELECT from_unixtime(max(access)) AS 'Last access' FROM users;" | ${drush} sql-cli | xargs echo
   fi
   bzr status
   echo
