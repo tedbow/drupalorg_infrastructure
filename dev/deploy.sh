81475879d691b13740dad0db4aa70cdfc86346b3
Neil Drumm <drumm@delocalizedham.com>
2012-01-11 18:04:21 -0800

    Cleanup: move -y to variable and remove any trailing whilespace from files directory.

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -17,7 +17,7 @@ else
   snapshot="/var/dumps/mysql/${site}_database_snapshot-current.sql.gz"
 fi
 export TERM=dumb
-drush="/usr/local/bin/drush -r ${web_path}/htdocs -l ${fqdn}"
+drush="/usr/local/bin/drush -r ${web_path}/htdocs -y"
 db_pass=$(pwgen -s 16 1)
 
 [ -e "${web_path}" ] && echo "Project webroot already exists!" && exit 1
@@ -51,21 +51,21 @@ chgrp -R developers "${web_path}"
 ssh util zcat "${snapshot}" | mysql "${db_name}"
 
 # Disable modules that don't work well in development (yet)
-${drush} pm-disable paranoia -y
-${drush} pm-disable civicrm -y
+${drush} pm-disable paranoia
+${drush} pm-disable civicrm
 
 # Link up the files directory
-ln -s /media/${fqdn} "${web_path}/htdocs/$(${drush} status | sed -ne 's/^ *File directory path *: *//p')"
+ln -s /media/${fqdn} "${web_path}/htdocs/$(${drush} status | sed -ne 's/^ *File directory path *: *\([^ ]*\).*$/\1/p')"
 
 # Reload apache with new vhost
 restart_apache
 
 # Get ready for development
 echo 1 | ${drush} vdel cache
-${drush} vdel preprocess_css -y
-${drush} vdel preprocess_js -y
-${drush} pm-enable -y views_ui
-${drush} pm-enable -y imagecache_ui
+${drush} vdel preprocess_css
+${drush} vdel preprocess_js
+${drush} pm-enable views_ui
+${drush} pm-enable imagecache_ui
 
 ${drush} updatedb
 
@@ -74,22 +74,22 @@ ${drush} en test_gateway
 ${drush} vset uc_payment_credit_gateway test_gateway
 
 # Set up for potential bakery testing
-${drush} vdel bakery_slaves -y
-${drush} vset bakery_domain ".redesign.devdrupal.org" -y
+${drush} vdel bakery_slaves
+${drush} vset bakery_domain ".redesign.devdrupal.org"
 if [ "${site}" == "drupal" ]; then
   # Drupal.org sites are masters
-  ${drush} vset bakery_master "${name}-${site}.redesign.devdrupal.org" -y
-  ${drush} vset bakery_key "$(pwgen -s 32 1)" -y
+  ${drush} vset bakery_master "${name}-${site}.redesign.devdrupal.org"
+  ${drush} vset bakery_key "$(pwgen -s 32 1)"
 else
   if [ "${bakery_master-}" ]; then
     # Hook up to a Drupal.org
-    ${drush} vset bakery_master "http://${bakery_master}-drupal.redesign.devdrupal.org/" -y
-    drush_master="/usr/local/bin/drush -r /var/www/dev/${bakery_master}-drupal.redesign.devdrupal.org/htdocs -l ${bakery_master}-drupal.redesign.devdrupal.org"
-    ${drush} vset bakery_key $(${drush_master} vget bakery_key | sed -ne 's/^.*"\(.*\)"/\1/p') -y
-    ${drush_master} bakery-add-slave "http://${name}-${site}.redesign.devdrupal.org/" -y
+    ${drush} vset bakery_master "http://${bakery_master}-drupal.redesign.devdrupal.org/"
+    drush_master="/usr/local/bin/drush -r /var/www/dev/${bakery_master}-drupal.redesign.devdrupal.org/htdocs -l ${bakery_master}-drupal.redesign.devdrupal.org -y"
+    ${drush} vset bakery_key $(${drush_master} vget bakery_key | sed -ne 's/^.*"\(.*\)"/\1/p')
+    ${drush_master} bakery-add-slave "http://${name}-${site}.redesign.devdrupal.org/"
   else
     # Don't bother with bakery
-    ${drush} pm-disable bakery -y
+    ${drush} pm-disable bakery
   fi
 fi
 
