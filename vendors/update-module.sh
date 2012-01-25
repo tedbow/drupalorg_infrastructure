4b9141d4e33cc6500cd7caabddb29a83b78c52ef
Neil Drumm <drumm@delocalizedham.com>
2012-01-24 19:37:13 -0800

    Automatic joining and merging of themes.

--- vendors/update-module.sh
+++ vendors/update-module.sh
@@ -3,18 +3,23 @@ set -uex
 
 branch="${site}.drupal.org"
 [ ${site} = "drupal.org" ] && branch="drupal.org"
-vendor="/srv/bzr/vendor/${module}/${vendor_version}"
-location="sites/all/modules/${module}"
+vendor="/srv/bzr/vendor/${project}/${vendor_version}"
 
 rm -rf ${branch}
 bzr co /srv/bzr/${branch}/
 cd ${branch}
 
-if [ -d ${location} ]; then
+if [ -d sites/all/*/${project} ]; then
   bzr merge ${vendor}
-  bzr commit -m "Automatic merge from ${module} ${vendor_version}. ${message-}"
+  bzr commit -m "Automatic merge from ${project} ${vendor_version}. ${message-}"
 else
-  bzr checkout ${vendor} ${location}
+  bzr checkout ${vendor} tmp
+  if grep -q "^engine" tmp/*.info; then
+    location="sites/all/modules/${project}"
+  else
+    location="sites/all/themes/${project}"
+  fi
+  mv tmp ${location}
   bzr join ${location}
-  bzr commit -m "Automatic join from ${module} ${vendor_version}. ${message-}"
+  bzr commit -m "Automatic join from ${project} ${vendor_version}. ${message-}"
 fi
