dc3a287860d42ccc0c35418c66faf967b63dc3a9
Neil Drumm <drumm@delocalizedham.com>
2012-01-28 13:14:41 -0800

    Code style

--- staging/snapshot_to_staging.sh
+++ staging/snapshot_to_staging.sh
@@ -2,18 +2,18 @@
 . staging/common.sh 'snapshot_to'
 
 # Repopulate DB
-db=$($drush sql-conf | sed -ne 's/^\s*\[database\] => //p')
-snapshot=$(echo $domain | sed -e 's/\..*$//')
+db=$(${drush} sql-conf | sed -ne 's/^\s*\[database\] => //p')
+snapshot=$(echo ${domain} | sed -e 's/\..*$//')
 if [ "${domain}" = "staging.devdrupal.org" ]; then
   snapshot="drupal"
 fi
-echo "DROP DATABASE ${db}; CREATE DATABASE ${db};" | $drush sql-cli
-ssh util zcat "/var/dumps/mysql/${snapshot}_database_snapshot.staging-current.sql.gz" | $drush sql-cli
+echo "DROP DATABASE ${db}; CREATE DATABASE ${db};" | ${drush} sql-cli
+ssh util zcat "/var/dumps/mysql/${snapshot}_database_snapshot.staging-current.sql.gz" | ${drush} sql-cli
 
 # todo configure bakery to use staging.drupal.org as a master site
-$drush pm-disable bakery
+${drush} pm-disable bakery
 
 # Try updatedb, clear and prime caches
-$drush updatedb
-$drush cc all
+${drush} updatedb
+${drush} cc all
 wget -O /dev/null http://${domain} --user=drupal --password=drupal
