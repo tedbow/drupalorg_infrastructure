6d3df754e95421c1da6884eab0e81143771b0044
Neil Drumm <drumm@delocalizedham.com>
2012-02-15 17:23:50 -0800

    Staging deploys are now BZR-triggered, so we can assume we always want a full update.

--- staging/deploy.sh
+++ staging/deploy.sh
@@ -1,15 +1,11 @@
 # Include common staging script.
 . staging/common.sh 'deploy'
 
-# Update code and keep track of versions.
+# Update code.
 cd /var/www/${domain}/htdocs
-before=$(bzr version-info | grep "^revno: ")
 bzr up
-after=$(bzr version-info | grep "^revno: ")
 
-# If an update happened, try updatedb, clear and prime caches.
-if [ "${before}" != "${after}" ]; then
-  ${drush} updatedb
-  ${drush} cc all
-  wget -O /dev/null http://${domain} --user=drupal --password=drupal
-fi
+# updatedb, clear and prime caches.
+${drush} updatedb
+${drush} cc all
+wget -O /dev/null http://${domain} --user=drupal --password=drupal
