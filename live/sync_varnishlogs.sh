355278b6160b0cb24891db7a7c03a882f31c602c
Neil Drumm <drumm@delocalizedham.com>
2012-03-16 20:23:32 -0700

    Code style

--- live/sync_varnishlogs.sh
+++ live/sync_varnishlogs.sh
@@ -1,9 +1,12 @@
 # Sync the varnishlogs from all webnodes
-varnish_logdir=/var/log/DROP
-domain_name=drupal.bak
+
+# Exit immediately on uninitialized variable or error, and print each command.
+set -uex
+
+varnish_logdir="/var/log/DROP"
+domain_name="drupal.bak"
 rsync_args="--delete --delete-excluded --exclude=transfer.log --include=*"
 
-for host in www{1..3} www{5..7};
-do
-    nice -n 19 rsync -rt $rsync_args $host.$domain_name::varnishlogs/ $varnish_logdir/$host/
+for host in www{1..3} www{5..7}; do
+  nice -n 19 rsync -rt ${rsync_args} ${host}.${domain_name}::varnishlogs/ ${varnish_logdir}/${host}/
 done
