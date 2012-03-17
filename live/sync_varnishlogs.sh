81d258c82a77d208b8a2a039f4206e3af6754e8d
Neil Drumm <drumm@delocalizedham.com>
2012-03-16 20:26:34 -0700

    [#32124] Use future-proof webnode list.

--- live/sync_varnishlogs.sh
+++ live/sync_varnishlogs.sh
@@ -1,12 +1,15 @@
-# Sync the varnishlogs from all webnodes
+# Sync the varnishlogs from all webnodes.
 
 # Exit immediately on uninitialized variable or error, and print each command.
 set -uex
+
+# Get list of webnodes
+. live/webnodes.sh
 
 varnish_logdir="/var/log/DROP"
 domain_name="drupal.bak"
 rsync_args="--delete --delete-excluded --exclude=transfer.log --include=*"
 
-for host in www{1..3} www{5..7}; do
-  nice -n 19 rsync -rt ${rsync_args} ${host}.${domain_name}::varnishlogs/ ${varnish_logdir}/${host}/
+for i in ${webnodes[@]}; do
+  nice -n 19 rsync -rt ${rsync_args} "www${i}.${domain_name}::varnishlogs/" "${varnish_logdir}/www${i}/"
 done
