5f2ada16f58c0c3c5c1147c235f5785d17b782da
Neil Drumm <drumm@delocalizedham.com>
2012-04-05 15:57:35 -0700

    Further reduce DB dumps

--- snapshot/drupal.reduce.sql
+++ snapshot/drupal.reduce.sql
@@ -2,13 +2,17 @@
 
 -- Reduce the DB size to make development easier.
 -- http://drupal.org/node/636340#comment-3193836
-DELETE FROM node WHERE type IN ('forum','project_issue') AND created < (unix_timestamp() - 80*24*60*60);
+DELETE FROM node WHERE type IN ('forum','project_issue') AND created < (unix_timestamp() - 60*24*60*60);
 DELETE node, project_issues FROM node INNER JOIN project_issues WHERE node.nid = project_issues.nid AND project_issues.sid IN (7,14,2);
 DELETE node FROM node LEFT JOIN comments ON node.nid = comments.nid WHERE node.type = 'forum' AND comments.nid IS NULL;
 DELETE node_revisions FROM node_revisions LEFT JOIN node ON node.nid = node_revisions.nid WHERE node.nid IS NULL;
-DELETE node_revisions FROM node_revisions LEFT JOIN node ON node.nid = node_revisions.nid AND node.vid = node_revisions.vid WHERE node.nid IS NULL AND node_revisions.timestamp < (unix_timestamp() - 80*24*60*60);
+DELETE node_revisions FROM node_revisions LEFT JOIN node ON node.nid = node_revisions.nid AND node.vid = node_revisions.vid WHERE node.nid IS NULL AND node_revisions.timestamp < (unix_timestamp() - 60*24*60*60);
 DELETE node_comment_statistics FROM node_comment_statistics LEFT JOIN node ON node.nid = node_comment_statistics.nid WHERE node.nid IS NULL;
 TRUNCATE node_counter;
 DELETE comments FROM comments LEFT JOIN node ON node.nid = comments.nid WHERE node.nid IS NULL;
 DELETE tracker2_user FROM tracker2_user LEFT JOIN node ON tracker2_user.nid = node.nid WHERE node.nid IS NULL;
 DELETE forum2_index FROM forum2_index LEFT JOIN node ON forum2_index.nid = node.nid WHERE node.nid IS NULL;
+
+DELETE FROM versioncontrol_operations WHERE date < (unix_timestamp() - 60*24*60*60);
+DELETE versioncontrol_item_revisions FROM versioncontrol_item_revisions LEFT JOIN versioncontrol_operations ON versioncontrol_item_revisions.vc_op_id = versioncontrol_operations.vc_op_id WHERE versioncontrol_operations.vc_op_id IS NULL;
+DELETE versioncontrol_git_item_revisions FROM versioncontrol_git_item_revisions LEFT JOIN versioncontrol_item_revisions ON versioncontrol_git_item_revisions.item_revision_id = versioncontrol_item_revisions.item_revision_id WHERE versioncontrol_item_revisions.item_revision_id IS NULL;
