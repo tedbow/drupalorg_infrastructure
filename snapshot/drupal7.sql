2ed5dbb8232d25e8eaffe621089607c94ae550a4
Neil Drumm <drumm@delocalizedham.com>
2012-04-23 12:33:23 -0700

    [#1481026] D7b table renames

--- snapshot/drupal7.sql
+++ snapshot/drupal7.sql
@@ -4,23 +4,19 @@ UPDATE users SET access = 280299600;
 TRUNCATE mailhandler;
 
 -- Remove sensitive variables and profile data
-DELETE FROM profile_values WHERE fid IN (select fid from profile_fields where visibility in (1, 4));
+DELETE FROM profile_value WHERE fid IN (select fid FROM profile_fields WHERE visibility in (1, 4));
 
 -- Get rid of unpublished/blocked nodes, users, comments and related data in other tables.
 DELETE FROM node WHERE status <> 1;
-DELETE FROM comments WHERE status <> 0;
+DELETE FROM comment WHERE status <> 0;
 DELETE node FROM node LEFT JOIN users ON node.uid = users.uid WHERE users.uid IS NULL;
-DELETE node_revisions FROM node_revisions LEFT JOIN node ON node.nid = node_revisions.nid WHERE node.nid IS NULL;
-DELETE comments FROM comments LEFT JOIN node ON node.nid = comments.nid WHERE node.nid IS NULL;
-DELETE comments FROM comments LEFT JOIN users ON comments.uid = users.uid WHERE users.uid IS NULL;
-DELETE comments FROM comments LEFT JOIN comments c2 ON comments.pid = c2.cid WHERE c2.cid IS NULL AND comments.pid <> 0;
+DELETE node_revision FROM node_revision LEFT JOIN node ON node.nid = node_revision.nid WHERE node.nid IS NULL;
+DELETE comment FROM comment LEFT JOIN node ON node.nid = comment.nid WHERE node.nid IS NULL;
+DELETE comment FROM comment LEFT JOIN users ON comment.uid = users.uid WHERE users.uid IS NULL;
+DELETE comment FROM comment LEFT JOIN comment c2 ON comment.pid = c2.cid WHERE c2.cid IS NULL AND comment.pid <> 0;
 DELETE project_issue_comments FROM project_issue_comments LEFT JOIN node ON node.nid = project_issue_comments.nid WHERE node.nid IS NULL;
-DELETE project_issue_comments FROM project_issue_comments LEFT JOIN comments ON comments.cid = project_issue_comments.cid WHERE comments.cid IS NULL;
+DELETE project_issue_comments FROM project_issue_comments LEFT JOIN comment ON comment.cid = project_issue_comments.cid WHERE comment.cid IS NULL;
 DELETE files FROM files LEFT JOIN users ON files.uid = users.uid WHERE users.uid IS NULL;
-DELETE files FROM files INNER JOIN upload ON files.fid = upload.fid LEFT JOIN node ON upload.nid = node.nid WHERE upload.fid IS NULL;
-DELETE upload FROM upload LEFT JOIN node ON upload.nid = node.nid WHERE node.nid IS NULL;
-DELETE files FROM files INNER JOIN comment_upload ON files.fid = comment_upload.fid LEFT JOIN comments ON comments.cid = comment_upload.cid WHERE comments.cid IS NULL;
-DELETE comment_upload FROM comment_upload LEFT JOIN comments ON comments.cid = comment_upload.cid WHERE comments.cid IS NULL;
 DELETE image FROM image LEFT JOIN node ON image.nid = node.nid WHERE node.nid IS NULL;
 DELETE image_attach FROM image_attach LEFT JOIN node ON image_attach.nid = node.nid WHERE node.nid IS NULL;
 
