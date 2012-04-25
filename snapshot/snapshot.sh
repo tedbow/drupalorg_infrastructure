fd6ebc52dbf4c2db331f4d5a4464c946a0bcfd1c
Neil Drumm <drumm@delocalizedham.com>
2012-04-25 15:18:39 -0700

    Comments.

--- snapshot/snapshot.sh
+++ snapshot/snapshot.sh
@@ -3,17 +3,25 @@
 # Exit immediately on uninitialized variable or error, and print each command.
 set -uex
 
+# Run a snapshot phase. See below for how it is called. ${suffix} is the phase
+# and used in filenames.
 function snapshot {
+  # Execute common SQL commands.
   [ -f "snapshot/common${suffix}.sql" ] && mysql -o ${tmp_args} < "snapshot/common${suffix}.sql"
+  # Execute common SQL commands, but don't exit if they fail.
   [ -f "snapshot/common-force${suffix}.sql" ] && mysql -f -o ${tmp_args} < "snapshot/common-force${suffix}.sql"
 
+  # Skip if this sanitization and phase does not exit.
   [ ! -f "snapshot/${sanitization}${suffix}.sql" ] && return
+  # Execute SQL for this sanitization and phase.
   mysql -o ${tmp_args} < "snapshot/${sanitization}${suffix}.sql"
 
+  # Save the DB dump.
   mysqldump --single-transaction ${tmp_args} | bzip2 > "/var/dumps/mysql/${JOB_NAME}${suffix}-${BUILD_NUMBER}-in-progress.sql.bz2"
   mv -v "/var/dumps/mysql/${JOB_NAME}${suffix}-${BUILD_NUMBER}-in-progress.sql.bz2" "/var/dumps/mysql/${JOB_NAME}${suffix}-${BUILD_NUMBER}.sql.bz2"
   ln -sfv "/var/dumps/mysql/${JOB_NAME}${suffix}-${BUILD_NUMBER}.sql.bz2" "/var/dumps/mysql/${JOB_NAME}${suffix}-current.sql.bz2"
 
+  # Remove old snapshots.
   old_snapshots=$(ls -t /var/dumps/mysql/${JOB_NAME}${suffix}-[0-9]*.sql.{bz2,gz} | tail -n +2)
   if [ -n "${old_snapshots}" ]; then
     rm -v ${old_snapshots}
@@ -28,7 +36,10 @@ function clear_tmp {
 db_name=$1
 db_user=$2
 db_pass=$3
+
+# If the sanitization is not set, use the DB name.
 [ "${sanitization-}" ] || sanitization=${db_name}
+# If the DB host is not set, use db3-vip.
 [ "${db_host-}" ] || db_host=db3-vip.drupal.org
 
 tmp_db=drupal_sanitize
@@ -41,17 +52,28 @@ ln -sf /var/dumps $WORKSPACE/dumps
 
 clear_tmp
 
+# Copy live to tmp.
 mysqldump -h$db_host -u$db_user -p$db_pass --single-transaction $db_name 2> mysqldump-errors.txt | mysql -o ${tmp_args}
 [ -s mysqldump-errors.txt ] && cat mysql-errors.txt && exit 1
 
+# Truncate all tables with cache in the name.
 echo "SHOW TABLES LIKE '%cache%';" | mysql -o ${tmp_args} | tail -n +2 | sed -e "s/^\(.*\)$/TRUNCATE \1;/" | mysql -o ${tmp_args}
 
+# Snapshot in stages.
+
+# Raw is nearly unsanitized, excpet for some keys. Git-dev uses this for emails.
 suffix=.raw
 snapshot
+
+# A snapshot suitable for staging. We remove emails to avoid emailing people.
 suffix=.staging
 snapshot
+
+# A snapshot suitable for dev. We remove all private information.
 suffix=.dev
 snapshot
+
+# A smaller snapshot for taking up less space on dev.
 suffix=.reduce
 snapshot
 
