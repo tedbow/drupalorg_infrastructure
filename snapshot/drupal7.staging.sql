5b32e2b68b579bfad374aab0eda31132119d60bd
Neil Drumm <drumm@delocalizedham.com>
2012-04-26 21:03:07 -0700

    We don't need to redo staging anitization because d7 is taging

--- snapshot/drupal7.staging.sql
+++
@@ -1,5 +0,0 @@
-UPDATE comment SET mail = CONCAT(name, '@localhost');
-UPDATE project_issue_projects SET mail_digest = 'foo@localhost', mail_copy = 'foo@localhost';
-UPDATE projects SET mail = CONCAT("empty", '@localhost');
-UPDATE simplenews_subscriptions SET mail = CONCAT(snid, '@localhost');
-UPDATE multiple_email me INNER JOIN users u ON u.uid = me.uid SET me.email = concat(me.eid, '.', u.mail);
