c1fff4678b9208f78ac99c6a9dc4dcff7d3f74c6
Neil Drumm <drumm@delocalizedham.com>
2012-05-29 12:27:55 -0700

    D7 has comment swapped

--- snapshot/cod-7.dev.sql
+++ snapshot/cod-7.dev.sql
@@ -7,7 +7,7 @@ TRUNCATE blocked_ips;
 
 -- Get rid of unpublished/blocked nodes, users, comments and related data in other tables.
 DELETE FROM node WHERE status <> 1;
-DELETE FROM comment WHERE status <> 0;
+DELETE FROM comment WHERE status <> 1;
 DELETE node FROM node LEFT JOIN users ON node.uid = users.uid WHERE users.uid IS NULL;
 DELETE node_revision FROM node_revision LEFT JOIN node ON node.nid = node_revision.nid WHERE node.nid IS NULL;
 DELETE comment FROM comment LEFT JOIN node ON node.nid = comment.nid WHERE node.nid IS NULL;
