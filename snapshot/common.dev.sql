9d208c62ba21f8fe1faf9117f735b1e9a9e2882b
Neil Drumm <drumm@delocalizedham.com>
2012-07-14 15:43:05 -0700

    Don't delete bacon.

--- snapshot/common.dev.sql
+++ snapshot/common.dev.sql
@@ -2,6 +2,6 @@ TRUNCATE history;
 TRUNCATE watchdog;
 TRUNCATE authmap;
 
-DELETE FROM users WHERE status <> 1 AND uid <> 0;
+DELETE FROM users WHERE status <> 1 AND uid <> 0 AND name <> 'bacon';
 UPDATE users SET data = '', pass = 'nope';
 DELETE users_roles FROM users_roles LEFT JOIN users ON users_roles.uid = users.uid WHERE users.uid IS NULL;
