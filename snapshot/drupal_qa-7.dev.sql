60c71106bab6481a20984287aadb81ddf8e0e2e5
Neil Drumm <drumm@delocalizedham.com>
2012-07-17 17:29:38 -0700

    QA 7 dev snapshot.

---
+++ snapshot/drupal_qa-7.dev.sql
@@ -0,0 +1,22 @@
+UPDATE users SET access = 280299600;
+
+-- Get rid of irrelevant data.
+TRUNCATE blocked_ips;
+
+-- Get rid of unpublished/blocked nodes, users, comments and related data in other tables.
+DELETE f FROM field_data_body AS f INNER JOIN node n ON (f.entity_id = n.nid AND f.entity_type = 'node' AND n.status <> 1);
+DELETE f FROM field_revision_body AS f INNER JOIN node n ON (f.entity_id = n.nid AND f.entity_type = 'node' AND n.status <> 1);
+DELETE f FROM field_data_comment_body AS f INNER JOIN node n ON (f.entity_id = n.nid AND f.entity_type = 'node' AND n.status <> 1);
+DELETE f FROM field_revision_comment_body AS f INNER JOIN node n ON (f.entity_id = n.nid AND f.entity_type = 'node' AND n.status <> 1);
+
+DELETE FROM node WHERE status <> 1;
+DELETE FROM comment WHERE status <> 1;
+DELETE node FROM node LEFT JOIN users ON node.uid = users.uid WHERE users.uid IS NULL;
+DELETE node_revision FROM node_revision LEFT JOIN node ON node.nid = node_revision.nid WHERE node.nid IS NULL;
+DELETE comment FROM comment LEFT JOIN node ON node.nid = comment.nid WHERE node.nid IS NULL;
+DELETE comment FROM comment LEFT JOIN users ON comment.uid = users.uid WHERE users.uid IS NULL;
+DELETE comment FROM comment LEFT JOIN comment c2 ON comment.pid = c2.cid WHERE c2.cid IS NULL AND comment.pid <> 0;
+DELETE files FROM files LEFT JOIN users ON files.uid = users.uid WHERE users.uid IS NULL;
+
+-- Remove assorted IP / email data
+UPDATE comment SET hostname = "127.0.0.1";
