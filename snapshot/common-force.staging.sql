fab770b3a06501aeee17e176a20882e455c93e2d
Neil Drumm <drumm@delocalizedham.com>
2012-08-08 22:16:31 -0700

    Truncate search index earlier to save staging space.

---
+++ snapshot/common-force.staging.sql
@@ -0,0 +1,5 @@
+-- Executed with mysql -f, every query will be executed, regardless of failure.
+TRUNCATE search_dataset;
+TRUNCATE search_index;
+TRUNCATE search_node_links;
+TRUNCATE search_total;
