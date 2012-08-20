1bdfa45fda3ae06bac1e544d6ca512e77a01e061
Neil Drumm <drumm@delocalizedham.com>
2012-08-20 10:38:27 +0200

    [#1736940] Monitor staging sites for uptime

--- staging/verify.sh
+++ staging/verify.sh
@@ -6,5 +6,8 @@ bzr log -r-1 -n0 > ${WORKSPACE}/version.txt
 bzr status > ${WORKSPACE}/status.txt
 bzr diff > ${WORKSPACE}/diff.txt
 
+# Make sure the site is up
+wget -O /dev/null "http://${uri}" --user=drupal --password=drupal
+
 # Exit with error if there are changes.
 exit $(cat ${WORKSPACE}/status.txt | wc -l)
