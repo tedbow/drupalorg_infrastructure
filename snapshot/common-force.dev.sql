5eff26b307e43f9b0428e356b6b28d2faf20a621
Neil Drumm <drumm@delocalizedham.com>
2013-01-30 22:20:02 -0800

    Truncate more private customer info.

--- snapshot/common-force.dev.sql
+++ snapshot/common-force.dev.sql
@@ -9,3 +9,5 @@ TRUNCATE semaphore;
 TRUNCATE votingapi_vote;
 TRUNCATE field_data_commerce_customer_address;
 TRUNCATE field_revision_commerce_customer_address;
+TRUNCATE commerce_payment_transaction;
+TRUNCATE commerce_payment_transaction_revision;
