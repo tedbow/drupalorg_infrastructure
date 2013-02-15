eac0c9221218d98f3d0601f5b1a071fc8a4262fa
Neil Drumm <drumm@delocalizedham.com>
2013-02-15 15:05:38 -0800

    Do not loop for vanished files. Files always vanish.

--- staging/sync-files.sh
+++ staging/sync-files.sh
@@ -5,13 +5,12 @@
 # Exit immediately on uninitialized variable or error, and print each command.
 set -uex
 
-# 24 is vanished source files, as in deleted before syncing completes. Retry
-# when this happens.
-status=24
-while [ ${status} = 24 ]; do
-  # Reset status and use || to catch failure before set -e.
-  status=0
-  rsync -av --exclude=tmp/ /var/www/drupal.org/htdocs/files/ /var/www/staging.devdrupal.org/htdocs/files/ || status=$?
-done
+# Use || to catch failure before set -e.
+status=0
+rsync -av --exclude=tmp/ /var/www/drupal.org/htdocs/files/ /var/www/staging.devdrupal.org/htdocs/files/ || status=$?
 
+# 24 is vanished source files, as in deleted before syncing completes.
+[ ${status} = 24 ] && exit 0
+
+# Otherwise, exit with the real status.
 exit ${status}
