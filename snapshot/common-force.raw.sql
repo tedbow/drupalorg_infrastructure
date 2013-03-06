1a5a901b782873cbf90cd27e014ce299b0f9271e
Neil Drumm <drumm@delocalizedham.com>
2013-03-06 12:45:05 -0800

    Move search index truncation to earlier stage.

--- snapshot/common-force.raw.sql
+++ snapshot/common-force.raw.sql
@@ -1,5 +1,12 @@
+-- Executed with mysql -f, every query will be executed, regardless of failure.
+
 -- Empty out scratch tables used during drupal7 migration.
--- Errors are harmless in other situations.
 TRUNCATE project_issue_migration_timeline;
 TRUNCATE project_issue_migration_original_thread;
 TRUNCATE project_issue_migration_original_issue_data;
+
+-- Empty out often-bulky search indexes
+TRUNCATE search_dataset;
+TRUNCATE search_index;
+TRUNCATE search_node_links;
+TRUNCATE search_total;
--- snapshot/common-force.staging.sql
+++
@@ -1,5 +0,0 @@
--- Executed with mysql -f, every query will be executed, regardless of failure.
-TRUNCATE search_dataset;
-TRUNCATE search_index;
-TRUNCATE search_node_links;
-TRUNCATE search_total;
