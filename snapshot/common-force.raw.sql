e657339a169b1d85d60db77b24e21f532ef81da3
Neil Drumm <drumm@delocalizedham.com>
2013-04-30 16:50:26 -0700

    Also remove a new copy of all the data.

--- snapshot/common-force.raw.sql
+++ snapshot/common-force.raw.sql
@@ -2,6 +2,7 @@
 
 -- Empty out scratch tables used during drupal7 migration.
 TRUNCATE project_issue_migration_timeline;
+TRUNCATE project_issue_migration_timeline_init;
 TRUNCATE project_issue_migration_original_thread;
 TRUNCATE project_issue_migration_original_issue_data;
 
