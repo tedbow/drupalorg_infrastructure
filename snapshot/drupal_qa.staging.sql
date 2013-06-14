66b663bf25101f50cfc05ae9489c74160add1a11
jthorson <jthorson@148199.no-reply.drupal.org>
2013-06-14 00:26:27 -0400

    Issue #2014983 by jthorson: Update qa.staging.drupal.org DB snapshot scripts.

--- snapshot/drupal_qa.staging.sql
+++ snapshot/drupal_qa.staging.sql
@@ -4,11 +4,9 @@ DELETE FROM actions WHERE callback = 'pifr_server_notification_email_action';
 -- Stop active tests in progress
 UPDATE pifr_test SET status = 4, last_requested = unix_timestamp(), last_tested = unix_timestamp() WHERE test_id IN (SELECT test_id FROM pifr_environment_status);
 
--- Generate placeholder results for any tests in progress (Note: Will throw 3 warnings!)
-INSERT INTO pifr_result (test_id) SELECT test_id from pifr_environment_status WHERE 1;
-
--- Datafill placeholder results for any tests in progress
-UPDATE pifr_result pr, pifr_environment_status pes SET pr.environment_id = 1, pr.code = 1, pr.details = 'a:1:{s:7:"@reason";s:43:"Test cancelled by admin prior to completion";}' WHERE pr.test_id = pes.test_id;
+-- Generate placeholder results for any tests in progress (Note: Will throw warnings!)
+INSERT INTO pifr_result (test_id, environment_id, code, details) SELECT test_id, 1, 1, 'a:1:{s:7:"@reason";s:43:"Test cancelled by admin prior to completion";}' from pifr_environment_status WHERE 1;
+INSERT INTO pifr_log (test_id, client_id, code, timestamp) SELECT test_id, client_id, 14, unix_timestamp() from pifr_environment_status WHERE 1;
 
 -- Clear tests in progress from pifr_environment_status
 DELETE FROM pifr_environment_status WHERE 1;
@@ -19,5 +17,8 @@ DELETE FROM pifr_client_test WHERE 1;
 -- disable all testbots
 UPDATE pifr_client SET status = 1 WHERE 1;
 
--- enable staging/dev bots
-UPDATE pifr_client SET status = 4 WHERE client_id IN (973, 988);
+-- enable staging/dev bots and project servers
+UPDATE pifr_client SET status = 4 WHERE client_id IN (973, 988, 1578, 1583);
+
+-- Update client confirmation variable to not require confirmation
+UPDATE variable SET value = 's:2:"-1";' where name = 'pifr_server_client_test_interval';
