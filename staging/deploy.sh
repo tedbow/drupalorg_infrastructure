672f43401f4fe440297c8088337bd6b8256e3f64
Neil Drumm <drumm@delocalizedham.com>
2013-07-10 12:38:45 -0700

    Also handle CiviCRM for the Association site.

--- staging/deploy.sh
+++ staging/deploy.sh
@@ -8,4 +8,10 @@ bzr up
 # updatedb, clear and prime caches.
 ${drush} -v updatedb --interactive
 ${drush} cc all
+# Also handle CiviCRM for the Association site.
+if [ "${uri}" = "association.staging.devdrupal.org" ]; then
+  ${drush} cc civicrm
+  ${drush} compile-templates
+fi
+
 test_site
