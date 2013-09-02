cc87e455888485a52bbcc3ed0f9ae1025baf8173
Neil Drumm <drumm@delocalizedham.com>
2013-09-02 16:05:50 -0700

    Make sure non-production modules are not enavled.

--- live/verify.sh
+++ live/verify.sh
@@ -6,5 +6,10 @@ bzr log -r-1 -n0 > ${WORKSPACE}/version.txt
 bzr status > ${WORKSPACE}/status.txt
 bzr diff > ${WORKSPACE}/diff.txt
 
+# Make sure Devel and Views UI are not enabled.
+if ${drush} pm-list --status=enabled --pipe | grep --quiet '^\(devel\|views_ui\)$'; then
+  exit 1
+fi
+
 # Exit with error if there are changes.
 exit $(cat ${WORKSPACE}/status.txt | wc -l)
