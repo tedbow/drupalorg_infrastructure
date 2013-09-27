d11663c1af80ce150880085e8440d15ede791b0b
Ricardo Amaro <mail@ricardoamaro.com>
2013-09-27 12:51:06 +0000

    staticalize_site script for automatic site crawling

---
+++ camps/staticalize_site.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+
+# Exit immediately on uninitialized variable or error, and print each command.
+#set -uex
+
+###########################
+# Script for Automatic create static Camp sites in html into a tgz
+#
+# Related: http://drupal.org/node/27882 - Creating a static archive of a Drupal site
+# wget --recursive --no-clobber --page-requisites --html-extension \
+# --convert-links --restrict-file-names=windows \
+# --domains colorado2010.camps.drupal.org --no-parent colorado2010.camps.drupal.org/
+#
+# Contributor ricardoamaro
+# Leads: 
+# https://docs.google.com/spreadsheet/ccc?key=0Ao8Y0KepJTHzdDB3YXJWaFk2QTRUV3dZVUhfbThWaUE#gid=0
+###########################
+
+if [[ "$1" == "" ]]; then
+   echo -e "Usage:\nstaticalize_site.sh {domain}"
+   exit 1
+else
+    SITE=$1 
+fi
+
+echo "This script will create a tgz version of ${SITE} on ~/static/${SITE}/"
+
+cd ~/static
+#rm -rf ~/static/${SITE}
+mkdir -p ~/static/${SITE}/
+
+wget -m -U Mozilla --page-requisites --retry-connrefused --limit-rate=5000k --convert-links --restrict-file-names=windows --domains ${SITE}  --no-parent http://${SITE}/
+wget -m -U Mozilla --page-requisites --retry-connrefused --limit-rate=5000k --convert-links --html-extension --restrict-file-names=windows --domains ${SITE}  --no-parent http://${SITE}/
+
+echo "Going to tgz ~/static/${SITE}/"
+sleep 5
+
+cd ~/static/${SITE}/
+tar czvf ~/static/${SITE}.tgz *
+echo "done ${SITE}"
+ls -lah ~/static/${SITE}.tgz
+
+exit 0
+
