fe9bc9557175ea32a03056189d00e73c3dbfa0fe
Neil Drumm <drumm@delocalizedham.com>
2013-11-02 22:55:18 -0700

    Fix last commit

--- snapshot/drupal.dev.sql
+++ snapshot/drupal.dev.sql
@@ -25,5 +25,5 @@ DELETE comment FROM comment LEFT JOIN node ON node.nid = comment.nid WHERE node.
 DELETE comment FROM comment LEFT JOIN users ON comment.uid = users.uid WHERE users.uid IS NULL;
 DELETE comment FROM comment LEFT JOIN comment c2 ON comment.pid = c2.cid WHERE c2.cid IS NULL AND comment.pid <> 0;
 DELETE files FROM files LEFT JOIN users ON files.uid = users.uid WHERE users.uid IS NULL;
-DELETE file_managed FROM files LEFT JOIN users ON files.uid = users.uid WHERE users.uid IS NULL;
+DELETE file_managed FROM file_managed LEFT JOIN users ON file_managed.uid = users.uid WHERE users.uid IS NULL;
 DELETE image FROM image LEFT JOIN node ON image.nid = node.nid WHERE node.nid IS NULL;
