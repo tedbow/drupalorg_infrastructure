e3b8b7ac867c2c13d9df414f6e8f55ad5596a52f
Melissa Anderson <melissa@happypunch.com>
2013-11-13 12:26:58 -0800

    SQL formatting for simplenews_subscriptions.py

--- table_customizations/simplenews_subscriptions.py
+++ table_customizations/simplenews_subscriptions.py
@@ -8,6 +8,13 @@ class Simplenews_Subscriptions(table_customizations.TableHandler):
         for column in [e[1] for e in column_names if e[0]]:
             columns += ', ' + column
             srccolumns += ", CONCAT(snid, '@sanitized.invalid')"
-        query = "INSERT INTO `{dest}`.`{table}` ({columns}) SELECT {srccolumns} FROM `{source}`.`{table}` {limit}".format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
+        query = """
+            INSERT INTO
+              `{dest}`.`{table}` ({columns}) 
+            SELECT 
+              {srccolumns} 
+            FROM
+              `{source}`.`{table}` {limit}
+          """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         print query
         return query
