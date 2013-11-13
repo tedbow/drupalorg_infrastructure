0aa4250a97cd0a3d1c155a3d95ed5ed4f8d0bbcf
Melissa Anderson <melissa@happypunch.com>
2013-11-13 12:20:41 -0800

    SQL formatting for user_roles.py

--- table_customizations/users_roles.py
+++ table_customizations/users_roles.py
@@ -7,6 +7,21 @@ class Users_Roles(table_customizations.TableHandler):
         print "Special tables {0} reached the plain insert.".format([e[1] for e in column_names if e[0]])
         query = "INSERT INTO `{dest}`.`{table}` ({columns}) SELECT {columns} FROM `{source}`.`{table}` {limit}".format(table=self.table, dest=self.dst, source=self.src, columns=columns, limit=self.limit)
         if self.dataset == 'skeleton':
-            query = "INSERT INTO `{dest}`.`{table}` ({columns}) SELECT {columns} FROM `{source}`.`{table}` LEFT JOIN `{dest}`.`users` ON (`{source}`.`{table}`.uid = `{dest}`.`users`.uid) WHERE `{dest}`.`users`.uid IS NOT NULL {limit}".format(table=self.table, dest=self.dst, source=self.src, columns=columns, limit=self.limit)
+            query = """
+              INSERT INTO 
+                `{dest}`.`{table}` ({columns}) 
+              SELECT 
+                {columns} 
+              FROM
+                `{source}`.`{table}` 
+                LEFT JOIN 
+                  `{dest}`.`users` 
+                ON 
+                  (`{source}`.`{table}`.uid = `{dest}`.`users`.uid) 
+                WHERE
+                  `{dest}`.`users`.uid 
+                IS NOT NULL 
+                  {limit}
+              """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, limit=self.limit)
         print query
         return query
