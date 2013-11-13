3b422696ced59ba83dc3b39cd4820dff41516be7
Melissa Anderson <melissa@happypunch.com>
2013-11-13 12:33:56 -0800

    SQL formatting for authmap.py

--- table_customizations/authmap.py
+++ table_customizations/authmap.py
@@ -8,6 +8,13 @@ class Authmap(table_customizations.TableHandler):
         for column in [e[1] for e in column_names if e[0]]:
             columns += ', ' + column
             srccolumns += ", CONCAT(aid, '@sanitized.invalid')"
-        query = "INSERT INTO `{dest}`.`{table}` ({columns}) SELECT {srccolumns} FROM `{source}`.`{table}` {limit}".format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
+        query = """
+          INSERT INTO 
+            `{dest}`.`{table}` ({columns}) 
+          SELECT
+            {srccolumns} 
+          FROM 
+            `{source}`.`{table}` {limit}
+          """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         print query
         return query
