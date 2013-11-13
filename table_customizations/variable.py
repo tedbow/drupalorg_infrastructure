de3821dfdf7ee41e428562e41b6cf83c9855bab0
Melissa Anderson <melissa@happypunch.com>
2013-11-13 12:22:44 -0800

    SQL formatting for variable.py

--- table_customizations/variable.py
+++ table_customizations/variable.py
@@ -8,6 +8,17 @@ class Variable(table_customizations.TableHandler):
         for column in [e[1] for e in column_names if e[0]]:
             columns += ', ' + column
             #Special function would go here
-        query = "INSERT INTO `{dest}`.`{table}` ({columns}) SELECT {srccolumns} FROM `{source}`.`{table}` WHERE `name` NOT LIKE '%key%' {limit}".format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
+        query = """
+          INSERT INTO
+            `{dest}`.`{table}` ({columns}) 
+          SELECT 
+            {srccolumns} 
+          FROM
+            `{source}`.`{table}`
+          WHERE
+            `name`
+          NOT LIKE
+            '%key%' {limit}
+          """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         print query
         return query
