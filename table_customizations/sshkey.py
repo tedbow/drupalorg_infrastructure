87a718ea54c3f4e4d5febfb7723bbd3350fdb79f
Melissa Anderson <melissa@happypunch.com>
2013-11-13 12:25:11 -0800

    SQL formatting for sshkey.py

--- table_customizations/sshkey.py
+++ table_customizations/sshkey.py
@@ -8,6 +8,13 @@ class Sshkey(table_customizations.TableHandler):
         for column in [e[1] for e in column_names if e[0]]:
             columns += ', ' + column
             srccolumns += ", 'nobody@nomail.invalid'"
-        query = "INSERT INTO `{dest}`.`{table}` ({columns}) SELECT {srccolumns} FROM `{source}`.`{table}` {limit}".format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
+        query = """
+          INSERT INTO 
+            `{dest}`.`{table}` ({columns}) 
+          SELECT
+            {srccolumns} 
+          FROM
+            `{source}`.`{table}` {limit}
+            """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         print query
         return query
