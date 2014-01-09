5b86a4787c272b1b6109c2906a1ad40d3f02e2a3
Neil Drumm <drumm@delocalizedham.com>
2014-01-08 20:50:09 -0800

    Remove tables which no longer exist.

--- snapshot/common-force.raw.sql
+++ snapshot/common-force.raw.sql
@@ -1,12 +1,6 @@
 -- Executed with mysql -f, every query will be executed, regardless of failure.
 
 -- Empty out scratch tables used during drupal7 migration.
-TRUNCATE project_issue_migration_timeline;
-TRUNCATE project_issue_migration_timeline_init;
-TRUNCATE project_issue_migration_original_thread;
-TRUNCATE project_issue_migration_original_issue_data;
-TRUNCATE search_index_d6;
-TRUNCATE search_total_d6;
 TRUNCATE project_issue_projects;
 TRUNCATE projects;
 TRUNCATE project_issues;
