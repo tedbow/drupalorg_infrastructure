b7a340ed3b7a71e5d2c0079c6c167dbe9262d5c7
Rudy Grigar <rudy@grigar.net>
2014-02-06 09:53:03 -0800

    add handling for sanitizing the primary drupal db

--- sanitize/sanitize.sh
+++ sanitize/sanitize.sh
@@ -5,7 +5,8 @@
 # Infra - infrastructure dev
 # Boss - for drupal.org
 # Skeleton - for drupal.org dev
-profile=${1}
+database=${1}
+profile=${2}
 export_db="drupal_export"
 
 # We are only creating dev snapshots right now.
@@ -18,8 +19,12 @@ user=$(cat sanitize/password.py | grep user | sed -e "s/user = '//" -e "s/'//")
 password=$(cat sanitize/password.py | grep password | sed -e "s/password = '//" -e "s/'//")
 tmp_args="-h${host} -u${user} -p${password} ${export_db}"
 
+if [ ${database} == "drupal" ]; then
+  mysqldump -h db2-main-vip.drupal.org -u${user} -p${password} --single-transaction --quick ${database} | database="drupal_sanitize" mysql -h${host} -u${user} -p${password} ${database}
+fi
+
 # Sanitize into the export database.
-python26 ./sanitize/sanitize_db.py -s infrastructure -d ${export_db} -p ${profile}
+python26 ./sanitize/sanitize_db.py -s ${database} -d ${export_db} -p ${profile}
 
 # Save the DB dump.
 mysqldump --single-transaction --quick ${tmp_args} | sed -e 's/^) ENGINE=[^ ]*/)/' | bzip2 > "/var/dumps/${subdir}/${JOB_NAME}${suffix}-${BUILD_NUMBER}-in-progress.sql.bz2"
