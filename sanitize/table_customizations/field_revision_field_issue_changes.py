2fc7e9c53e8d5ecdbe9452c39c8e8f917c323943
Rudy Grigar <basic@drupal.org>
2014-02-20 19:48:40 +0000

    join on primary key instead of entity_id to avoid duplicates on deltas

--- sanitize/table_customizations/field_revision_field_issue_changes.py
+++ sanitize/table_customizations/field_revision_field_issue_changes.py
@@ -23,6 +23,18 @@ class Field_Revision_Field_Issue_Changes(table_customizations.TableHandler):
               INNER JOIN 
                 {source}.field_data_field_issue_changes
               ON
-                {source}.field_data_field_issue_changes.entity_id = {source}.{table}.entity_id {limit}
+                {source}.field_data_field_issue_changes.entity_id = {source}.{table}.entity_id
+              AND
+                {source}.{table}.entity_id = {source}.field_data_field_issue_changes.entity_id 
+              AND
+                {source}.{table}.entity_type = {source}.field_data_field_issue_changes.entity_type 
+              AND
+                {source}.{table}.revision_id = {source}.field_data_field_issue_changes.revision_id
+              AND
+                {source}.{table}.deleted = {source}.field_data_field_issue_changes.deleted
+              AND
+                {source}.{table}.delta = {source}.field_data_field_issue_changes.delta
+              AND
+                {source}.{table}.language = {source}.field_data_field_issue_changes.language {limit}
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         return query
