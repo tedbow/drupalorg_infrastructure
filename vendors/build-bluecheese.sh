82eabd33dc708d585da2f1533b41af5b2329b68b
Rudy Grigar <rudy@grigar.net>
2014-03-19 16:33:01 -0400

    Update wrapper for buildvm directory structure and bundler

--- vendors/build-bluecheese.sh
+++ vendors/build-bluecheese.sh
@@ -2,7 +2,7 @@
 set -uex
 
 # Jenkins job tracks http://git.drupal.org/project/bluecheese.git
-cd ${WORKSPACE}
+cd /var/git/bluecheese
 
 # Make sure remote exists and is updated.
 git remote show private || git remote add private ssh://git@bitbucket.org/drupalorg-infrastructure/bluecheese-private.git
@@ -19,7 +19,7 @@ git pull
 git merge 7.x-1.x
 
 # Compile CSS.
-compass compile
+bundle exec compass compile
 if ! git diff --quiet css/styles.css; then
   git add css/styles.css
   git commit -m 'compass compile'
