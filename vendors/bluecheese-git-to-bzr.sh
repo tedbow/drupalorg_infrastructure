2549b22de5c6d464cfd60779da55b91e1543d4f5
Neil Drumm <drumm@delocalizedham.com>
2014-03-25 10:15:14 -0700

    Cleanup, in case a previous build failed.

--- vendors/bluecheese-git-to-bzr.sh
+++ vendors/bluecheese-git-to-bzr.sh
@@ -4,6 +4,9 @@ set -uex
 # Jenkins job tracks git@bitbucket.org:drupalorg-infrastructure/bluecheese-private.git
 cd ${WORKSPACE}
 
+# Cleanup, in case a previous build failed.
+rm -rf archive.tar bzr-vendor
+
 # Get the BZR copy of the theme.
 bzr checkout --lightweight /bzr/bluecheese-7 bzr-vendor
 
