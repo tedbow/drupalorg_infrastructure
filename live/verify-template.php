c8cb2650fe8a13a956412443c147c51f661149d4
Neil Drumm <drumm@delocalizedham.com>
2014-03-26 11:57:37 -0700

    #2224865: Basic HTML report.

---
+++ live/verify-template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Site status</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <p><span class="badge"><?php print $_ENV['version'] ?></span> is currently deployed</p>
+
+    <script src="js/jquery-1.11.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
--- live/verify.sh
+++ live/verify.sh
@@ -1,10 +1,18 @@
 # Include common live script.
 . live/common.sh 'verify'
 
+# Collect information.
 cd ${webroot}
+export version=$(bzr version-info --custom --template="{revno}")
 bzr log -r-1 -n0 > ${WORKSPACE}/version.txt
 bzr status > ${WORKSPACE}/status.txt
 bzr diff > ${WORKSPACE}/diff.txt
+
+# Set up report area.
+cd ${WORKSPACE}
+[ ! -d 'html' ] && mkdir 'html'
+# Generate HTML report.
+php live/verify-template.php > 'html/index.html'
 
 # Make sure Devel is not enabled.
 if ${drush} pm-list --status=enabled --pipe | grep --quiet '^\(devel\)$'; then
