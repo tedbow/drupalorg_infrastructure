898bf3db3611a6c548f4c551ef3304c0f6463210
Neil Drumm <drumm@delocalizedham.com>
2014-05-21 13:53:54 -0700

    #2224865 Do not run pm-refresh first.

--- live/verify.sh
+++ live/verify.sh
@@ -17,7 +17,6 @@ export features=$(
     ${drush} features-diff "${feature}" | head -n 100
   done
 )
-${drush} pm-refresh
 export updates=$(${drush_no} --simulate --pipe pm-updatecode)
 
 # Set up report area.
