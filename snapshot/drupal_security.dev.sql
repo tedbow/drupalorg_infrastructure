66106b4effc1a5de9c65e02e0f59cd4852a60598
Neil Drumm <drumm@delocalizedham.com>
2014-07-27 20:47:21 -0700

    There are no dev snapshots for the Security site.

--- snapshot/drupal_security.dev.sql
+++
@@ -1,4 +0,0 @@
--- CAUTION: DO NOT RUN THIS ON DATABASE WHERE YOU CARE ABOUT THE INFORMATION!!!
-
--- Remove sensitive variables
-DELETE FROM profile_values WHERE fid IN (select fid from profile_fields where visibility in (1, 4));
