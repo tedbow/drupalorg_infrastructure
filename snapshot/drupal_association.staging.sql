310327b22e948570efc5aad4227631b57f78edc1
Neil Drumm <drumm@delocalizedham.com>
2014-09-16 13:59:55 -0700

    Commerce was uninstalled.

--- snapshot/drupal_association.staging.sql
+++ snapshot/drupal_association.staging.sql
@@ -1,4 +1,3 @@
 UPDATE comment SET mail = CONCAT(name, '@sanitized.invalid');
 UPDATE donations SET mail = CONCAT(did, '@sanitized.invalid');
 UPDATE contact SET recipients = 'noreply@sanitized.invalid';
-UPDATE commerce_order SET mail = CONCAT(order_id, '@sanitized.invalid');
