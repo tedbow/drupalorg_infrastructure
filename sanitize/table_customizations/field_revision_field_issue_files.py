8687cb79d085c918ff45c7353013a124a0b48590
Archie Brentano <isntall.us@gmail.com>
2014-09-19 12:28:02 -0700

    frfif round two

--- sanitize/table_customizations/field_revision_field_issue_files.py
+++ sanitize/table_customizations/field_revision_field_issue_files.py
@@ -20,16 +20,18 @@ class Field_Revision_Field_Issue_Files(table_customizations.TableHandler):
                 {srccolumns}
               FROM
                 {source}.{table}
-            INNER JOIN
-              {source}.node_revision ON node_revision.vid = {table}.revision_id
-            INNER JOIN
-              {source}.node
-            ON
-              node.nid = node_revision.nid
-            AND
-              node.vid = node_revision.vid
-            WHERE
-              node_revision.timestamp >= (unix_timestamp() - 60*24*60*60)
-              ON {source}.node_revision.vid = {source}.{table}.revision_id {limit}
+              INNER JOIN
+                {source}.node_revision
+              ON
+                {source}.node_revision.vid = {source}.{table}.revision_id
+              INNER JOIN
+                {source}.node
+              ON
+                {source}.node.nid = {source}.node_revision.nid
+              AND
+                {source}.node.vid = {source}.node_revision.vid
+              WHERE
+                {source}.node_revision.timestamp >= (unix_timestamp() - 60*24*60*60)
+              {limit}
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         return query
