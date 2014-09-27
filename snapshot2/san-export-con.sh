689894f7fdfd8f76e1207da10efd638f66d380b6
Archie Brentano <isntall.us@gmail.us>
2014-09-27 08:22:48 +0200

    tab and multi-thread db import

--- snapshot2/san-export-con.sh
+++ snapshot2/san-export-con.sh
@@ -5,19 +5,28 @@ set -uex
 ### Drop raw copy of the DB
 ### stop mysql
 DBIMPORT="drupal"
+DBEXPORT="drupal_export"
 SANTYPE=$1
+
+[ ! -d /var/dumps/${SANTYPE}/td/ ] && mkdir /var/dumps/${SANTYPE}/td/
+rm -rf /var/dumps/${SANTYPE}/td/*
 service mysql start && \
 /media/infrastructure/sanitize/sanitize.sh ${DBIMPORT} ${SANTYPE} $2 && \
 mysql -uroot -e "DROP DATABASE IF EXISTS drupal;" && \
 mysql -uroot -e "DROP DATABASE IF EXISTS pivots;" && \
 mysql -uroot -e "DROP DATABASE IF EXISTS narayan;" && \
+mysqldump ${DBEXPORT} d > /var/dumps/${SANTYPE}/td/${DBEXPORT}-tables.sql
+mysqldump ${DBEXPORT} --tab=/var/dumps/${SANTYPE}/td/
 service mysql stop
 rm -rf /var/lib/mysql/*
 mysql_install_db
-pbunzip2 -f /var/dumps/${SANTYPE}/db_backup.${SANTYPE}-current.sql.bz2 > /var/dumps/${SANTYPE}/db_backup.${SANTYPE}-current.sql
+
+
 service mysql start
 mysql -uroot -e "CREATE DATABASE drupal_export;"
-mysql -uroot drupal_export < /var/dumps/${SANTYPE}/db_backup.${SANTYPE}-current.sql
+
+time mysql ${DBEXPORT} < /var/dumps/${SANTYPE}/td/${DBEXPORT}-tables.sql
+time mysqlimport -uroot --use-threads=8 ${DBEXPORT}  /var/dumps/${SANTYPE}/td/*.txt
+
 service mysql stop
 exit
-
