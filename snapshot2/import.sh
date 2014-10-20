d201b96cecdb600524e15b07e07d92d7ed0eae96
Archie Brentano <isntall.us@gmail.com>
2014-10-20 13:55:04 -0700

    removing unneeded vars

--- snapshot2/import.sh
+++ snapshot2/import.sh
@@ -3,8 +3,6 @@
 set -uex
 CWD=$(dirname "${BASH_SOURCE[0]}")
 export DATE=$(date +'%Y%m%d%H%M')
-export SANTYPE=$1
-export SANOUT=$2
 [ ! -f ${CWD}/conf ] && exit 1
 source ${CWD}/conf
 ${CWD}/raw-import.sh
