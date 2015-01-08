d8bbff7422050e4509efa9c6dea62ffd17c5a861
Archie Brentano <isntall.us@gmail.us>
2015-01-08 13:26:57 -0800

    adding jenkins command

---
+++ snapshot2/snapshot_jenkins_prod2dev.sh
@@ -0,0 +1,6 @@
+##!/bin/bash
+#set -uex
+###For calling in jenkins
+#export SCRIPTDIR="/usr/local/drupal-infrastructure"
+#cd ${SCRIPTDIR}
+#./snapshot2/snapshot_prod2dev.sh <PRODDB> <PRODDBSERBER> <SANSERVER>
