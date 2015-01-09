a9ed78b21847ca0064ee5b5e5e208b23780c5731
Archie Brentano <isntall.us@gmail.us>
2015-01-09 11:25:09 -0800

    variables needed to be higher

--- snapshot2/snapshot_prod-rsync.sh
+++ snapshot2/snapshot_prod-rsync.sh
@@ -6,13 +6,12 @@ set -uex
 [ ! -f "/etc/dbdump/conf" ] && exit 1
 source /etc/dbdump/conf
 
-DUMPDIR="${PRODDUMPDIR}/${PRODDB}"
-DESTDUMP="${FSDEST}/${PRODDB}"
-
-
 PRODDB="${1}"
 SSHDEST="${2}"
 CWD="${3}"
+
+DUMPDIR="${PRODDUMPDIR}/${PRODDB}"
+DESTDUMP="${FSDEST}/${PRODDB}"
 
 rsync -zavhP --delete --exclude-from ${CWD}/rsync-table-exclusions.txt \
   ${DUMPDIR} ${SSHDEST}:${DESTDUMP}/
