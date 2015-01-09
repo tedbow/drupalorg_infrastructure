6c7fe3f42407e0c2e673bd413bec28c8d43f226b
Archie Brentano <isntall.us@gmail.us>
2015-01-09 09:53:13 -0800

    this version works on jenkins

--- snapshot2/snapshot_jenkins_proddump.sh
+++ snapshot2/snapshot_jenkins_proddump.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
-
+##!/bin/bash
+#
 #set -uex
+#
 ###Copy into jenkins, remove first comment and add prod db name
+#SSHUSER="ec2"
+#PRODBSERVER="db6-reader-vip.drupal.org"
+#SCRIPTDIR="/usr/local/drupal-infrastructure"
 #PRODDB=""
-#[ -z "$PRODDB" ] && exit 1
+#
 ###ssh to prod db server mysql dump
-#ssh ${SSHUSER}@${STAGINGDBSERVER} "${SCRIPTDIR}/snapshot2/snapshot_prod-dump.sh ${PRODDB} ${PRODDUMPDIR}"
+#ssh ${SSHUSER}@${PRODBSERVER} "${SCRIPTDIR}/snapshot2/snapshot_prod-dump.sh ${PRODDB}"
