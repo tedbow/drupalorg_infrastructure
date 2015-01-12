b70e1d661ce6f2ba0ff9ab7bf9176fa869969871
Archie Brentano <isntall.us@gmail.us>
2015-01-12 13:23:24 -0800

    fixing typo

--- snapshot2/snapshot_prod-import-dev.sh
+++ snapshot2/snapshot_prod-import-dev.sh
@@ -8,11 +8,13 @@ FSDEST="${2}"
 SSHUSER="${3}"
 RAWDUMP="${FSDEST}/${PRODDB}"
 sudo chown -R ${SSHUSER}:${SSHUSER} ${RAWDUMP}/
+[ ! -d "${DEVDEST}/current-${PRODDB}" ] && sudo btrfs sub create ${DEVDEST}/current-${PRODDB}
+sudo chown -R ${SSHUSER}:${SSHUSER} ${DEVDEST}/current-${PRODDB}
 
 docker run -t --rm \
   -v ${RAWDUMP}/:/mnt/ \
-  -v${DEVDEST}:/var/lib/mysql/ \
+  -v ${DEVDEST}/current-${PRODDB}:/var/lib/mysql/ \
   -v ${INFRAREPO}/:/media/infrastructure/ \
   ${DOCKERCON} \
-  /media/infrastructure/snapshot2/snapsho_prod-import-con.sh ${PRODB} "/mnt" ${NTHREADS}
+  /media/infrastructure/snapshot2/snapsho_prod-import-con.sh ${PRODDB} "/mnt" ${NTHREADS}
 sync
