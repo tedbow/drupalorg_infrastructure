f1b855227b1778418abf071b82aafc205e2d785b
Archie Brentano <isntall.us@gmail.us>
2015-01-12 11:30:05 -0800

    not needed

--- snapshot2/binback.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -uex
-SSHTARGET=$1
-STORAGE=$2
-BINVARS=$3
-time sudo innobackupex ${BINVARS} --user='root' --password='88*<hwUz$@' --stream=xbstream \
-  /var/tmp/backup/b1 | ssh -i /home/ec2/.ssh/id_rsa ec2@${SSHTARGET} "xbstream -x -C ${STORAGE}/mysql/current-raw/"
