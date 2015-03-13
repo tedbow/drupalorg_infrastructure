a7d26c9178859e655f815798b3f1c8878870faf2
Mixologic <ryan+ddoto@association.drupal.org>
2015-03-13 12:19:23 -0700

    Wrong path.

--- stats/fastlydownload_parse.sh
+++ stats/fastlydownload_parse.sh
@@ -6,6 +6,6 @@ cd /data/logs/fastly/varnish-syslogs
 # Look for files that have not yet been processed, and ignore todays file.
 for filename in *; do
    if [ ! -f "/data/logs/fastly/downloadcounts/${filename%fastly}downloadcounts.csv" ] && [ $filename != $(date +%Y.%m.%d.fastly) ]; then
-     /usr/local/drupal-infrastructure/fastlycounts.awk $filename
+     /usr/local/drupal-infrastructure/stats/fastlycounts.awk $filename
    fi;
 done
