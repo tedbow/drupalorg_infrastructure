deb4ee3b2547c7ba4e906fb3a9ac7cb30afcc492
Mixologic <ryan+ddoto@association.drupal.org>
2015-03-13 12:06:56 -0700

    adds the script that runs the fastly varnishlog parser

---
+++ stats/fastlydownload_parse.sh
@@ -0,0 +1,11 @@
+export PATH=$PATH:/usr/local/bin
+export TERM=dumb
+
+cd /data/logs/fastly/varnish-syslogs
+
+# Look for files that have not yet been processed, and ignore todays file.
+for filename in *; do
+   if [ ! -f "/data/logs/downloadcounts/${filename%fastly}downloadcounts.csv" ] && [ $filename != $(date +%Y.%m.%d.fastly) ]; then
+     /usr/local/drupal-infrastructure/fastlycounts.awk $filename
+   fi;
+done
