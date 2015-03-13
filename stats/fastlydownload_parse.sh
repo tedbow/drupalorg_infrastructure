f3ba9c36a33c7c27b55bab0475c9d2f0023682b2
Mixologic <ryan+ddoto@association.drupal.org>
2015-03-13 12:12:56 -0700

    moving the downloadcounts under fastly.

--- stats/fastlydownload_parse.sh
+++ stats/fastlydownload_parse.sh
@@ -5,7 +5,7 @@ cd /data/logs/fastly/varnish-syslogs
 
 # Look for files that have not yet been processed, and ignore todays file.
 for filename in *; do
-   if [ ! -f "/data/logs/downloadcounts/${filename%fastly}downloadcounts.csv" ] && [ $filename != $(date +%Y.%m.%d.fastly) ]; then
+   if [ ! -f "/data/logs/fastly/downloadcounts/${filename%fastly}downloadcounts.csv" ] && [ $filename != $(date +%Y.%m.%d.fastly) ]; then
      /usr/local/drupal-infrastructure/fastlycounts.awk $filename
    fi;
 done
