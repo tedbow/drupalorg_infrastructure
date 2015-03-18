8a647e0facf6a7bd1c1515eb9fd4927f6ae82d33
Archie Brentano <isntall.us@gmail.us>
2015-03-18 09:55:30 -0700

    adding sudo to btrfs commands

--- dev/snapshot-sendreceive-db.sh
+++ dev/snapshot-sendreceive-db.sh
@@ -7,21 +7,19 @@ SSTYPE="${1}"
 [ ! -d "${DBDUMPCONF}" ] && source "${DBDUMPCONF}" || exit 1
 
 ### Get the name of the parent on the FileSystemDestinationDirectory
-SSDESTDIRSUBVOL="/mnt/storage/sub_vol_db/storage_subvol"
-DESTPARENT="$(cd ${SSDESTDIRSUBVOL} && ls | grep "${SSTYPE}"  | sort | tail -n 1)"
+DESTPARENT="$(cd ${SSDESTSUBVOL} && ls | grep "${SSTYPE}"  | sort | tail -n 1)"
 
 #### OGPARENT="$(cd ${SSORIGSUBVOL} && ls | grep "${SSTYPE}"  | sort | tail -n 2 | head)"
 OGCHILD="$(cd ${SSORIGSUBVOL} &&ls | grep "${SSTYPE}"  | sort | tail -n 1)"
 NEWSS="${SSORIGSUBVOL}/${OGCHILD}"
 
 ### check if SS already exists
-[ -d "${SSDESTDIRSUBVOL}/${OGCHILD}" ] && echo "Dir ${SSDESTDIRSUBVOL}/${OGCHILD} already exists" && exit 0
+[ -d "${SSDESTSUBVOL}/${OGCHILD}" ] && echo "Dir ${SSDESTSUBVOL}/${OGCHILD} already exists" && exit 0
 
 ### Verify that the DESTPARENT exists onf the FS OG
 ## check to see if there is a DESTPARENT
-[ ! -z "${DESTPARENT}" ] &&  time btrfs send ${NEWSS} | btrfs receive ${SSDESTDIRSUBVOL}/ && exit 0
+[ -z "${DESTPARENT}" ] && time sudo btrfs send ${NEWSS} | sudo btrfs receive ${SSDESTSUBVOL}/ && exit 0 || exit 1
 ## verify that the DESTPARENT exists on the FSORIG
 [ -d "${SSORIGSUBVOL}/${DESTPARENT}" ] || echo "The parent does not exist on the originating file system" && exit 1
 ## find closest relative on dest dir
-time btrfs send -p"${SSORIGSUBVOL}/${DESTPARENT}" "${NEWSS}" | btrfs receive ${SSDESTDIRSUBVOL}/
-
+time sudo btrfs send -p"${SSORIGSUBVOL}/${DESTPARENT}" "${NEWSS}" | sudo btrfs receive ${SSDESTSUBVOL}/
