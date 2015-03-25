9fcfd0861f08a8488b08580fa84bd85d3e62f2b0
Mixologic <ryan+ddoto@association.drupal.org>
2015-03-24 17:10:10 -0700

    Artifacts of experiments.

--- monitoring/testbot_check.sh
+++ monitoring/testbot_check.sh
@@ -6,6 +6,6 @@
 set -uex
 
 cd /var/www/qa.drupal.org/htdocs
-TESTBOTS=`/usr/bin/drush sqlc < /usr/local/drupal-infrastructure/monitoring/testbot_check.sql |awk 'NR > 1 {system curl --data print "Client #" $4 ": " $5 " Stalled for more than 1 hour. Last activity was : " $1,$2} '`
+TESTBOTS=`/usr/bin/drush sqlc < /usr/local/drupal-infrastructure/monitoring/testbot_check.sql |awk 'NR > 1 {print "Client #" $4 ": " $5 " Stalled for more than 1 hour. Last activity was : " $1,$2} '`
 
 curl -X POST --data-urlencode 'payload={"channel": "#testbots", "username": "PIFR the pufferfish", "text": "'"${TESTBOTS}"'", "icon_emoji": ":puffer:"}' https://hooks.slack.com/services/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
