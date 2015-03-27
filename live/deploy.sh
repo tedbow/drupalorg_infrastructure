82eb65352942ce7e1683b78926d8925e717cac0a
Neil Drumm <drumm@delocalizedham.com>
2015-03-27 16:07:14 -0700

    More verbosity

--- live/deploy.sh
+++ live/deploy.sh
@@ -12,24 +12,24 @@ if [ "${updatedb-}" = "true" ]; then
   ${drush} -v updatedb --interactive
 fi
 if [ "${civicrm_upgrade_db-}" = "true" ]; then
-  ${drush} civicrm-upgrade-db
+  ${drush} -v civicrm-upgrade-db
 fi
 if [ "${cc_menu-}" = "true" ]; then
-  ${drush} cc "menu"
+  ${drush} -v cc "menu"
 fi
 if [ "${cc_theme-}" = "true" ]; then
-  ${drush} cc "theme-registry"
+  ${drush} -v cc "theme-registry"
 fi
 if [ "${cc_cssjs-}" = "true" ]; then
-  ${drush} cc "css-js"
+  ${drush} -v cc "css-js"
 fi
 if [ "${cc_views-}" = "true" ]; then
-  ${drush} cc "views"
+  ${drush} -v cc "views"
 fi
 if [ "${cc_all-}" = "true" ]; then
-  ${drush} cc "all"
+  ${drush} -v cc "all"
 fi
 if [ "${civicrm_cache_clear-}" = "true" ]; then
-  ${drush} cc civicrm
-  ${drush} compile-templates
+  ${drush} -v cc civicrm
+  ${drush} -v compile-templates
 fi
