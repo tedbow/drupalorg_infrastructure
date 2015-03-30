e50cad9b682f452c281a91c8a4767496ba044a2c
Mixologic <ryan+ddoto@association.drupal.org>
2015-03-30 07:56:19 -0700

    adds additional queries for further size reduction

--- snapshot/drupal.reduce.sql
+++ snapshot/drupal.reduce.sql
@@ -9,17 +9,72 @@ DELETE tracker_user FROM tracker_user LEFT JOIN users ON tracker_user.uid = user
 DELETE node_revision FROM node_revision LEFT JOIN node ON node.nid = node_revision.nid WHERE node.nid IS NULL;
 DELETE node_revision FROM node_revision LEFT JOIN node ON node.nid = node_revision.nid AND node.vid = node_revision.vid WHERE node.nid IS NULL AND node_revision.timestamp < (unix_timestamp() - 60*24*60*60);
 DELETE node_comment_statistics FROM node_comment_statistics LEFT JOIN node ON node.nid = node_comment_statistics.nid WHERE node.nid IS NULL;
-DELETE comment FROM comment LEFT JOIN node ON node.nid = comment.nid WHERE node.nid IS NULL;
 
 DELETE field_data_body FROM field_data_body LEFT JOIN node_revision ON node_revision.vid = field_data_body.revision_id WHERE node_revision.vid IS NULL;
 DELETE field_revision_body FROM field_revision_body LEFT JOIN node_revision ON node_revision.vid = field_revision_body.revision_id WHERE node_revision.vid IS NULL;
+-- Delete comments attached to deleted nodes.
+DELETE comment FROM comment LEFT JOIN node ON node.nid = comment.nid WHERE node.nid IS NULL;
 DELETE field_data_comment_body FROM field_data_comment_body LEFT JOIN comment ON comment.cid = field_data_comment_body.entity_id WHERE comment.cid IS NULL;
+DELETE cb FROM field_revision_comment_body cb LEFT JOIN comment c on c.cid = cb.entity_id WHERE c.cid IS NULL;
+
 DELETE field_data_field_issue_changes FROM field_data_field_issue_changes LEFT JOIN comment ON comment.cid = field_data_field_issue_changes.entity_id WHERE comment.cid IS NULL;
+DELETE fdf FROM field_data_field_issue_assigned fdf LEFT JOIN node_revision ON node_revision.vid = fdf.revision_id WHERE node_revision.vid IS NULL;
+DELETE fdf FROM field_data_field_issue_category fdf LEFT JOIN node_revision ON node_revision.vid = fdf.revision_id WHERE node_revision.vid IS NULL;
+DELETE fdf FROM field_data_field_issue_component fdf LEFT JOIN node_revision ON node_revision.vid = fdf.revision_id WHERE node_revision.vid IS NULL;
+DELETE fdf FROM field_data_field_issue_priority fdf LEFT JOIN node_revision ON node_revision.vid = fdf.revision_id WHERE node_revision.vid IS NULL;
+DELETE fdf FROM field_data_field_issue_files fdf LEFT JOIN node_revision ON node_revision.vid = fdf.revision_id WHERE node_revision.vid IS NULL;
+DELETE fdf FROM field_data_field_issue_status fdf LEFT JOIN node_revision ON node_revision.vid = fdf.revision_id WHERE node_revision.vid IS NULL;
+DELETE fdf FROM field_data_field_issue_version fdf LEFT JOIN node_revision ON node_revision.vid = fdf.revision_id WHERE node_revision.vid IS NULL;
+DELETE fdf FROM field_data_field_project fdf LEFT JOIN node_revision ON node_revision.vid = fdf.revision_id WHERE node_revision.vid IS NULL;
+
 DELETE field_revision_field_issue_changes FROM field_revision_field_issue_changes LEFT JOIN field_data_field_issue_changes ON field_data_field_issue_changes.entity_id = field_revision_field_issue_changes.entity_id WHERE field_data_field_issue_changes.entity_id IS NULL;
 DELETE field_revision_field_issue_files FROM field_revision_field_issue_files LEFT JOIN node_revision ON node_revision.vid = field_revision_field_issue_files.revision_id WHERE node_revision.vid IS NULL;
+DELETE field_revision_field_issue_component FROM field_revision_field_issue_component LEFT JOIN node_revision ON node_revision.vid = field_revision_field_issue_component.revision_id WHERE node_revision.vid IS NULL;
+DELETE field_revision_field_issue_assigned FROM field_revision_field_issue_assigned LEFT JOIN node_revision ON node_revision.vid = field_revision_field_issue_assigned.revision_id WHERE node_revision.vid IS NULL;
+DELETE field_revision_field_issue_category FROM field_revision_field_issue_category LEFT JOIN node_revision ON node_revision.vid = field_revision_field_issue_category.revision_id WHERE node_revision.vid IS NULL;
+DELETE field_revision_field_issue_component FROM field_revision_field_issue_component LEFT JOIN node_revision ON node_revision.vid = field_revision_field_issue_component.revision_id WHERE node_revision.vid IS NULL;
+DELETE field_revision_field_issue_priority FROM field_revision_field_issue_priority LEFT JOIN node_revision ON node_revision.vid = field_revision_field_issue_priority.revision_id WHERE node_revision.vid IS NULL;
+DELETE field_revision_field_issue_related FROM field_revision_field_issue_related LEFT JOIN node_revision ON node_revision.vid = field_revision_field_issue_related.revision_id WHERE node_revision.vid IS NULL;
+DELETE field_revision_field_issue_status FROM field_revision_field_issue_status LEFT JOIN node_revision ON node_revision.vid = field_revision_field_issue_status.revision_id WHERE node_revision.vid IS NULL;
+DELETE field_revision_field_issue_version FROM field_revision_field_issue_version LEFT JOIN node_revision ON node_revision.vid = field_revision_field_issue_version.revision_id WHERE node_revision.vid IS NULL;
+DELETE field_revision_field_project FROM field_revision_field_project LEFT JOIN node_revision ON node_revision.vid = field_revision_field_project.revision_id WHERE node_revision.vid IS NULL;
+DELETE field_revision_body FROM field_revision_body LEFT JOIN node_revision ON node_revision.vid = field_revision_body.revision_id WHERE node_revision.vid IS NULL;
 
+-- Delete taxonomy vocab associated with deleted nodes
+DELETE ft FROM roles_drupal.field_revision_taxonomy_vocabulary_9 ft LEFT JOIN roles_drupal.node n on n.nid = ft.entity_id WHERE n.nid is NULL;
+-- Delete versioncontrol operations more than 60 days old.
 DELETE FROM versioncontrol_operations WHERE author_date < (unix_timestamp() - 60*24*60*60);
+DELETE versioncontrol_operation_labels FROM versioncontrol_operation_labels LEFT JOIN versioncontrol_operations ON versioncontrol_operation_labels.vc_op_id = versioncontrol_operations.vc_op_id WHERE versioncontrol_operations.vc_op_id IS NULL;
+DELETE versioncontrol_git_operations FROM versioncontrol_git_operations LEFT JOIN versioncontrol_operations ON versioncontrol_git_operations.vc_op_id = versioncontrol_operations.vc_op_id WHERE versioncontrol_operations.vc_op_id IS NULL;
 DELETE versioncontrol_item_revisions FROM versioncontrol_item_revisions LEFT JOIN versioncontrol_operations ON versioncontrol_item_revisions.vc_op_id = versioncontrol_operations.vc_op_id WHERE versioncontrol_operations.vc_op_id IS NULL;
 DELETE versioncontrol_git_item_revisions FROM versioncontrol_git_item_revisions LEFT JOIN versioncontrol_item_revisions ON versioncontrol_git_item_revisions.item_revision_id = versioncontrol_item_revisions.item_revision_id WHERE versioncontrol_item_revisions.item_revision_id IS NULL;
+
+-- Delete searchapi data that points to deleted nodes.
+DELETE v FROM search_api_db_project_issues_project_issue_followers v LEFT JOIN node n on n.nid = v.item_id WHERE n.nid IS NULL;
+DELETE v FROM search_api_db_project_issues_project_issue_participants v LEFT JOIN node n on n.nid = v.item_id WHERE n.nid IS NULL;
 DELETE v FROM search_api_db_project_issues_text v LEFT JOIN node n ON n.nid = v.item_id WHERE n.nid IS NULL;
 DELETE v FROM search_api_db_project_issues v LEFT JOIN node n ON n.nid = v.item_id WHERE n.nid IS NULL;
+
+-- Delete irrelevant sampler data.
+DELETE FROM sampler_project_issue_opened_vs_closed_by_category WHERE timestamp < (unix_timestamp() - 2*365*24*60*60);
+DELETE FROM sampler_project_issue_reporters_participants_by_project WHERE timestamp < (unix_timestamp() - 2*365*24*60*60);
+DELETE FROM sampler_project_issue_responses_by_project WHERE timestamp < (unix_timestamp() - 2*365*24*60*60);
+DELETE FROM sampler_project_issue_new_issues_comments_by_project WHERE timestamp < (unix_timestamp() - 2*365*24*60*60);
+DELETE FROM sampler_project_release_new_releases WHERE timestamp < (unix_timestamp() - 2*365*24*60*60);
+
+-- Delete flags that point to non-existant nodes.
+DELETE fc FROM flag_content fc
+LEFT JOIN node n on n.nid = fc.content_id and fc.content_type = 'node'
+WHERE n.nid IS NULL;
+
+-- Delete users who have never logged in.
+DELETE u FROM users u WHERE u.login = 0 and u.uid != 0;
+DELETE me FROM multiple_email me LEFT JOIN users u ON u.uid = me.uid WHERE u.uid IS NULL;
+DELETE fc FROM field_revision_field_country fc LEFT JOIN users u ON u.uid = fc.entity_id WHERE u.uid IS NULL;
+DELETE fc FROM field_data_field_country fc LEFT JOIN users u ON u.uid = fc.entity_id WHERE u.uid IS NULL;
+
+-- Delete role activty > 60 days
+DELETE ra FROM role_activity ra WHERE ra.timestamp < (unix_timestamp() - 60*24*60*60);
+TRUNCATE TABLE cvs_files;
+-- project_usage_week_release
+-- project_usage_week_project
