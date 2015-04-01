4dfd4a562a9d79170ca372b327c563ab1729568a
Rudy Grigar <basic@drupal.org>
2015-04-01 12:15:44 -0700

    Add more functions for building AMIs

--- drupalci/common.sh
+++ drupalci/common.sh
@@ -4,7 +4,16 @@
 export EC2_HOME=/opt/ec2/ec2-api-tools-1.7.3.2
 export EC2_URL=ec2.ap-southeast-2.amazonaws.com
 export JAVA_HOME=/usr
+export PACKER_HOME=/opt/packer
 
 latestBaseAMI() {
   $EC2_HOME/bin/ec2-describe-images | awk '/DrupalCI D.O base image/ { ami=$2 } END { print ami }'
+}
+
+buildBaseAMI() {
+  $PACKER_HOME/packer build packer/packer.json
+}
+
+buildAMI() {
+  $PACKER_HOME/packer build -var "source_ami=$(latestBaseAMI)" packer/packer.json
 }
