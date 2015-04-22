b7bde1c115b41b3d282b210c4241d8c0f5ef8b10
Rudy Grigar <basic@drupal.org>
2015-04-22 00:31:42 -0700

    minor tweak to EC2_HOME

--- drupalci/common.sh
+++ drupalci/common.sh
@@ -1,6 +1,6 @@
 # EC2 configuration
 . ~/.ec2creds
-export EC2_HOME=/opt/ec2/ec2-api-tools-1.7.3.2
+export EC2_HOME=/opt/ec2/ec2-api-tools-*
 export EC2_URL=ec2.us-west-2.amazonaws.com
 export JAVA_HOME=/usr
 
