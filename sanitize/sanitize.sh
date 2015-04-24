f71be610586ee00da16b1710f6275bd1a726e8c8
Rudy Grigar <basic@drupal.org>
2015-04-24 10:12:09 -0700

    force a dev 'stage' to match up with existing jobs, and fix the suffix with combined dump+compression

--- sanitize/sanitize.sh
+++ sanitize/sanitize.sh
@@ -17,7 +17,7 @@ database=${1:=-h}
 profile=${2:=empty}
 export_db="drupal_export"
 dbopt="--single-transaction --quick"
-stage="whitelist"
+stage="dev"
 filetype="sql"
 compression="bz2"
 dumppath="/var/dumps/${stage}"
@@ -84,7 +84,7 @@ dumpcur="${dumppath}/${fvar1}-current.${suffix}"
 
 # Save the DB dump, strip ENGINE type from the output
 echo "start the dump"
-mysqldump ${dbopt} ${tmp_args} ${export_db} | sed -e 's/^) ENGINE=[^ ]*/)/' | pbzip2 -fc > ${dumppath}/${dumpinprogress}.${filetype}
+mysqldump ${dbopt} ${tmp_args} ${export_db} | sed -e 's/^) ENGINE=[^ ]*/)/' | pbzip2 -fc > ${dumppath}/${dumpinprogress}.${suffix}
 
 # Move -in-progress to final location and symlink to current
 mv -v ${dumppath}/${dumpinprogress}.${suffix} ${dumppath}/${dumpfile}
