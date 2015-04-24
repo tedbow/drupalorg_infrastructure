3434d350b1a373352fbc355e4d8d37e2ecd609eb
Neil Drumm <drumm@delocalizedham.com>
2015-04-23 17:04:59 -0700

    No more BZR on staging

--- staging/verify.sh
+++ staging/verify.sh
@@ -2,22 +2,12 @@
 . staging/common.sh 'verify'
 
 cd ${webroot}
-if [ -d .bzr ]; then
- bzr log -r-1 -n0 > ${WORKSPACE}/version.txt
- bzr status > ${WORKSPACE}/status.txt
- bzr diff > ${WORKSPACE}/diff.txt
-else 
- git log --name-status HEAD^..HEAD > ${WORKSPACE}/version.txt
- git status > ${WORKSPACE}/status.txt
- git diff  > ${WORKSPACE}/diff.txt
-fi
+git log --name-status HEAD^..HEAD > ${WORKSPACE}/version.txt
+git status > ${WORKSPACE}/status.txt
+git diff  > ${WORKSPACE}/diff.txt
 
 # Make sure the site is up.
 test_site
 
 # Exit with error if there are changes.
-if [ -d .bzr ]; then
- exit $(cat ${WORKSPACE}/status.txt | wc -l)
-else 
- exit $(cat ${WORKSPACE}/diff.txt | wc -l)
-fi
+exit $(cat ${WORKSPACE}/diff.txt | wc -l)
