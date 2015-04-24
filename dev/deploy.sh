ae994658082cdc401146c92a2e207bddeb1a5e6b
Rudy Grigar <basic@drupal.org>
2015-04-24 14:49:44 -0700

    Verified this now works

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -130,7 +130,9 @@ ${drush} pm-disable paranoia
 ${drush} pm-disable beanstalkd
 
 # Link up the files directory
-ln -sfv /media/nfs/${fqdn} "${web_path}/htdocs/$(${drush} status | sed -ne 's/^ *File directory path *: *\([^ ]*\).*$/\1/p')"
+drupal_files="${web_path}/htdocs/$(${drush} status | sed -ne 's/^ *File directory path *: *\([^ ]*\).*$/\1/p')"
+[ -d ${drupal_files} ] && rm -rf ${drupal_files}
+ln -s /media/nfs/${fqdn} ${drupal_files}
 
 # Sync xhprof webapp directory
 rsync -av /usr/share/xhprof/ "${web_path}/xhprof/htdocs/"
