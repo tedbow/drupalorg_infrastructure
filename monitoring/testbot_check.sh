42f52f0e34f1078a951e0bd5b2825f172158fe1e
Mixologic <ryan+ddoto@association.drupal.org>
2015-04-23 17:22:53 -0700

    Fixing the slackbot integration to not leak the key.

--- monitoring/testbot_check.sh
+++ monitoring/testbot_check.sh
@@ -5,7 +5,12 @@
 # Exit immediately on uninitialized variable or error, and print each command.
 set -uex
 
+# .slackkeys contains a file like so:
+##!/bin/bash
+#ROOMKEY=/{HASH}/{HASH}/{HASH}
+source /usr/local/drupal-infrastructure/monitoring/.slackkeys
+
 cd /var/www/qa.drupal.org/htdocs
 TESTBOTS=`/usr/bin/drush sqlc < /usr/local/drupal-infrastructure/monitoring/testbot_check.sql |awk 'BEGIN{ORS="\\\\n";}NR > 1 {print "Client #" $4 ": " $5 " Idle for more than 1 hour. Last activity was : " $1,$2 " GMT"} '`
 
-curl -X POST --data-urlencode 'payload={"channel": "#testbots", "username": "PIFR the pufferfish", "text": "'"${TESTBOTS}"'", "icon_emoji": ":puffer:"}' https://hooks.slack.com/services/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
+curl -X POST --data-urlencode 'payload={"channel": "#testbots", "username": "PIFR the pufferfish", "text": "'"${TESTBOTS}"'", "icon_emoji": ":puffer:"}' https://hooks.slack.com/services/${ROOMKEY}
