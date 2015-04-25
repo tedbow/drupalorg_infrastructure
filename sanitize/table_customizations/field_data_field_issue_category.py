ec12966544fd728edb409bb7673f5e0e921f4932
Neil Drumm <drumm@delocalizedham.com>
2015-04-24 17:23:24 -0700

    Trim issue fields

---
+++ sanitize/table_customizations/field_data_field_issue_category.py
@@ -0,0 +1,22 @@
+import table_customizations
+
+class Field_Data_Field_Issue_Category(table_customizations.TableHandler):
+
+    def get_sql(self, column_names):
+        columns, srccolumns = self.field_handler.column_handler(column_names, self.table)
+        query = """
+          INSERT INTO {dest}.{table} ({columns})
+          SELECT {srccolumns}
+          FROM {source}.{table} {limit}
+          """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
+        if self.dataset == 'skeleton':
+            query = """
+              INSERT INTO {dest}.{table} ({columns})
+              SELECT {srccolumns}
+              FROM {source}.{table}
+                LEFT JOIN {source}.node n on n.nid = {source}.{table}.field_issue_changes_nid
+              WHERE n.type NOT IN ('project_issue','forum')
+                OR n.created >= (unix_timestamp() - 60*24*60*60)
+              {limit}
+            """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
+        return query
