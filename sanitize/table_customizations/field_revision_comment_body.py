a5d03198947029346ddfd329cbc7d6ffdba3b116
Neil Drumm <drumm@delocalizedham.com>
2015-04-27 14:05:03 -0700

    Clean up JOINs

--- sanitize/table_customizations/field_revision_comment_body.py
+++ sanitize/table_customizations/field_revision_comment_body.py
@@ -11,12 +11,12 @@ class Field_Revision_Comment_Body(table_customizations.TableHandler):
           """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         if self.dataset == 'skeleton':
             query = """
-             INSERT INTO {dest}.{table} ({columns})
-             SELECT {srccolumns}
-             FROM {source}.{table}
-               LEFT JOIN {source}.node n on n.nid = {source}.{table}.entity_id
-             WHERE {source}.n.type NOT IN ('project_issue','forum')
-               OR {source}.n.created >= (unix_timestamp() - 60*24*60*60)
-             {limit}
+              INSERT INTO {dest}.{table} ({columns})
+              SELECT {srccolumns}
+              FROM {source}.{table}
+              INNER JOIN {source}.comment c ON c.cid = {source}.{table}.entity_id AND c.status = 1
+              INNER JOIN {source}.node n ON n.nid = c.cid AND n.status = 1
+              WHERE {source}.n.type NOT IN ('project_issue','forum') OR {source}.n.created >= (unix_timestamp() - 60*24*60*60)
+              {limit}
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         return query
