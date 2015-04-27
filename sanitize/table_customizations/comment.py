36b9a6a1bebaf671462f69e937f0025705c8f0a9
Neil Drumm <drumm@delocalizedham.com>
2015-04-27 12:18:08 -0700

    Check comment.status column

--- sanitize/table_customizations/comment.py
+++ sanitize/table_customizations/comment.py
@@ -5,29 +5,19 @@ class Comment(table_customizations.TableHandler):
     def get_sql(self, column_names):
         columns, srccolumns = self.field_handler.column_handler(column_names, self.table)
         query = """
-          INSERT INTO
-            {dest}.{table} ({columns})
-          SELECT
-            {srccolumns}
-          FROM
-            {source}.{table} {limit}
+          INSERT INTO {dest}.{table} ({columns})
+          SELECT {srccolumns}
+          FROM {source}.{table}
+          WHERE {source}.{table}.status = 1
+          {limit}
           """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         if self.dataset == 'skeleton':
             query = """
-              INSERT INTO
-                {dest}.{table} ({columns})
-              SELECT
-                {srccolumns}
-              FROM
-                {source}.{table}
-              LEFT JOIN
-                {source}.node
-              ON
-                {source}.node.nid = {source}.{table}.nid
-              WHERE
-                {source}.node.type NOT IN ('project_issue','forum')
-              OR
-                {source}.node.created >= (unix_timestamp() - 60*24*60*60)
+              INSERT INTO {dest}.{table} ({columns})
+              SELECT {srccolumns}
+              FROM {source}.{table}
+              LEFT JOIN {source}.node ON {source}.node.nid = {source}.{table}.nid
+              WHERE {source}.{table}.status = 1 AND {source}.node.status = 1 AND ({source}.node.type NOT IN ('project_issue','forum') OR {source}.node.created >= (unix_timestamp() - 60*24*60*60))
               {limit}
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         return query
