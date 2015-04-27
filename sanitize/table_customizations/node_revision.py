d65bdd24fc417f184326fa3126c59e5ab60692c0
Neil Drumm <drumm@delocalizedham.com>
2015-04-27 14:06:48 -0700

    Check node.status

--- sanitize/table_customizations/node_revision.py
+++ sanitize/table_customizations/node_revision.py
@@ -5,28 +5,17 @@ class Node_Revision(table_customizations.TableHandler):
     def get_sql(self, column_names):
         columns, srccolumns = self.field_handler.column_handler(column_names, self.table)
         query = """
-          INSERT INTO
-            {dest}.{table} ({columns})
-          SELECT
-            {srccolumns}
-          FROM
-            {source}.{table} {limit}
+          INSERT INTO {dest}.{table} ({columns})
+          SELECT {srccolumns}
+          FROM {source}.{table}
+          {limit}
           """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         if self.dataset == 'skeleton':
             query = """
-              INSERT INTO
-                {dest}.{table} ({columns})
-              SELECT
-                {srccolumns}
-              FROM
-                {source}.{table}
-              INNER JOIN
-                {source}.node
-              ON
-                {source}.node.nid = {table}.nid
-              AND
-                {source}.node.vid = {table}.vid
-              WHERE
-                {source}.{table}.timestamp >= (unix_timestamp() - 60*24*60*60) {limit}
+              INSERT INTO {dest}.{table} ({columns})
+              SELECT {srccolumns}
+              FROM {source}.{table}
+              INNER JOIN {source}.node ON {source}.node.nid = {table}.nid AND {source}.node.vid = {table}.vid AND {source}.node.status = 1
+              WHERE {source}.{table}.timestamp >= (unix_timestamp() - 60*24*60*60) {limit}
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         return query
