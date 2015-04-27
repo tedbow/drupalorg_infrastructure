fc853fa76322950dead1c166886bf3d0411707d8
Neil Drumm <drumm@delocalizedham.com>
2015-04-27 12:15:15 -0700

    Check node.status column

--- sanitize/table_customizations/node.py
+++ sanitize/table_customizations/node.py
@@ -5,31 +5,22 @@ class Node(table_customizations.TableHandler):
     def get_sql(self, column_names):
         columns, srccolumns = self.field_handler.column_handler(column_names, self.table)
         query = """
-          INSERT INTO
-            {dest}.{table} ({columns})
-          SELECT
-            {srccolumns}
-          FROM
-            {source}.{table} {limit}
+          INSERT INTO {dest}.{table} ({columns})
+          SELECT {srccolumns}
+          FROM {source}.{table}
+          WHERE {source}.{table}.status = 1
+          {limit}
           """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         if self.dataset == 'skeleton':
             query = """
-              INSERT INTO
-                {dest}.{table} ({columns})
-              SELECT
-                {srccolumns}
-              FROM
-                {source}.{table}
-              WHERE
-                {source}.{table}.type NOT IN ('project_issue','forum')
-              OR
-              (
-                  (({source}.{table}.type = 'forum' AND {source}.{table}.nid IN (SELECT nid FROM {source}.comment))
-                OR
-                  ({source}.{table}.type = 'project_issue'))
-                AND
-                  {source}.{table}.created >= (unix_timestamp() - 60*24*60*60)
-              )
+              INSERT INTO {dest}.{table} ({columns})
+              SELECT {srccolumns}
+              FROM {source}.{table}
+              WHERE {source}.{table}.status = 1 AND ({source}.{table}.type NOT IN ('project_issue','forum')
+                OR (
+                  (({source}.{table}.type = 'forum' AND {source}.{table}.nid IN (SELECT nid FROM {source}.comment)) OR ({source}.{table}.type = 'project_issue'))
+                  AND {source}.{table}.created >= (unix_timestamp() - 60*24*60*60)
+                ))
               {limit}
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         return query
