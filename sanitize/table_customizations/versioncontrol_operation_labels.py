05a169a116c3f06664a2c465b253d3a281d13465
Neil Drumm <drumm@delocalizedham.com>
2015-04-26 22:33:19 -0700

    Fix JOINs

--- sanitize/table_customizations/versioncontrol_operation_labels.py
+++ sanitize/table_customizations/versioncontrol_operation_labels.py
@@ -14,7 +14,7 @@ class Versioncontrol_Operation_Labels(table_customizations.TableHandler):
               INSERT INTO {dest}.{table} ({columns})
               SELECT {srccolumns}
               FROM {source}.{table}
-                INNER JOIN {source}.versioncontrol_operations vo ON vo.vc_op_id = {source}.vc_op_id
+                INNER JOIN {source}.versioncontrol_operations vo ON vo.vc_op_id = {source}.{table}.vc_op_id
                 AND vo.author_date >= (unix_timestamp() - 60*24*60*60)
               {limit};
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
