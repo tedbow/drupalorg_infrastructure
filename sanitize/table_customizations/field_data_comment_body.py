4138590e5d458b2888111c94e5f9b9714802ff6e
Neil Drumm <drumm@delocalizedham.com>
2015-04-27 22:12:55 -0700

    Fix JOIN & code style

--- sanitize/table_customizations/field_data_comment_body.py
+++ sanitize/table_customizations/field_data_comment_body.py
@@ -16,7 +16,7 @@ class Field_Data_Comment_Body(table_customizations.TableHandler):
               FROM {source}.{table}
               INNER JOIN {source}.comment ON {source}.comment.cid = {source}.{table}.entity_id AND {source}.comment.status = 1
               INNER JOIN {source}.node ON {source}.node.nid = {source}.comment.nid AND {source}.node.status = 1
-                AND {source}.node.type NOT IN ('project_issue','forum') OR {source}.node.created >= (unix_timestamp() - 60*24*60*60)
+                AND ({source}.node.type NOT IN ('project_issue','forum') OR {source}.node.created >= (unix_timestamp() - 60*24*60*60))
               {limit}
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         return query
