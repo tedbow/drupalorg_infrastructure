d15af7560edddb94999a52fd342ebdc5761a8a0e
Neil Drumm <drumm@delocalizedham.com>
2015-04-27 22:04:58 -0700

    Update to match node_revision.py

--- sanitize/table_customizations/field_revision_body.py
+++ sanitize/table_customizations/field_revision_body.py
@@ -15,9 +15,14 @@ class Field_Revision_Body(table_customizations.TableHandler):
             INSERT INTO {dest}.{table} ({columns})
             SELECT {srccolumns}
             FROM {source}.{table}
-            INNER JOIN {source}.node_revision ON node_revision.vid = {table}.revision_id
-            INNER JOIN {source}.node ON node.nid = node_revision.nid AND node.vid = node_revision.vid AND {source}.node.status = 1
-            WHERE node_revision.timestamp >= (unix_timestamp() - 60*24*60*60)
+            INNER JOIN {source}.node ON {source}.node.nid = {source}.{table}.entity_id AND {source}.node.status = 1
+              AND ({source}.node.type NOT IN ('project_issue','forum')
+                OR (
+                  (({source}.node.type = 'forum' AND {source}.node.nid IN (SELECT nid FROM {source}.comment)) OR ({source}.node.type = 'project_issue'))
+                  AND {source}.node.created >= (unix_timestamp() - 60*24*60*60)
+                ))
+            INNER JOIN {source}.node_revision ON {source}.node_revision.vid = {source}.{table}.revision_id
+              AND {source}.node_revision.timestamp >= (unix_timestamp() - 60*24*60*60) OR {source}.node.vid = {source}.node_revision.vid
             {limit}
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         return query
--- sanitize/table_customizations/node_revision.py
+++ sanitize/table_customizations/node_revision.py
@@ -15,7 +15,7 @@ class Node_Revision(table_customizations.TableHandler):
               INSERT INTO {dest}.{table} ({columns})
               SELECT {srccolumns}
               FROM {source}.{table}
-              INNER JOIN {source}.node ON {source}.node.nid = {table}.nid AND {source}.node.status = 1
+              INNER JOIN {source}.node ON {source}.node.nid = {source}.{table}.nid AND {source}.node.status = 1
                 AND ({source}.node.type NOT IN ('project_issue','forum')
                   OR (
                     (({source}.node.type = 'forum' AND {source}.node.nid IN (SELECT nid FROM {source}.comment)) OR ({source}.node.type = 'project_issue'))
