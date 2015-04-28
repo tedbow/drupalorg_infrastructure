6d1868e55a59d266c6fd7b2dba7b3b5ed8eff942
Neil Drumm <drumm@delocalizedham.com>
2015-04-27 21:42:26 -0700

    Update to match node.py

--- sanitize/table_customizations/field_data_body.py
+++ sanitize/table_customizations/field_data_body.py
@@ -17,7 +17,11 @@ class Field_Data_Body(table_customizations.TableHandler):
             FROM {source}.{table}
             INNER JOIN {source}.node_revision ON {source}.node_revision.vid = {source}.{table}.revision_id
             INNER JOIN {source}.node ON {source}.node.nid = {source}.node_revision.nid AND {source}.node.vid = {source}.node_revision.vid AND {source}.node.status = 1
-            WHERE {source}.node_revision.timestamp >= (unix_timestamp() - 60*24*60*60)
+              AND ({source}.node.type NOT IN ('project_issue','forum')
+                OR (
+                  (({source}.node.type = 'forum' AND {source}.node.nid IN (SELECT nid FROM {source}.comment)) OR ({source}.node.type = 'project_issue'))
+                  AND {source}.node.created >= (unix_timestamp() - 60*24*60*60)
+                ))
             {limit}
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         return query
