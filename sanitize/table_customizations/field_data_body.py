1a192d7b0d9d690348441a84d25b423e66488aff
Neil Drumm <drumm@delocalizedham.com>
2015-04-28 09:32:18 -0700

    Simplify JOIN

--- sanitize/table_customizations/field_data_body.py
+++ sanitize/table_customizations/field_data_body.py
@@ -15,8 +15,7 @@ class Field_Data_Body(table_customizations.TableHandler):
             INSERT INTO {dest}.{table} ({columns})
             SELECT {srccolumns}
             FROM {source}.{table}
-            INNER JOIN {source}.node_revision ON {source}.node_revision.vid = {source}.{table}.revision_id
-            INNER JOIN {source}.node ON {source}.node.nid = {source}.node_revision.nid AND {source}.node.vid = {source}.node_revision.vid AND {source}.node.status = 1
+            INNER JOIN {source}.node ON {source}.node.nid = {source}.{table}.entity_id AND {source}.node.status = 1
               AND ({source}.node.type NOT IN ('project_issue','forum')
                 OR (
                   (({source}.node.type = 'forum' AND {source}.node.nid IN (SELECT nid FROM {source}.comment)) OR ({source}.node.type = 'project_issue'))
