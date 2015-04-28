d36179f134fb820c208c5dfd10eb6e084e870412
Neil Drumm <drumm@delocalizedham.com>
2015-04-27 21:56:00 -0700

    Get a more useful set of node revisions

--- sanitize/table_customizations/node_revision.py
+++ sanitize/table_customizations/node_revision.py
@@ -15,7 +15,13 @@ class Node_Revision(table_customizations.TableHandler):
               INSERT INTO {dest}.{table} ({columns})
               SELECT {srccolumns}
               FROM {source}.{table}
-              INNER JOIN {source}.node ON {source}.node.nid = {table}.nid AND {source}.node.vid = {table}.vid AND {source}.node.status = 1
-              WHERE {source}.{table}.timestamp >= (unix_timestamp() - 60*24*60*60) {limit}
+              INNER JOIN {source}.node ON {source}.node.nid = {table}.nid AND {source}.node.status = 1
+                AND ({source}.node.type NOT IN ('project_issue','forum')
+                  OR (
+                    (({source}.node.type = 'forum' AND {source}.node.nid IN (SELECT nid FROM {source}.comment)) OR ({source}.node.type = 'project_issue'))
+                    AND {source}.node.created >= (unix_timestamp() - 60*24*60*60)
+                  ))
+              WHERE {source}.{table}.timestamp >= (unix_timestamp() - 60*24*60*60) OR {source}.node.vid = {source}.{table}.vid
+              {limit}
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         return query
