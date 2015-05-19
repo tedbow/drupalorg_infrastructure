abdaa2381eb771ea3e3b67e3adcb55ed27c7cfde
Mixologic <ryan+ddoto@association.drupal.org>
2015-05-19 07:40:45 -0700

    Issue #2475039 Also skip gz files in filename list

--- stats/fastlydownload_parse.sh
+++ stats/fastlydownload_parse.sh
@@ -6,7 +6,7 @@ export TERM=dumb
 cd /data/logs/fastly/varnish-syslogs
 
 # Look for files that have not yet been processed, and ignore todays file.
-for filename in *; do
+for filename in *[^gz]; do
    if [ ! -f "/data/logs/fastly/downloadcounts/${filename%fastly}downloadcounts.csv" ] && [ $filename != $(date +%Y.%m.%d.fastly) ]; then
      /usr/local/drupal-infrastructure/stats/fastlycounts.awk $filename
    fi;
