d2aedbad42600e961e2549df2dee7ab544953828
Rudy Grigar <basic@drupal.org>
2015-06-29 13:52:10 -0700

    localize-7: add temporary pre and post update steps

--- staging/deploy.sh
+++ staging/deploy.sh
@@ -7,6 +7,11 @@ git pull
 
 # Clear caches, try updatedb.
 ${drush} cc all || true
+# Temporary D7 upgrade steps for localize
+if [ "${uri}" = "localize-7.staging.devdrupal.org" ]; then
+  . staging/localize_7.sh
+  localize_7_pre_update
+fi
 ${drush} updatedb --interactive
 ${drush} cc all
 
@@ -14,6 +19,8 @@ ${drush} cc all
 if [ "${uri}" = "assoc.staging.devdrupal.org" ]; then
   ${drush} cc drush
   ${drush} compile-templates
+elif [ "${uri}" = "localize-7.staging.devdrupal.org" ]; then
+  localize_7_post_update
 fi
 
 test_site
