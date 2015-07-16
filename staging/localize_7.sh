9c56b0f623039a476b08ea57c2d9591145d90492
Jakob Perry <japerry@45640.no-reply.drupal.org>
2015-07-15 18:29:51 -0700

    Add some more update processes to LDO migration

--- staging/localize_7.sh
+++ staging/localize_7.sh
@@ -10,8 +10,12 @@ function localize_7_post_update {
   # Set the flag for OG to have global group roles
   ${drush} variable-set og_7000_access_field_default_value 0
 
+  # Set the installation profile
+  ${drush} variable-set install_profile minimal
+  ${drush} en minimal
+
   # Enable required modules.
-  ${drush} en og_context og_ui migrate localizedrupalorg localizedrupalorg_groups localizedrupalorg_permissions localizedrupalorg_polls localizedrupalorg_stories localizedrupalorg_users localizedrupalorg_wikis
+  ${drush} en og og_context og_ui migrate localizedrupalorg localizedrupalorg_groups localizedrupalorg_permissions localizedrupalorg_polls localizedrupalorg_stories localizedrupalorg_users localizedrupalorg_wikis
 
   # Display a birdview of OG migration and migrate data.
   ${drush} ms
@@ -22,4 +26,12 @@ function localize_7_post_update {
 
   # Disable Migrate once migration is done.
   ${drush} dis migrate
+
+  # Enable Admin menu and related modules and disable seven theme
+  ${drush} en admin_menu adminimal adminimal_admin_menu
+  ${drush} dis seven
+
+  # Rebuild Registry and upgrade
+  ${drush} rr
+  ${drush} updb
 }
