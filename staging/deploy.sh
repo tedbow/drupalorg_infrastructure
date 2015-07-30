2395a6c9700865dee3fe397cd0e7425bc0402af1
Jakob Perry <japerry@45640.no-reply.drupal.org>
2015-07-30 09:35:18 -0700

    Remove localize steps from deployment phase

--- staging/deploy.sh
+++ staging/deploy.sh
@@ -7,11 +7,7 @@ git pull
 
 # Clear caches, try updatedb.
 ${drush} cc all || true
-# Temporary D7 upgrade steps for localize
-if [ "${uri}" = "localize-7.staging.devdrupal.org" ]; then
-  . /usr/local/drupal-infrastructure/staging/localize_7.sh
-  localize_7_pre_update
-fi
+
 ${drush} updatedb --interactive
 ${drush} cc all
 
@@ -19,8 +15,6 @@ ${drush} cc all
 if [ "${uri}" = "assoc.staging.devdrupal.org" ]; then
   ${drush} cc drush
   ${drush} compile-templates
-elif [ "${uri}" = "localize-7.staging.devdrupal.org" ]; then
-  localize_7_post_update
 fi
 
 test_site
