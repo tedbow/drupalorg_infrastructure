69dab257ba1dc11f3a81de1f51098bb2bfa2b152
Jakob Perry <japerry@45640.no-reply.drupal.org>
2015-08-05 11:57:33 -0700

    Updated LDO d7 migration script

--- staging/localize_7.sh
+++ staging/localize_7.sh
@@ -7,23 +7,48 @@ function localize_7_pre_update {
 }
 
 function localize_7_post_update {
-  # Set the installation profile
+  # Set the installation profile.
   ${drush} variable-set install_profile minimal
 
-  # Do not try getting projects from Drupal.org's DB.
-  ${drush} variable-delete l10n_server_connector_l10n_project_drupalorg_cron
-
-  # Enable required modules.
-  ${drush} en og og_context og_ui migrate migrate_ui diff l10n_community l10n_drupal l10n_drupal_rest l10n_groups l10n_packager l10n_pconfig l10n_remote l10n_server localizedrupalorg localizedrupalorg_groups localizedrupalorg_permissions localizedrupalorg_polls localizedrupalorg_stories localizedrupalorg_users localizedrupalorg_wikis
-
-  # Rebuild Registry
+  # Enable the post D7 upgrade modules.
+  ${drush} en og og_context og_ui migrate migrate_ui diff l10n_community l10n_drupal l10n_drupal_rest l10n_groups l10n_packager l10n_pconfig l10n_remote l10n_server localizedrupalorg
   ${drush} rr
+  ${drush} en admin_menu adminimal adminimal_admin_menu contextual
 
-  # Enable Admin menu and related modules and disable seven theme
-  ${drush} en admin_menu adminimal adminimal_admin_menu
-
-  # Setup localizedrupalorg module to be ready to be updated later
+  # run some post updates on the localizeddrupalorg module.
   (
     echo "UPDATE system SET schema_version = 7101 WHERE name='localizedrupalorg'";
-  ) | drush sql-cli
+  ) | ${drush} sql-cli
+
+  ${drush} updb --interactive
+  ${drush} cc all
+
+  # Prepare migrator
+  ${drush} cc drush
+  ${drush} ms --refresh
+
+  # migrate some things!
+  ${drush} mi OgMigrateAddFields
+  ${drush} mi ogmigrategroupl10n_group
+  ${drush} mi OgUiMigrateAddField
+  ${drush} mi oguipopulatefieldl10n_group
+
+  #migrate the rest
+  ${drush} mi OgMigrateOgurRoles
+  ${drush} mi OgMigrateUser
+  ${drush} mi OgMigrateOgur
+  ${drush} mi OgMigrateContent
+
+  #post user migrate LDO steps
+  ${drush} ldo-ogb
+  ${drush} ldo-mcd
+  ${drush} ldo-abr
+
+  # Add the features last, after migration
+  ${drush} en localizedrupalorg_groups localizedrupalorg_permissions localizedrupalorg_polls localizedrupalorg_stories localizedrupalorg_users localizedrupalorg_wikis
+  ${drush} ldo-fr
+
+  # Disable and uninstall migrate
+  ${drush} dis migrate_ui migrate
+  ${drush} pm-uninstall migrate_ui migrate
 }
