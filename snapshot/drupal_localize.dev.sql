2250559388468c0ea75bb0f7e3eddd5495b178a3
Rudy Grigar <basic@drupal.org>
2015-08-11 16:26:51 -0700

    localize: og doesnt have a nid column, nor does it have data, remove this sanitization

--- snapshot/drupal_localize.dev.sql
+++ snapshot/drupal_localize.dev.sql
@@ -15,7 +15,6 @@ DELETE files FROM files LEFT JOIN users ON files.uid = users.uid WHERE users.uid
 DELETE files FROM files INNER JOIN field_data_upload upload ON files.fid = upload.upload_fid LEFT JOIN node ON upload.entity_id = node.nid WHERE upload.upload_fid IS NULL;
 DELETE upload FROM field_data_upload upload LEFT JOIN node ON upload.entity_id = node.nid WHERE node.nid IS NULL;
 DELETE file_managed FROM file_managed LEFT JOIN users ON file_managed.uid = users.uid WHERE users.uid IS NULL;
-DELETE og FROM og LEFT JOIN node ON node.nid = og.nid WHERE node.nid IS NULL;
 DELETE og_users_roles_group FROM og_users_roles_group LEFT JOIN node ON node.nid = og_users_roles_group.gid WHERE node.nid IS NULL;
 DELETE f FROM field_data_upload AS f LEFT JOIN comment c ON f.entity_id = c.cid WHERE c.cid IS NULL;
 DELETE f FROM field_revision_upload AS f LEFT JOIN comment c ON f.entity_id = c.cid WHERE c.cid IS NULL;
