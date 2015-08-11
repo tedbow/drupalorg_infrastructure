d9793a50c2207fad860b4954b004b12c1bf5603f
Rudy Grigar <basic@drupal.org>
2015-08-11 15:22:14 -0700

    localize: add field_data_upload logic

--- snapshot/drupal_localize.dev.sql
+++ snapshot/drupal_localize.dev.sql
@@ -12,6 +12,8 @@ DELETE comment FROM comment LEFT JOIN node ON node.nid = comment.nid WHERE node.
 DELETE comment FROM comment LEFT JOIN users ON comment.uid = users.uid WHERE users.uid IS NULL;
 DELETE comment FROM comment LEFT JOIN comment c2 ON comment.pid = c2.cid WHERE c2.cid IS NULL AND comment.pid <> 0;
 DELETE files FROM files LEFT JOIN users ON files.uid = users.uid WHERE users.uid IS NULL;
+DELETE files FROM files INNER JOIN field_data_upload upload ON files.fid = upload.upload_fid LEFT JOIN node ON upload.entity_id = node.nid WHERE upload.upload_fid IS NULL;
+DELETE upload FROM field_data_upload upload LEFT JOIN node ON upload.entity_id = node.nid WHERE node.nid IS NULL;
 DELETE file_managed FROM file_managed LEFT JOIN users ON file_managed.uid = users.uid WHERE users.uid IS NULL;
 DELETE og FROM og LEFT JOIN node ON node.nid = og.nid WHERE node.nid IS NULL;
 DELETE og_users_roles_group FROM og_users_roles_group LEFT JOIN node ON node.nid = og_users_roles_group.gid WHERE node.nid IS NULL;
