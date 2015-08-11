1197523ac9cd8220a7b7cc0991997bea5166f69a
Neil Drumm <drumm@delocalizedham.com>
2015-08-11 14:30:39 -0400

    First pass at updating queries for D7

--- snapshot/drupal_localize.dev.sql
+++ snapshot/drupal_localize.dev.sql
@@ -5,15 +5,16 @@ TRUNCATE og_notifications;
 
 -- Get rid of unpublished/blocked nodes, users, comments and related data in other tables.
 DELETE FROM node WHERE status <> 1;
-DELETE FROM comments WHERE status <> 0;
+DELETE FROM comment WHERE status <> 0;
 DELETE node FROM node LEFT JOIN users ON node.uid = users.uid WHERE users.uid IS NULL;
 DELETE node_revisions FROM node_revisions LEFT JOIN node ON node.nid = node_revisions.nid WHERE node.nid IS NULL;
-DELETE comments FROM comments LEFT JOIN node ON node.nid = comments.nid WHERE node.nid IS NULL;
-DELETE comments FROM comments LEFT JOIN users ON comments.uid = users.uid WHERE users.uid IS NULL;
-DELETE comments FROM comments LEFT JOIN comments c2 ON comments.pid = c2.cid WHERE c2.cid IS NULL AND comments.pid <> 0;
+DELETE comment FROM comment LEFT JOIN node ON node.nid = comment.nid WHERE node.nid IS NULL;
+DELETE comment FROM comment LEFT JOIN users ON comment.uid = users.uid WHERE users.uid IS NULL;
+DELETE comment FROM comment LEFT JOIN comment c2 ON comment.pid = c2.cid WHERE c2.cid IS NULL AND comment.pid <> 0;
 DELETE files FROM files LEFT JOIN users ON files.uid = users.uid WHERE users.uid IS NULL;
 DELETE files FROM files INNER JOIN upload ON files.fid = upload.fid LEFT JOIN node ON upload.nid = node.nid WHERE upload.fid IS NULL;
 DELETE upload FROM upload LEFT JOIN node ON upload.nid = node.nid WHERE node.nid IS NULL;
+DELETE file_managed FROM file_managed LEFT JOIN users ON file_managed.uid = users.uid WHERE users.uid IS NULL;
 DELETE og FROM og LEFT JOIN node ON node.nid = og.nid WHERE node.nid IS NULL;
 DELETE og_ancestry FROM og_ancestry LEFT JOIN node ON node.nid = og_ancestry.nid WHERE node.nid IS NULL;
 DELETE og_ancestry FROM og_ancestry LEFT JOIN node ON node.nid = og_ancestry.group_nid WHERE node.nid IS NULL;
