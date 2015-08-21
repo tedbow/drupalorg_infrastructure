753ab1cfa6a82f75c05ba94af0fe8d2a583e7b93
Neil Drumm <drumm@delocalizedham.com>
2015-08-21 10:10:55 -0400

    Fix mixup

--- sanitize/table_customizations/pift_ci_job.py
+++ sanitize/table_customizations/pift_ci_job.py
@@ -1,6 +1,6 @@
 import table_customizations
 
-class Pift_Ci_Job_Result(table_customizations.TableHandler):
+class Pift_Ci_Job(table_customizations.TableHandler):
 
     def get_sql(self, column_names):
         columns, srccolumns = self.field_handler.column_handler(column_names, self.table)
@@ -14,7 +14,6 @@ class Pift_Ci_Job_Result(table_customizations.TableHandler):
               INSERT INTO {dest}.{table} ({columns})
               SELECT {srccolumns}
               FROM {source}.{table}
-              INNER JOIN {source}.pift_ci_job ON {source}.pift_ci_job.job_id = {source}.{table}.job_id
-              AND {source}.pift_ci_job.created >= (unix_timestamp() - 30*24*60*60)
+              WHERE {source}.{table}.created >= (unix_timestamp() - 30*24*60*60)
             """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
         return query
