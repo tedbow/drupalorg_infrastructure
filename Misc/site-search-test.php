e1713c23d4c894f2f56c6c079c9047db5fd7c505
Neil Drumm <drumm@delocalizedham.com>
2015-09-10 12:45:20 -0400

    Add a script for analyzing search configurations' effect on results

---
+++ Misc/site-search-test.php
@@ -0,0 +1,122 @@
+<?php
+
+$environments = array('solr_5', 'solr_5_0');
+$searches = array(
+  // 'search keys' => array(
+  //   'result id' => 'flair',
+  // ),
+  // Flairs:
+  // '✓' Great result
+  // '!' Bad result
+  'rules' => array(
+    'e7ikco/node/190124' => '✓', // Module
+    'e7ikco/node/298476' => '✓', // Documentation
+  ),
+  'mediacurrent' => array(
+    'e7ikco/node/1125004' => '✓', // Organization
+  ),
+  'acquia' => array(
+    'e7ikco/node/1204416' => '✓', // Organization
+  ),
+  'coding standards' => array(
+    'e7ikco/node/318' => '✓', // Documentation
+  ),
+  'draggableviews' => array(
+    'e7ikco/node/283087' => '✓', // Module
+    'e7ikco/node/283498' => '✓', // Documentation
+  ),
+  'zen' => array(
+    'e7ikco/node/88566' => '✓', // Theme
+    'e7ikco/node/193318' => '✓', // Documentation
+  ),
+  'tag1' => array(
+    'e7ikco/node/1762646' => '✓', // Organization
+  ),
+  'views' => array(
+    'e7ikco/node/1912118' => '✓', // Documentation
+    'e7ikco/node/38878' => '✓', // Module
+    'e7ikco/node/785504' => '!', // Placeholder
+  ),
+  'apachesolr' => array(
+    'e7ikco/node/204268' => '✓', // Module
+  ),
+  'apache solr' => array(
+    'e7ikco/node/204268' => '✓', // Module
+  ),
+  'installation guide' => array(
+    'e7ikco/node/251019' => '✓', // Documentation
+  ),
+  'glossary' => array(
+    'e7ikco/node/937' => '✓', // Documentation
+  ),
+  'media' => array(
+    'e7ikco/node/19304' => '✓', // Module
+  ),
+  'redirect' => array(
+    'e7ikco/node/3287' => '✓', // Module
+    'e7ikco/node/80382' => '✓', // Module
+  ),
+  'xml sitemap' => array(
+    'e7ikco/node/190839' => '✓', // Module
+  ),
+  'ctools' => array(
+    'e7ikco/node/343333' => '✓', // Module
+    'e7ikco/node/2179357' => '!', // Sandbox
+  ),
+  'drupal geeks' => array(
+    'e7ikco/node/2013897' => '✓', // Organization
+  ),
+);
+
+$stdout = fopen('php://stdout', 'w');
+foreach ($searches as $keys => $notes) {
+  drupal_static_reset();
+  $output = array();
+  $search_page = apachesolr_search_page_load('core_search');
+  $search_page['settings']['apachesolr_search_per_page'] = 100;
+  $conditions = apachesolr_search_conditions_default($search_page);
+
+  foreach (apachesolr_search_search_results($keys, $conditions, $search_page) as $n => $result) {
+    $output[$result['fields']['id']] = array(
+      '' => isset($notes[$result['fields']['id']]) ? $notes[$result['fields']['id']] : '',
+      'title' => $result['title'],
+      'bundle' => $result['bundle'],
+      'link' => $result['link'],
+      'score' => $result['score'],
+      'score_old' => NULL,
+      'score_delta' => NULL,
+      'rank' => $n + 1,
+      'rank_old' => NULL,
+      'rank_delta' => NULL,
+    );
+  }
+
+  // Swap environment.
+  $search_page['env_id'] = ($search_page['env_id'] === $environments[0]) ? $environments[1] : $environments[0];
+
+  foreach (apachesolr_search_search_results($keys, $conditions, $search_page) as $n => $result) {
+    if (isset($output[$result['fields']['id']])) {
+      $output[$result['fields']['id']]['score_old'] = $result['score'];
+      $output[$result['fields']['id']]['score_delta'] = number_format($output[$result['fields']['id']]['score'] - $result['score'], 7);
+      $output[$result['fields']['id']]['rank_old'] = $n + 1;
+      $output[$result['fields']['id']]['rank_delta'] = $output[$result['fields']['id']]['rank'] - ($n + 1);
+    }
+  }
+
+  if (!isset($header_shown)) {
+    $row = reset($output);
+    fputcsv($stdout, array_keys($row));
+    $header_shown = TRUE;
+  }
+
+  fputcsv($stdout, array(
+    '→',
+    $keys,
+    NULL,
+    url('search/site/' . $keys, array('absolute' => TRUE)),
+  ));
+  foreach ($output as $row) {
+    fputcsv($stdout, $row);
+  }
+  print "\n";
+}
