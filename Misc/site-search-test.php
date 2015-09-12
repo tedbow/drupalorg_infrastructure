af7e08511c89387b2506ad4943c0990f0eafcc04
Neil Drumm <drumm@delocalizedham.com>
2015-09-12 13:16:35 -0400

    Update site hash

--- Misc/site-search-test.php
+++ Misc/site-search-test.php
@@ -10,75 +10,75 @@ $searches = array(
   // '~' Okay result
   // '!' Bad result
   'coding standards' => array(
-    'e7ikco/node/318' => '✓', // Documentation
-    'e7ikco/node/2465321' => '~', // Project
+    'sh0zn1/node/318' => '✓', // Documentation
+    'sh0zn1/node/2465321' => '~', // Project
   ),
   'installation guide' => array(
-    'e7ikco/node/251019' => '✓', // Documentation
+    'sh0zn1/node/251019' => '✓', // Documentation
   ),
   'glossary' => array(
-    'e7ikco/node/937' => '✓', // Documentation
+    'sh0zn1/node/937' => '✓', // Documentation
   ),
   'rules' => array(
-    'e7ikco/node/190124' => '✓', // Module
-    'e7ikco/node/298476' => '✓', // Documentation
+    'sh0zn1/node/190124' => '✓', // Module
+    'sh0zn1/node/298476' => '✓', // Documentation
   ),
   'draggableviews' => array(
-    'e7ikco/node/283087' => '✓', // Module
-    'e7ikco/node/283498' => '✓', // Documentation
+    'sh0zn1/node/283087' => '✓', // Module
+    'sh0zn1/node/283498' => '✓', // Documentation
   ),
   'zen' => array(
-    'e7ikco/node/88566' => '✓', // Theme
-    'e7ikco/node/193318' => '✓', // Documentation
-    'e7ikco/node/1072403' => '!', // Unsupported
-    'e7ikco/node/485812' => '!', // Unsupported
+    'sh0zn1/node/88566' => '✓', // Theme
+    'sh0zn1/node/193318' => '✓', // Documentation
+    'sh0zn1/node/1072403' => '!', // Unsupported
+    'sh0zn1/node/485812' => '!', // Unsupported
   ),
   'views' => array(
-    'e7ikco/node/1912118' => '✓', // Documentation
-    'e7ikco/node/38878' => '✓', // Module
-    'e7ikco/node/785504' => '!', // Placeholder
+    'sh0zn1/node/1912118' => '✓', // Documentation
+    'sh0zn1/node/38878' => '✓', // Module
+    'sh0zn1/node/785504' => '!', // Placeholder
   ),
   'apachesolr' => array(
-    'e7ikco/node/204268' => '✓', // Module
+    'sh0zn1/node/204268' => '✓', // Module
   ),
   'apache solr' => array(
-    'e7ikco/node/204268' => '✓', // Module
+    'sh0zn1/node/204268' => '✓', // Module
   ),
   'media' => array(
-    'e7ikco/node/19304' => '✓', // Module
-    'e7ikco/node/2358115' => '✓', // Resource guide
+    'sh0zn1/node/19304' => '✓', // Module
+    'sh0zn1/node/2358115' => '✓', // Resource guide
   ),
   'redirect' => array(
-    'e7ikco/node/3287' => '✓', // Module
-    'e7ikco/node/80382' => '✓', // Module
+    'sh0zn1/node/3287' => '✓', // Module
+    'sh0zn1/node/80382' => '✓', // Module
   ),
   'xml sitemap' => array(
-    'e7ikco/node/190839' => '✓', // Module
+    'sh0zn1/node/190839' => '✓', // Module
   ),
   'ctools' => array(
-    'e7ikco/node/343333' => '✓', // Module
-    'e7ikco/node/2179357' => '!', // Sandbox
+    'sh0zn1/node/343333' => '✓', // Module
+    'sh0zn1/node/2179357' => '!', // Sandbox
   ),
   'core' => array(
-    'e7ikco/node/3060' => '✓', // Drupal core
+    'sh0zn1/node/3060' => '✓', // Drupal core
   ),
   'drupal core' => array(
-    'e7ikco/node/3060' => '✓', // Drupal core
+    'sh0zn1/node/3060' => '✓', // Drupal core
   ),
   'drupal' => array(
-    'e7ikco/node/3060' => '✓', // Drupal core
+    'sh0zn1/node/3060' => '✓', // Drupal core
   ),
   'tag1' => array(
-    'e7ikco/node/1762646' => '✓', // Organization
+    'sh0zn1/node/1762646' => '✓', // Organization
   ),
   'mediacurrent' => array(
-    'e7ikco/node/1125004' => '✓', // Organization
+    'sh0zn1/node/1125004' => '✓', // Organization
   ),
   'acquia' => array(
-    'e7ikco/node/1204416' => '✓', // Organization
+    'sh0zn1/node/1204416' => '✓', // Organization
   ),
   'drupal geeks' => array(
-    'e7ikco/node/2013897' => '✓', // Organization
+    'sh0zn1/node/2013897' => '✓', // Organization
   ),
 );
 
