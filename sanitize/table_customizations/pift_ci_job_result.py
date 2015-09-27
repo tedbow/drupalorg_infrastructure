aa23a05b509bb9b456ad3727fdfad872f1c233a2
Neil Drumm <drumm@delocalizedham.com>
2015-09-27 16:33:23 -0400

    Issue #2575797: Revert changes since 94adf1c, pift results are not actually too large and are really needed

---
+++ sanitize/table_customizations/pift_ci_job_result.py
@@ -0,0 +1,20 @@
+import table_customizations
+
+class Pift_Ci_Job_Result(table_customizations.TableHandler):
+
+    def get_sql(self, column_names):
+        columns, srccolumns = self.field_handler.column_handler(column_names, self.table)
+        query = """
+          INSERT INTO {dest}.{table} ({columns})
+          SELECT {srccolumns}
+          FROM {source}.{table}
+          """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
+        if self.dataset == 'skeleton':
+            query = """
+              INSERT INTO {dest}.{table} ({columns})
+              SELECT {srccolumns}
+              FROM {source}.{table}
+              INNER JOIN {source}.pift_ci_job ON {source}.pift_ci_job.job_id = {source}.{table}.job_id
+              AND {source}.pift_ci_job.created >= (unix_timestamp() - 20*24*60*60)
+            """.format(table=self.table, dest=self.dst, source=self.src, columns=columns, srccolumns=srccolumns, limit=self.limit)
+        return query
