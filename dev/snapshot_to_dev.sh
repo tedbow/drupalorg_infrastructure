5a66d2f3d303eb156b05529855cf1f15e4c534bd
Archie Brentano <isntall.us@gmail.com>
2015-10-08 18:01:37 -0700

    CD-ing into the WORKSPACE dir

--- dev/snapshot_to_dev.sh
+++ dev/snapshot_to_dev.sh
@@ -4,4 +4,5 @@
 rsync -v --copy-links --password-file ~/util.rsync.pass "rsync://devmysql@dbutil.drupal.org/mysql-dev/*_database_snapshot.dev-current.sql.bz2" "${WORKSPACE}"
 
 ### Build the docker container
+cd ${WORKSPACE}
 /usr/local/dev_infrastructure_containers/build.sh
