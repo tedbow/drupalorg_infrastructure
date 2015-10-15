f08a27b9313852c33dc903b70f3715b95d7a1599
Archie Brentano <isntall.us@gmail.com>
2015-10-14 17:11:17 -0700

    adding docker image cleanup script

---
+++ dev/docker-image-cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Image clean up
+###
+# Get all devwww images
+alldevwwwimages=$(docker images --no-trunc | grep devwww | awk '{print $3}' | sort -u)
+# Get all latest devwww images
+latestdevwwwimages=$(docker images --no-trunc | grep devwww | grep latest | awk '{print $3}' | sort -u)
+# Get all devwww images that are being used by containers
+devwwwcontainers=$(docker inspect --format '{{ .Image }}' $(docker ps -a | grep devwww | awk '{print $1}')  | sort -u)
+# List of images to keep: latest images and images in use
+keepdevwwwimages=$(sort -u <(echo "$devwwwcontainers") <(echo "$latestdevwwwimages"))
+# List of images that can be removed
+deletedevwwwimages=$(comm -2 -3 <(echo "$alldevwwwimages") <(echo "$keepdevwwwimages"))
+# remove images
+docker rmi $deletedevwwwimages
