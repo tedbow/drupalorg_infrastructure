9fb91882dcf4b1c6fb3dcf50727f1c68020e8384
Archie Brentano <isntall.us@gmail.com>
2015-11-20 11:23:25 -0800

    by isntall: adding sudo for drop cache and fixing scriptdir pathing

--- docker/db-images/db-image-concurrent-build.sh
+++ docker/db-images/db-image-concurrent-build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-
 DUMPSDIR="/var/dumps"
+export SCRIPTDIR="/usr/local/drupal-infrastructure/docker/db-images"
 ## Strings to ignore, like the raw dumps
 IGNORE="raw|qa|latinamerica2015|association_civicrm"
 DBSTRING="sql"
 CONCURRENCY="3"
-export SCRIPTDIR=$(pwd)
 export DATE=$(date +'%Y%m%d%H%M')
 
 
 echo "Drop linux caches"
-echo 3 > /proc/sys/vm/drop_caches
+sudo bash -c 'echo 3 > /proc/sys/vm/drop_caches'
 
 cd ${DUMPSDIR}
 find -type l | grep -Ev ${IGNORE} | grep ${DBSTRING} | awk -F'/' '{print $2 " " $3}' | xargs -P ${CONCURRENCY} -I {} -i bash -c  '${SCRIPTDIR}/db-image-build.sh ${DATE} {}'
