2a79c1593887208a997f70e4eec7717dedf59bd7
Archie Brentano <isntall.us@gmail.com>
2015-11-23 10:11:32 -0800

    by isntall: define which level of db-image to build

--- docker/db-images/db-image-concurrent-build.sh
+++ docker/db-images/db-image-concurrent-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DUMPSDIR="/var/dumps"
+export SANLEVEL="${1}"
 export SCRIPTDIR="/usr/local/drupal-infrastructure/docker/db-images"
 ## Strings to ignore, like the raw dumps
 IGNORE="raw|qa|latinamerica2015|association_civicrm|tmp"
@@ -12,5 +13,5 @@ export DATE=$(date +'%Y%m%d%H%M')
 echo "Drop linux caches"
 sudo bash -c 'echo 3 > /proc/sys/vm/drop_caches'
 
-cd ${DUMPSDIR}
-find -type l | grep -Ev ${IGNORE} | grep ${DBSTRING} | awk -F'/' '{print $2 " " $3}' | xargs -P ${CONCURRENCY} -I {} -i bash -c  '${SCRIPTDIR}/db-image-build.sh ${DATE} {}'
+cd ${DUMPSDIR}/${SANLEVEL}
+find -type l | grep -Ev ${IGNORE} | grep ${DBSTRING} | awk -F'/' '{print $2}' | xargs -P ${CONCURRENCY} -I {} -i bash -c  '${SCRIPTDIR}/db-image-build.sh ${DATE} ${SANLEVEL} {}'
