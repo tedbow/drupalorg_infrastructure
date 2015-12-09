d5e127fdfdf455e3dd22ff989c8d900c348d42db
Archie Brentano <isntall.us@gmail.com>
2015-12-09 11:47:08 -0800

    by isntall: refinine docker-image-cleanup.sh after moving image build to dbutil

--- dev/docker-image-cleanup.sh
+++ dev/docker-image-cleanup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 ## Image clean up
+## Assumptions, an image:
+##  is kept only if in use or latest
+##  has one tag, unless it is the latest
+##  is built on another system and loaded
+
 # Get all dev images
-alldevimages=$(docker images --no-trunc | grep dev | awk '{print $3}' | sort -u)
+alldevimages=$(docker images -q | sort -u)
 # Get all latest dev images
-latestdevimages=$(docker images --no-trunc | grep dev | grep latest | awk '{print $3}' | sort -u)
+latestdevimages=$(docker images | grep dev | grep latest | awk '{print $3}' | sort -u)
 # Get all dev images that are being used by containers
-devcontainers=$(docker inspect --format '{{ .Image }}' $(docker ps -a | grep dev | awk '{print $1}')  | sort -u)
-# List of images to keep: latest images and images in use
-keepdevimages=$(sort -u <(echo "$devcontainers") <(echo "$latestdevimages"))
+containerimageid=$(docker ps -a | grep -v "CONTAINER ID" | awk '{print $2}' | sort -u)
+# List of images to keep: latest images and images in use, do not print empty line
+keepdevimages=$(sort -u <( [ ! -z "$containerimageid" ] && echo "$containerimageid") <( [ ! -z "$latestdevimages" ] && echo "$latestdevimages"))
 # List of images that can be removed
 unuseddevimages=$(comm -2 -3 <(echo "$alldevimages") <(echo "$keepdevimages"))
+# Test if there are images to delete
 # list images that will be removed
-echo "$unuseddevimages"
-# remove images
-docker rmi $unuseddevimages
+# Remove images if there are images to delete
+[ ! -z $unuseddevimages ] && echo "Images to delete $unuseddevimages" && docker rmi $unuseddevimages || echo "No image to remove"
