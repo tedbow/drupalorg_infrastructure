2f0e40f14dfc93689f4aeeacf47e418440c1fece
Rudy Grigar <basic@drupal.org>
2016-01-06 13:16:10 -0800

    snapshot_to_dev: use whole-file

--- dev/snapshot_to_dev.sh
+++ dev/snapshot_to_dev.sh
@@ -5,7 +5,7 @@ SNAPSHOTPATH=/var/www/docker-images/
 mkdir -p ${SNAPSHOTPATH}
 
 ## Get the DB snapshots
-rsync -vr --password-file ~/util.rsync.pass  --exclude-from="./dev/db-exclusions.txt" --delete --delete-excluded "rsync://devmysql@dbutil.drupal.org/mysql-dev/*_database_snapshot.dev-*.image.tar.bz2" "${SNAPSHOTPATH}"
+rsync -vr --whole-file --password-file ~/util.rsync.pass  --exclude-from="./dev/db-exclusions.txt" --delete --delete-excluded "rsync://devmysql@dbutil.drupal.org/mysql-dev/*_database_snapshot.dev-*.image.tar.bz2" "${SNAPSHOTPATH}"
 
 ## Build the docker container
 cd ${SNAPSHOTPATH}
