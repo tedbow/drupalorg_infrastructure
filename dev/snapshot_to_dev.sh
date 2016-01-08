3e3d69f8fb550251a744928a37f9c4755f566d74
Rudy Grigar <basic@drupal.org>
2016-01-08 02:35:44 -0800

    Don't bother creating this

--- dev/snapshot_to_dev.sh
+++ dev/snapshot_to_dev.sh
@@ -2,7 +2,6 @@
 set -uex
 
 SNAPSHOTPATH=/var/lib/docker/docker-images/
-mkdir -p ${SNAPSHOTPATH}
 
 ## Get the DB snapshots
 rsync -vr --whole-file --password-file ~/util.rsync.pass  --exclude-from="./dev/db-exclusions.txt" --delete --delete-excluded "rsync://devmysql@dbutil.drupal.org/mysql-dev/*_database_snapshot.dev-*.image.tar.bz2" "${SNAPSHOTPATH}"
