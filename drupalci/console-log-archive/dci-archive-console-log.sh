4d65bb22bb17cc6190e8a03ae61369b99b6af37f
Archie Brentano <isntall.us@gmail.com>
2016-02-18 09:10:27 -0800

    dci: streamline tweaks of archival scipts

--- drupalci/console-log-archive/dci-archive-console-log.sh
+++ drupalci/console-log-archive/dci-archive-console-log.sh
@@ -1,19 +1,24 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
-CREDS="${1}"
-JOBNAME="${2}"
+export CREDS="${1}"
+export JOBNAME="${2}"
+#export URL="http://dispatcher-origin.drupalci.aws:8080"
+export URL="http://staging-dispatcher-origin.drupalci.aws:8080"
+export S3BUCKET="dci-console-log"
 
-URL="https://dispatcher.drupalci.org"
-S3BUCKET="dci-console-log"
-
-JOBPATH="job/${JOBNAME}"
-URI="$JOBPATH/api/json?tree=builds[id,timestamp]"
-DATESTAMP="$(date +'%Y.%m.%d')"
-idFILE="dci-archive-id-${DATESTAMP}.txt"
-FOLDER="${DATESTAMP}"
-COMPRESSEDFILE="${FOLDER}.tar.lrz"
+export JOBPATH="job/${JOBNAME}"
+export URI="$JOBPATH/api/json?tree=allBuilds[id,timestamp]"
+export DATESTAMP="$(date +'%Y.%m.%d.%H%M')"
+export idFILE="dci-id-${DATESTAMP}.txt"
+export FOLDER="${DATESTAMP}"
+export idARCHIVE="${FOLDER}/archive-${idFILE}"
+export COMPRESSEDFILE="${FOLDER}.tgz"
+# Trying to use a variable in the date command wasn't wokring well
+export XDAYEPOCH=$(date -v-90d +"%s")
+export XDAYEPOCHMILI="$((${XDAYEPOCH} * 1000))"
 
 # Get json from jenkins, decode it and put into temp file
+echo "Getting the list of all builds for ${JOBNAME}"
 curl --silent --globoff -u ${CREDS} "$URL/${URI}" | php ./dci-decode-json.php > ./${idFILE}
 
 # Create a temporary folder for the consoletext to live
@@ -22,22 +27,36 @@ if [[ ! -d ./${FOLDER} ]]; then
 fi
 
 # Get the various consoleText and save to file
-for i in $(cat ./${idFILE}); do
-  echo "Downloading consoleText of job #${i}"
-  curl --silent ${URL}/${JOBPATH}/${i}/consoleText > ${FOLDER}/${i}-consoleText.txt;
+echo "Parsing builds to archive"
+cat ./${idFILE} | while read i; do
+  buildTIMESTAMP="$(echo ${i} | awk '{print $2}')"
+  if [[ "${buildTIMESTAMP}" -lt "${XDAYEPOCHMILI}" ]]; then
+    buildID=$(echo ${i} | awk '{print $1}')
+    echo "${buildID}" >> ./${idARCHIVE}
+  fi
 done;
 
-lrztar -o ./${COMPRESSEDFILE} ./${FOLDER}
+startID=$(tail -n 1 ./${idARCHIVE})
+endID=$(head -n 1 ./${idARCHIVE})
 
+echo "Downloading build consoleText for IDs from ${startID} to ${endID}"
+cat ./${idARCHIVE} | xargs -P 4 -I {} bash -c 'curl --silent ${URL}/${JOBPATH}/{}/consoleText > ${FOLDER}/{}-consoleText.txt'
+
+echo "Compressing consoleText"
+tar zcf ./${COMPRESSEDFILE} ./${FOLDER}
+
+echo "Uploading ${COMPRESSEDFILE}"
 aws s3 cp  ./${COMPRESSEDFILE} s3://${S3BUCKET}/${JOBPATH}/
 
 # Verify compressed file is in S3 and then delete job builds
 if [[ $(aws s3 ls s3://${S3BUCKET}/${JOBPATH}/${COMPRESSEDFILE}) ]]; then
-  for i in $(cat ./${idFILE}); do
-    echo "Deleting build #${i}"
-    curl -u ${CREDS} -X POST ${URL}/${JOBPATH}/${i}/doDelete;
-  done;
+  echo "Deleting build consoleText for IDs from ${startID} to ${endID}"
+  cat ./${idARCHIVE} | xargs -P 2 -I {} bash -c 'curl  -u ${CREDS} -X POST ${URL}/${JOBPATH}/{}/doDelete'
 fi
 
-echo "rm -rf ./${idFILE} ${FOLDER} ${COMPRESSEDFILE}"
-rm -rf ./${idFILE} ${FOLDER} ${COMPRESSEDFILE}
+echo "Total number of builds $(cat ./${idFILE} | wc -l)"
+echo "Number of builds that were archived $(cat ./${idARCHIVE} | wc -l)"
+du -sh *
+
+echo "rm -rf ${idFILE} ${FOLDER} ${COMPRESSEDFILE}"
+rm -rf ${idFILE} ${FOLDER} ${COMPRESSEDFILE}
