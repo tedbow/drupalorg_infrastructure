1d97242decbd324ca9d73f295106951685d164f3
Archie Brentano <isntall.us@gmail.com>
2016-03-21 15:37:14 -0700

    Revert "devwww2: changing arguements to work with java aws cli"
    This reverts commit 5c0603062c4a3f15f354059ef12855d542fb570f.

--- dev/aws_trigger_ebs_snapshots.sh
+++ dev/aws_trigger_ebs_snapshots.sh
@@ -5,19 +5,19 @@ DATE=$(date +'%Y%m%d%H%M')
 DESCRIPTOR="auto_devwww2_/dev"
 
 # devwww2-root
-create_ebs_snapshot -d "${DATE}_${DESCRIPTOR}/sda1" 'vol-8bc83a6d'
+create_ebs_snapshot 'vol-8bc83a6d' "${DATE}_${DESCRIPTOR}/sda1"
 
 # devwww2-www
-create_ebs_snapshot -d "${DATE}_${DESCRIPTOR}/sdg" 'vol-2f36c4c9'
+create_ebs_snapshot 'vol-2f36c4c9' "${DATE}_${DESCRIPTOR}/sdg"
 
 # devwww2-dockerlvm 20160316
-create_ebs_snapshot -d "${DATE}_${DESCRIPTOR}/sdi" 'vol-cf09b976'
+create_ebs_snapshot 'vol-cf09b976' "${DATE}_${DESCRIPTOR}/sdi"
 
 # devwww2-legacy-mysql
-create_ebs_snapshot -d "${DATE}_${DESCRIPTOR}/sdj" 'vol-242efbc2'
+create_ebs_snapshot 'vol-242efbc2' "${DATE}_${DESCRIPTOR}/sdj"
 
 # devwww2-btrfs-media
-create_ebs_snapshot -d "${DATE}_${DESCRIPTOR}/sdk" 'vol-40f327a6'
+create_ebs_snapshot 'vol-40f327a6' "${DATE}_${DESCRIPTOR}/sdk"
 
 # devwww2 dumps
-create_ebs_snapshot -d "${DATE}_${DESCRIPTOR}/sdl" 'vol-2befd3cd'
+create_ebs_snapshot 'vol-2befd3cd' "${DATE}_${DESCRIPTOR}/sdl"
