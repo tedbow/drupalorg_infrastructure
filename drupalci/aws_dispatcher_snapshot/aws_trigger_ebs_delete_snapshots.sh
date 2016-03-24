3f9a93dc00c1783e495c3babcff15ed25117db65
Archie Brentano <isntall.us@gmail.com>
2016-03-24 11:21:31 -0700

    full paths are helpful

--- drupalci/aws_dispatcher_snapshot/aws_trigger_ebs_delete_snapshots.sh
+++ drupalci/aws_dispatcher_snapshot/aws_trigger_ebs_delete_snapshots.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-source /usr/local/drupal-infrastructure/drupalci/aws_dispatcher_snapshot
+source /usr/local/drupal-infrastructure/drupalci/aws_dispatcher_snapshot/aws_common.sh
 
 date31=$(date --date="31 days ago" +'%Y%m%d%H%M')
 
