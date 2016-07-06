6560987753524ee43d5704b0eff0e63830d148b0
Rudy Grigar <basic@drupal.org>
2016-07-06 09:57:42 -0700

    Lets use 4 days

--- dev/aws_trigger_ebs_delete_snapshots.sh
+++ dev/aws_trigger_ebs_delete_snapshots.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 source /usr/local/drupal-infrastructure/dev/aws_common.sh
 
-date2=$(date --date="2 days ago" +'%Y%m%d%H%M')
+date4=$(date --date="4 days ago" +'%Y%m%d%H%M')
 
 descriptor="auto_devwww2_/dev"
 
-delete_old_ebs_snapshots "${date2}" "${descriptor}"
+delete_old_ebs_snapshots "${date4}" "${descriptor}"
