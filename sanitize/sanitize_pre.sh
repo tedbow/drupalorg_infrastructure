c805981f4d561bc35907ba0d825ebda036c9ba08
Rudy Grigar <basic@drupal.org>
2016-07-08 16:58:31 -0700

    Make a backup

--- sanitize/sanitize_pre.sh
+++ sanitize/sanitize_pre.sh
@@ -12,7 +12,7 @@ sudo systemctl stop puppet
 sudo systemctl status puppet || true
 time ssh bender@db6-reader-vip.drupal.org
 ## After copying the production data, take a backup before decompressing
-sudo /opt/puppetlabs/puppet/bin/backup perform --trigger mysql --config-file '/etc/backup/config.rb' --tmp-path /var/tmp/Backup/
+sudo /home/drupalbackup/drupalbackup /var/sanitize/drupal_sanitize/
 time innobackupex --parallel=2 --compress-threads=4 --decompress /var/sanitize/drupal_sanitize/
 time innobackupex --apply-log --use-memory=6G /var/sanitize/drupal_sanitize/
 sudo rm -rf /var/lib/mysql/
