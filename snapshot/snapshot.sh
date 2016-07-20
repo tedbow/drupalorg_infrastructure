d8babc80e55c311f0523e90267c9ac51653e3fe9
Rudy Grigar <basic@drupal.org>
2016-07-20 09:27:22 -0700

    Revert binary snapshots until we have more time to debug

--- snapshot/snapshot.sh
+++ snapshot/snapshot.sh
@@ -32,28 +32,6 @@ function snapshot {
   mysqldump --single-transaction --quick --max-allowed-packet=256M ${tmp_args} | sed -e 's/^) ENGINE=[^ ]*/) ROW_FORMAT=COMPRESSED/' | pbzip2 -p4 > "/var/dumps/${subdir}/${JOB_NAME}${suffix}-${BUILD_NUMBER}-in-progress.sql.bz2"
   mv -v "/var/dumps/${subdir}/${JOB_NAME}${suffix}-${BUILD_NUMBER}-in-progress.sql.bz2" "/var/dumps/${subdir}/${JOB_NAME}${suffix}-${BUILD_NUMBER}.sql.bz2"
   ln -sfv "${JOB_NAME}${suffix}-${BUILD_NUMBER}.sql.bz2" "/var/dumps/${subdir}/${JOB_NAME}${suffix}-current.sql.bz2"
-  # Create and save a binary snapshot.
-  if [ "${subdir}" != 'raw' ]; then
-    sudo rm -rf /var/sanitize/drupal_export/${subdir}/${db_name}
-    while [ -e /var/sanitize/drupal_export/.lock ]; do
-      sleep 60
-    done
-    touch /var/sanitize/drupal_export/.lock
-    sudo innobackupex --no-timestamp --databases="${db_name}" /var/sanitize/drupal_export/${subdir}/${db_name}
-    sudo chown -R bender:bender "/var/sanitize/drupal_export/${subdir}/${db_name}"
-    mysqldump --no-data --single-transaction --quick --max-allowed-packet=256M ${tmp_args} > "/var/sanitize/drupal_export/${subdir}/${db_name}/${db_name}.sql"
-    sudo innobackupex --apply-log --export "/var/sanitize/drupal_export/${subdir}/${db_name}"
-    sudo chown -R bender:bender "/var/sanitize/drupal_export/${subdir}/${db_name}"
-    rm /var/sanitize/drupal_export/.lock
-    tar -czvf "/var/dumps/${subdir}/${JOB_NAME}${suffix}-${BUILD_NUMBER}-binary.tar.gz" "/var/sanitize/drupal_export/${subdir}/${db_name}"
-    ln -sfv "${JOB_NAME}${suffix}-${BUILD_NUMBER}-binary.tar.gz" "/var/dumps/${subdir}/${JOB_NAME}${suffix}-binary-current.tar.gz"
-    # Don't forget me... remove old binary snapshots too.
-    old_snapshots=$(ls -t /var/dumps/${subdir}/${JOB_NAME}${suffix}-[0-9]*-binary.tar.gz | tail -n +2)
-    if [ -n "${old_snapshots}" ]; then
-      rm -v ${old_snapshots}
-    fi
-    sudo rm -rf /var/sanitize/drupal_export/${subdir}/${db_name}
-  fi
 
   # Remove old snapshots.
   old_snapshots=$(ls -t /var/dumps/${subdir}/${JOB_NAME}${suffix}-[0-9]*.sql.{bz2,gz} | tail -n +2)
