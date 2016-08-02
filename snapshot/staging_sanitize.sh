84428835410d86c6d72a9fe76b9fa85c919e03bc
Rudy Grigar <basic@drupal.org>
2016-08-01 20:58:51 -0700

    Move compression into the staging sanitization steps to avoid compressing throw away data

--- snapshot/staging_sanitize.sh
+++ snapshot/staging_sanitize.sh
@@ -8,7 +8,6 @@ set -uex
 # Generate a list of all databases to be sanitized
 for db in $(sudo mysql -N -B -e 'SHOW DATABASES' | grep -v -e 'jira_assoc' -e 'information_schema' -e 'performance_schema' -e 'mysql' -e 'percona' -e 'temp' -e 'drupal_export'); do 
   echo "### Sanitizing ${db} ###"
-
   # If the sanitization is not set, use the DB name.
   [ "${sanitization-}" ] || sanitization=${db}
 
@@ -28,8 +27,13 @@ for db in $(sudo mysql -N -B -e 'SHOW DATABASES' | grep -v -e 'jira_assoc' -e 'i
   # A snapshot suitable for staging. We remove emails to avoid emailing people.
   suffix=.staging
   snapshot
-
   echo "### Completed sanitizing ${db} ###"
+
+  # Generate a list of all tables (except 'pift_ci_job_result' which is already
+  # compressed), and alter them to the compressed row_format.
+  echo "### Compressing ${db} ###"
+  ( sudo mysql "$db" -e "SHOW TABLES" --batch --skip-column-names | grep -v 'pift_ci_job_result' | xargs -n 1 -P 6 -I{} sudo mysql -e 'ALTER TABLE `'{}'` ROW_FORMAT=COMPRESSED;' "$db")
+  echo "### Completed compressing ${db} ###"
 done
 
 # Snapshot the staging level databases
