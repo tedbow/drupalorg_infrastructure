96b59dcc7ddfd104f7c24df372e936e01f1b79fa
Rudy Grigar <basic@drupal.org>
2016-08-02 10:36:22 -0700

    Clean up with changes from staging snapshot

--- snapshot/dev_sanitize.sh
+++ snapshot/dev_sanitize.sh
@@ -9,20 +9,21 @@ set -uex
 # which is already compressed), and alter them to the compressed row_format.
 for db in $(sudo mysql -N -B -e 'SHOW DATABASES' | grep -v -e 'jira_assoc' -e 'information_schema' -e 'performance_schema' -e 'mysql' -e 'percona' -e 'temp' -e 'drupal_export'); do 
   echo "### Sanitizing ${db} ###"
-
-  # If the sanitization is not set, use the DB name.
-  [ "${sanitization-}" ] || sanitization=${db}
+  # Sanitize using the DB name.
+  sanitization=${db}
 
   tmp_db=${db}
   tmp_args="${tmp_db}"
 
+  if [ ${db} == 'association_civicrm' ]; then
+    skip_common=1
+  else
+    skip_common=0
+  fi
+
   # Snapshot in stages.
   # A snapshot suitable for dev. We remove all private information.
   suffix=.dev
-  sanitize
-
-  # A smaller snapshot for taking up less space on dev.
-  suffix=.reduce
   sanitize
 
   echo "### Completed sanitizing ${db} ###"
