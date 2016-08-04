78001c55b68402f29bc1244432783bf29a4688a1
Rudy Grigar <basic@drupal.org>
2016-08-04 13:29:39 -0700

    Leave the ** START and ** END, but gut this thing to be less magical.

--- docker/db-images/db-image-load.sh
+++ docker/db-images/db-image-load.sh
@@ -3,24 +3,9 @@
 ############################################
 # Use this script to load all containers
 ############################################
-
-
-
 echo "** START: $(date) **"
-sudo bash -c 'echo 3 > /proc/sys/vm/drop_caches'
-for DB in $(ls) ; do
-  ## Parse the DB name from the file name
-  DBNAME=$(echo ${DB} | awk -F'_' '{print $1}')
-  ## Parse the DATE name from the file name
-  DOCKERREPOSITORY=$(echo ${DB} | awk -F'.' '{print $2}' | awk -F'-' '{print $1}')
-  DATE=$(echo ${DB} | awk -F'.' '{print $2}' | awk -F'-' '{print $2}')
-  echo "  Loading: ${DOCKERREPOSITORY}/${DBNAME}:${DATE}";
-  bunzip2 -dc < ${DB} | docker load
-  docker tag -f ${DOCKERREPOSITORY}/${DBNAME}:${DATE} ${DOCKERREPOSITORY}/${DBNAME}:latest
-
-  echo "  ================================================================"
-  echo "  ----------  Conatiner ${DOCKERREPOSITORY}/${DBNAME}:${DATE} Loaded"
-  echo "  ================================================================"
+for image in $(ls); do
+  pbunzip2 -dc < ${image} | docker load
 done
-sudo bash -c 'echo 3 > /proc/sys/vm/drop_caches'
 echo "** END: $(date) **"
+
