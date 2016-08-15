d5ca5b0fa8cdcc989e1d9cd43809204dd52e9b19
Rudy Grigar <basic@drupal.org>
2016-08-15 14:30:44 -0700

    Hardcode until this is more cleaned up

--- snapshot/common.sh
+++ snapshot/common.sh
@@ -38,6 +38,9 @@ function snapshot {
     fi
     cp "/var/sanitize/drupal_export/${db}${suffix}-schema.sql" "/var/sanitize/drupal_export/${subdir}"
     cd "/var/sanitize/drupal_export/${subdir}"
+    if [ "${whitelist-}" ]; then
+      tar --use-compress-program=pigz  -cvf /var/dumps/${subdir}/${db}${suffix}-${BUILD_NUMBER}-binary.tar.gz ./${db}${suffix}-schema.sql ./drupal_export/{*.ibd,*.cfg,*.exp}
+    fi
     tar --use-compress-program=pigz  -cvf /var/dumps/${subdir}/${db}${suffix}-${BUILD_NUMBER}-binary.tar.gz ./${db}${suffix}-schema.sql ./${db}/{*.ibd,*.cfg,*.exp}
     sudo chown -R bender:bender "/var/dumps/${subdir}/${db}${suffix}-${BUILD_NUMBER}-binary.tar.gz"
     ln -sfv "${db}${suffix}-${BUILD_NUMBER}-binary.tar.gz" "/var/dumps/${subdir}/${db}${suffix}-binary-current.tar.gz"
