22c39bd5c1957c5764dda1e40a2cf0cc6f27d7f1
Rudy Grigar <basic@drupal.org>
2016-09-13 11:58:14 -0700

    Tweak subjects and amount of time before notifying

--- drupalci/dci_reaper.sh
+++ drupalci/dci_reaper.sh
@@ -4,16 +4,16 @@
 # Exit immediately on uninitialized variable or error, and print each command.
 set -uex
 
-minuteago=$(date --date '30 minutes ago' +%Y-%m-%d\T%H\:%m\:%S)
+hoursago=$(date --date '3 hours ago' +%Y-%m-%d\T%H\:%m\:%S)
 dayago=$(date --date '1 day ago' +%Y-%m-%d\T%H\:%m\:%S)
 
-# Query for instances running for > 30 minutes that were never tagged with the
+# Query for instances running for > 3 hours that were never tagged with the
 # Testrunner name
 instances=$(aws ec2 describe-instances --filters \
   "Name=instance-type,Values=cc2.8xlarge" \
   "Name=instance-state-name,Values=running" \
   --query 'Reservations[].Instances[].{InstanceId: InstanceId, LaunchTime: LaunchTime, Name: Tags[?Key==`Name`].Value}' \
-  | jq ".[] | select(.LaunchTime < \"$minuteago\") | select(.Name == null)" | jq --raw-output ".InstanceId")
+  | jq ".[] | select(.LaunchTime < \"$hoursago\") | select(.Name == null)" | jq --raw-output ".InstanceId")
 
 # Verify $instances contains real data and not just IFS
 if [ "x`printf '%s' "$instances" | tr -d "$IFS"`" != x ]; then
@@ -22,7 +22,7 @@ if [ "x`printf '%s' "$instances" | tr -d "$IFS"`" != x ]; then
   #aws ec2 terminate-instances --instance-ids ${instances}
   aws ec2 describe-instances --instance-ids ${instances} \
     --query 'Reservations[].Instances[].{InstanceId: InstanceId, LaunchTime: LaunchTime, Name: Tags[?Key==`Name`].Value}' | \
-    jq '.' | mail -s "Instances: ${instances} are untagged" sitemaint@association.drupal.org
+    jq '.' | mail -s "[drupalci_reaper] Instances: ${instances} are untagged" sitemaint@association.drupal.org
 fi
 
 # Query for instances running for > 24 hours
@@ -36,5 +36,5 @@ instances=$(aws ec2 describe-instances --filters \
 if [ "x`printf '%s' "$instances" | tr -d "$IFS"`" != x ]; then
   echo "Long running (>24 hours) instances detected"
   aws ec2 describe-instances --instance-ids ${instances} | \
-    mail -s "Instances: ${instances} have been running for > 24 hours" sitemaint@association.drupal.org
+    mail -s "[drupalci_reaper] Instances: ${instances} have been running > 24 hours" sitemaint@association.drupal.org
 fi
