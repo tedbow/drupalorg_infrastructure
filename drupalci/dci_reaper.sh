329384fad9469a974c0b2d7dac41f2473fda8648
Rudy Grigar <basic@drupal.org>
2016-09-14 12:12:02 -0700

    Only notify for instances running over 10 hours

--- drupalci/dci_reaper.sh
+++ drupalci/dci_reaper.sh
@@ -4,10 +4,9 @@
 # Exit immediately on uninitialized variable or error, and print each command.
 set -uex
 
-hoursago=$(date --date '3 hours ago' +%Y-%m-%d\T%H\:%m\:%S)
-dayago=$(date --date '1 day ago' +%Y-%m-%d\T%H\:%m\:%S)
+hoursago=$(date --date '10 hours ago' +%Y-%m-%d\T%H\:%m\:%S)
 
-# Query for instances running for > 3 hours that were never tagged with the
+# Query for instances running for > 10 hours that were never tagged with the
 # Testrunner name
 instances=$(aws ec2 describe-instances --filters \
   "Name=instance-type,Values=cc2.8xlarge" \
@@ -20,21 +19,22 @@ if [ "x`printf '%s' "$instances" | tr -d "$IFS"`" != x ]; then
   echo "Unprovisioned instances detected"
   # Terminate instances
   #aws ec2 terminate-instances --instance-ids ${instances}
-  aws ec2 describe-instances --instance-ids ${instances} \
-    --query 'Reservations[].Instances[].{InstanceId: InstanceId, LaunchTime: LaunchTime, Name: Tags[?Key==`Name`].Value}' | \
-    jq '.' | mail -s "[drupalci_reaper] Instances: ${instances} are untagged" sitemaint@association.drupal.org
+  #aws ec2 describe-instances --instance-ids ${instances} \
+  #  --query 'Reservations[].Instances[].{InstanceId: InstanceId, LaunchTime: LaunchTime, Name: Tags[?Key==`Name`].Value}' | \
+  #  jq '.' | mail -s "[drupalci_reaper] Instances: ${instances} are untagged" sitemaint@association.drupal.org
 fi
 
-# Query for instances running for > 24 hours
+# Query for instances running for > $hoursago
 instances=$(aws ec2 describe-instances --filters \
   "Name=instance-type,Values=cc2.8xlarge" \
   "Name=instance-state-name,Values=running" \
   --query 'Reservations[].Instances[].{InstanceId: InstanceId, LaunchTime: LaunchTime, Name: Tags[?Key==`Name`].Value}' \
-  | jq ".[] | select(.LaunchTime < \"$dayago\")" | jq --raw-output ".InstanceId")
+  | jq ".[] | select(.LaunchTime < \"$hoursago\")" | jq --raw-output ".InstanceId")
 
 # Verify $instances contains real data and not just IFS
 if [ "x`printf '%s' "$instances" | tr -d "$IFS"`" != x ]; then
-  echo "Long running (>24 hours) instances detected"
-  aws ec2 describe-instances --instance-ids ${instances} | \
-    mail -s "[drupalci_reaper] Instances: ${instances} have been running > 24 hours" sitemaint@association.drupal.org
+  echo "Long running instances (> 10 hours) detected"
+  aws ec2 describe-instances --instance-ids ${instances} \
+    --query 'Reservations[].Instances[].{InstanceId: InstanceId, LaunchTime: LaunchTime, Name: Tags[?Key==`Name`].Value}' | \
+    jq '.' | mail -s "[drupalci_reaper] Instances: ${instances} have been running for > 10 hours" sitemaint@association.drupal.org
 fi
