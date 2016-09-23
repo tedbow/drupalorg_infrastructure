4f726f6629195481e10e22d604d231f0b4c998dc
Rudy Grigar <basic@drupal.org>
2016-09-23 13:58:16 -0700

    let the reaper terminate instances

--- drupalci/dci_reaper.sh
+++ drupalci/dci_reaper.sh
@@ -4,9 +4,9 @@
 # Exit immediately on uninitialized variable or error, and print each command.
 set -uex
 
-hoursago=$(date --date '10 hours ago' +%Y-%m-%d\T%H\:%m\:%S)
+hoursago=$(date --date '11 hours ago' +%Y-%m-%d\T%H\:%m\:%S)
 
-# Query for instances running for > 10 hours that were never tagged with the
+# Query for instances running for > 11 hours that were never tagged with the
 # Testrunner name
 instances=$(aws ec2 describe-instances --filters \
   "Name=instance-type,Values=cc2.8xlarge" \
@@ -33,8 +33,9 @@ instances=$(aws ec2 describe-instances --filters \
 
 # Verify $instances contains real data and not just IFS
 if [ "x`printf '%s' "$instances" | tr -d "$IFS"`" != x ]; then
-  echo "Long running instances (> 10 hours) detected"
+  echo "Long running instances (> 11 hours) detected"
   aws ec2 describe-instances --instance-ids ${instances} \
     --query 'Reservations[].Instances[].{InstanceId: InstanceId, LaunchTime: LaunchTime, Name: Tags[?Key==`Name`].Value}' | \
-    jq '.' | mail -s "[drupalci_reaper] Instances: ${instances} have been running for > 10 hours" sitemaint@association.drupal.org
+    jq '.' | mail -s "[drupalci_reaper] Instances: ${instances} have been running for > 11 hours" sitemaint@association.drupal.org
+  aws ec2 terminate-instances --instance-ids ${instances}
 fi
