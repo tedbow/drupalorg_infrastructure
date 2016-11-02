06854f3b97e1c2461269d016c3e67f93d71bfd32
Rudy Grigar <basic@drupal.org>
2016-11-01 17:58:58 -0700

    Add a fabfile for staging

---
+++ staging/fabfile.py
@@ -0,0 +1,45 @@
+from fabric.api import *
+
+# Hosts for staging
+env.hosts = ["wwwstg1", "wwwstg2", "btchstg1"]
+
+# Per site configurations
+if env.site == "drupal":
+    repo_url = "git@bitbucket.org:drupalorg-infrastructure/drupal.org-built.git"
+    clone_path = "/var/www/staging.devdrupal.org/htdocs"
+elif env.site == "api":
+    repo_url = "git@bitbucket.org:drupalorg-infrastructure/api.drupal.org-built.git"
+    clone_path = "/var/www/api.staging.devdrupal.org/htdocs"
+elif env.site == "assoc":
+    repo_url = "git@bitbucket.org:drupalorg-infrastructure/assoc.drupal.org-built.git"
+    clone_path = "/var/www/assoc.staging.devdrupal.org/htdocs"
+elif env.site == "events":
+    repo_url = "git@bitbucket.org:drupalorg-infrastructure/events.drupal.org-built.git"
+    clone_path = "/var/www/events.staging.devdrupal.org/htdocs"
+elif env.site == "groups":
+    repo_url = "git@bitbucket.org:drupalorg-infrastructure/groups.drupal.org-built.git"
+    clone_path = "/var/www/groups.staging.devdrupal.org/htdocs"
+elif env.site == "jobs":
+    repo_url = "git@bitbucket.org:drupalorg-infrastructure/jobs.drupal.org-built.git"
+    clone_path = "/var/www/jobs.staging.devdrupal.org/htdocs"
+elif env.site == "localize":
+    repo_url = "git@bitbucket.org:drupalorg-infrastructure/localize.drupal.org-built.git"
+    clone_path = "/var/www/localize.staging.devdrupal.org/htdocs"
+elif env.site == "security":
+    repo_url = "git@bitbucket.org:drupalorg-infrastructure/security.drupal.org-built.git"
+    clone_path = "/var/www/security.staging.devdrupal.org/htdocs"
+else:
+    sys.exit(1)
+
+
+@parallel
+def deploy():
+    with cd("%s" % clone_path):
+        run("git pull")
+        run("sudo service php5-fpm reload")
+
+@parallel
+def clone():
+    run("mkdir -p %s" % clone_path)
+    run("git clone %s %s" % (repo_url,clone_path))
+
