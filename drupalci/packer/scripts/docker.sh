20329783116e5b083be6f655540146c1d4d5205d
Ryan Aslett <ryan+ddoto@association.drupal.org>
2016-12-23 08:33:48 -0800

    Updates docker to a modernish version

--- drupalci/packer/scripts/docker.sh
+++ drupalci/packer/scripts/docker.sh
@@ -4,7 +4,7 @@
 # Author:      Nick Schuch (nick@myschuch.com)
 # Description: Installs Docker.
 date
-curl -sSL https://get.docker.com/ | sed 's/docker-engine/docker-engine=1.10.3-0~trusty/' |sudo sh
+curl -sSL https://get.docker.com/ | sed 's/docker-engine/docker-engine_1.12.1-0~trusty_amd64.deb/' |sudo sh
 
 # We also need to add the "ubuntu" user to the docker group so it can run
 # containers.
