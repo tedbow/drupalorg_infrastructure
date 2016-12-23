0ad188f3d1cfccea5d2794bf48f80e02e0ce1083
Ryan Aslett <ryan+ddoto@association.drupal.org>
2016-12-22 18:03:16 -0800

    Hide secrets, execute from proper directory

--- drupalci/build_slave_ami.sh
+++ drupalci/build_slave_ami.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# Exit immediately on uninitialized variable or error, and print each command.
-set -uex
+
 
 # EC2 configuration
 . ~/.ec2creds
+# Exit immediately on uninitialized variable or error, and print each command.
+# But do this after the .ec2creds so as to not display secrets everywhere
+set -uex
 export EC2_HOME=/opt/ec2/ec2-api-tools-1.7.3.2
 export EC2_URL=ec2.us-west-2.amazonaws.com
 export JAVA_HOME=/usr
 
 # Packer configuration
 export PACKER_HOME=/usr/local/bin
-
+cd /usr/local/drupal-infrastructure
 ${PACKER_HOME}/packer build packer/packer.json
