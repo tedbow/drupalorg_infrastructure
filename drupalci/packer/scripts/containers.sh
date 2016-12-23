a3edf0a84680a2496470f9bd114d87e7d290a4bd
Ryan Aslett <ryan+ddoto@association.drupal.org>
2016-12-23 08:33:31 -0800

    Updates script to pull known used containers

--- drupalci/packer/scripts/containers.sh
+++ drupalci/packer/scripts/containers.sh
@@ -5,8 +5,17 @@
 # Description: Pull down the containers required for a build via the make command.
 date
 cd /opt/drupalci_testbot
-# TODO: this should get the containers from drupalci/* and not from the local filesystem.
-for CONTAINER in $(find ./containers -name Dockerfile | grep -v 'dev' | awk -F"/" '{print $4}');
-do
-    docker pull drupalci/${CONTAINER};
-done
+docker pull drupalci/mysql-5.5
+docker pull drupalci/pgsql-9.1
+docker pull drupalci/web-7
+docker pull drupalci/web-5.6
+docker pull drupalci/web-5.5
+docker pull drupalci/web-5.4
+docker pull drupalci/web-5.3
+
+# TODO: this should iterate over containers that are matchable by a docker search - need a good naming scheme for the
+# new containers.
+#for CONTAINER in $(find ./containers -name Dockerfile | grep -v 'dev' | awk -F"/" '{print $4}');
+#do
+#   docker pull drupalci/${CONTAINER};
+#done
