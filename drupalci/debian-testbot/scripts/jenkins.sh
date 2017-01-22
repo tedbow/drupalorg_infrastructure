e3f99bd15ab0b452382e2c16dde7230fc80c9879
Ryan Aslett <ryan+ddoto@association.drupal.org>
2017-01-21 22:05:17 -0800

    Adds a script that jenkins can execute

--- drupalci/debian-testbot/scripts/jenkins.sh
+++ drupalci/debian-testbot/scripts/jenkins.sh
@@ -48,3 +48,36 @@ python /usr/bin/userdata
 exit 0
 EOF
 ) >> /etc/rc.local
+
+# Create the script that jenkins will run:
+(
+cat << 'EOF'
+docker ps |grep drupalci |awk '{print $1}' |xargs docker stop &> /dev/null||true; docker ps -a |grep drupalci |awk '{print $1}' |xargs docker rm &>/dev/null || true
+find /var/lib/drupalci/workspace -mindepth 1 -maxdepth 1 -mtime +7 -exec sudo rm -rf "{}" \;
+
+set -uex
+id
+echo https://www.drupal.org/pift-ci-job/${Drupal_JobID#https://www.drupal.org:}
+curl -w '\n' -s http://169.254.169.254/latest/meta-data/instance-type
+curl -w '\n' -s http://169.254.169.254/latest/meta-data/ami-id
+curl -w '\n' -s http://169.254.169.254/latest/meta-data/public-ipv4
+#Get rid of web files over 7 days old
+env |grep DCI
+env |grep -v DCI
+
+cd /opt/drupalci/testrunner
+
+git fetch --all --tags
+git checkout ${Testrunner_Branch}
+git pull --rebase
+docker pull drupalci/${DCI_PHPVersion}
+
+# Make sure that any composer changes to drupalci_testbot are picked up
+# If a container rebuild has not happened yet.
+/usr/local/bin/composer install --no-progress --no-suggest
+
+./drupalci run ${DCI_JobType}
+EOF
+) > ${TESTRUNNER_DIR}/jenkins.sh
+chmod +x ${TESTRUNNER_DIR}/jenkins.sh
+chown testbot:testbot ${TESTRUNNER_DIR}/jenkins.sh
