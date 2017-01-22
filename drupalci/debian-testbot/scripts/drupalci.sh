d3df379fd5311a2522a8a202cd642ecf6121f37f
Ryan Aslett <ryan+ddoto@association.drupal.org>
2017-01-21 20:04:23 -0800

    fixes some perms, ordering and directories

--- drupalci/debian-testbot/scripts/drupalci.sh
+++ drupalci/debian-testbot/scripts/drupalci.sh
@@ -15,20 +15,17 @@ chmod 0440 /tmp/testbot
 mv /tmp/testbot /etc/sudoers.d/
 
 DIR="/opt/drupalci_testbot"
-DRUPAL_DIR="/opt/drupal_checkout"
+DRUPAL_DIR="${DIR}/drupal_checkout"
 COMPOSER_CACHE_DIR="/opt/composer_cache"
 mkdir ${COMPOSER_CACHE_DIR}
+composer config -g cache-dir ${COMPOSER_CACHE_DIR}
 git clone --branch production http://git.drupal.org/project/drupalci_testbot.git ${DIR}
 composer install --prefer-dist --no-progress --working-dir ${DIR}
 chown -R testbot:testbot ${DRUPAL_DIR}
 
 chmod 775 ${DIR}/drupalci
 ln -s ${DIR}/drupalci /usr/local/bin/drupalci
-
-if ! [ -h /opt/drupalci_testbot ];
-  then
-    ln -s /home/testbot/drupalci_testbot /opt/drupalci_testbot
-fi
+mkdir -p /home/testbot/drupalci_testbot
 
 # Lets prepopulate the composer cache
 git clone http://git.drupal.org/project/drupal.git ${DRUPAL_DIR}
@@ -56,6 +53,7 @@ rsync -a /opt/drupal_checkout/ /var/lib/drupalci/drupal-checkout
 chmod 777 /var/lib/drupalci/docker-tmp
 chmod 777 /var/lib/drupalci/coredumps
 chown -R testbot:testbot /var/lib/drupalci
+chown -R testbot:testbot /home/testbot/.*
 chown -R testbot:testbot /home/testbot
 
 exit 0
