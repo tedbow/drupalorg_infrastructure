0d80bf7257bd74bcbc44883cdf1356e526b08223
Neil Drumm <drumm@delocalizedham.com>
2017-03-22 17:07:27 -0400

    Found another missing www

--- snapshot/common.staging.sql
+++ snapshot/common.staging.sql
@@ -1,3 +1,3 @@
 UPDATE users SET mail = concat(md5(name), '@sanitized.invalid');
-UPDATE users SET init = if(init LIKE 'drupal.org/user/%/edit', concat('staging.dev', init), mail);
+UPDATE users SET init = if(init LIKE 'www.drupal.org/user/%/edit', concat('staging.dev', init), mail);
 UPDATE authmap SET authname = concat(aid, '@sanitized.invalid');
