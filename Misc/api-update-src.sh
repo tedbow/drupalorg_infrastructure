ea2dddee5e399171c0178627d40be81872d2ad78
Neil Drumm <drumm@delocalizedham.com>
2017-05-06 15:30:48 -0400

    Add script to update code for API.Drupal.org to parse

---
+++ Misc/api-update-src.sh
@@ -0,0 +1,11 @@
+set -uex
+
+cd /usr/local/api.drupal.org-src
+for d in $(ls); do
+  cd $d
+  git pull
+  if echo $d | grep -q '^drupal-8' && [ -f 'composer.json' ]; then
+    composer install --prefer-dist --no-interaction --ignore-platform-reqs --no-dev
+  fi
+  cd ..
+done
