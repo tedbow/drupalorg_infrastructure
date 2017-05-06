07fcc9b49cfc6591cc62588d72e3c0f60c42494a
Neil Drumm <drumm@delocalizedham.com>
2017-05-06 12:15:35 -0400

    Issue #2443023: Dedupe tables

--- snapshot/drupal.raw.sql
+++ snapshot/drupal.raw.sql
@@ -2,8 +2,17 @@
 UPDATE apachesolr_environment SET url = 'http://not.indexed.invalid';
 
 -- Empty out scratch tables used during drupal7 migration.
-TRUNCATE project_issue_projects;
-TRUNCATE projects;
-TRUNCATE project_issues;
-TRUNCATE project_issue_comments;
-TRUNCATE comment_upload;
+DROP TABLE project_issue_projects;
+DROP TABLE projects;
+DROP TABLE project_issues;
+DROP TABLE project_issue_comments;
+DROP TABLE project_releases_backup;
+DROP TABLE project_releases;
+DROP TABLE project_release_nodes;
+DROP TABLE project_release_legacy;
+DROP TABLE project;
+DROP TABLE project_release_projects;
+DROP TABLE project_comments_conversion_errors;
+DROP TABLE project_issue_priority;
+DROP TABLE project_issue_state;
+DROP TABLE comment_upload;
