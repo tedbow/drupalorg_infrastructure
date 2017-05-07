f4617ea90de353a705591cdf0420ecc3767e7e92
Neil Drumm <drumm@delocalizedham.com>
2017-05-07 13:16:33 -0400

    Issue #2443023: Remove reference to removed table

--- snapshot/drupal.staging.sql
+++ snapshot/drupal.staging.sql
@@ -1,3 +1,2 @@
 UPDATE comment SET mail = CONCAT(name, '@sanitized.invalid');
-UPDATE simplenews_subscriptions SET mail = CONCAT(snid, '@sanitized.invalid');
 UPDATE multiple_email me INNER JOIN users u ON u.uid = me.uid SET me.email = concat(me.eid, '.', u.mail);
