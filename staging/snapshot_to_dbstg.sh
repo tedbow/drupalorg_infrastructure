939eca3d8a173e1c5e5297f193c63b276169a377
Neil Drumm <drumm@delocalizedham.com>
2017-05-24 09:55:01 -0400

    Replace some Perl

--- staging/snapshot_to_dbstg.sh
+++ staging/snapshot_to_dbstg.sh
@@ -38,12 +38,10 @@ tar -I pigz -xvf ${db}.${stage}-binary-current.tar.gz -C ${target_db}
 chown -R mysql:mysql ./${target_db}/${db}/*
 chown bender:bender ./${target_db}/{*.sql,$db}
 
-# Just in case the tables aren't in the correct compressed format in the
-# schema, ensure they are created with compression. Note: the binary data and the
-# row format must match for tablespace import.
-# @TODO verify row_format=compressed everywhere
-perl -p -i -e 's/^\) ENGINE=InnoDB.*$/\) ENGINE=InnoDB ROW_FORMAT=compressed DEFAULT CHARSET=utf8\;/' ${target_db}/${db}.${stage}-schema.sql
 mysql ${target_db} < /data/dumps/${target_db}/${db}.${stage}-schema.sql 
+# Ensure tables have compression. The binary data and the row format must match
+# for tablespace import.
+( mysql "${target_db}" -e "SHOW TABLES" --batch --skip-column-names | xargs -n 1 -P 6 -I{} sudo mysql -e 'ALTER TABLE `'{}'` ROW_FORMAT=COMPRESSED;' "${target_db}")
 
 # Discard the data files for the newly created tables
 ( mysql ${target_db} -e "SHOW TABLES" --batch --skip-column-names | xargs -t -n 1 -P 20 -I{} mysql -e 'SET FOREIGN_KEY_CHECKS=0; ALTER TABLE `'{}'` DISCARD TABLESPACE;' ${target_db})
