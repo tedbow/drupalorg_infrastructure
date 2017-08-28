c332017ad67aafe7d56deb46ac8f48de1de5d4cf
Neil Drumm <drumm@delocalizedham.com>
2017-08-28 14:45:48 -0400

    Issue #2904893: Drupal 8.x branches on api.drupal.org should show developer code

--- Misc/api-update-src.sh
+++ Misc/api-update-src.sh
@@ -5,7 +5,7 @@ for d in $(ls); do
   cd $d
   git pull
   if echo $d | grep -q '^drupal-8' && [ -f 'composer.json' ]; then
-    composer install --prefer-dist --no-interaction --ignore-platform-reqs --no-dev
+    composer install --prefer-dist --no-interaction --ignore-platform-reqs --dev
   fi
   cd ..
 done
