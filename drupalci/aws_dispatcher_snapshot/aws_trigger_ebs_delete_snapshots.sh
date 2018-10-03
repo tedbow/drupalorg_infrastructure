0bf8d05fa5b7098a71c7f9a5bf2442fa08ae1a00
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2018-10-03 09:27:38 -0700

    Removes old snapshot mechanisms

--- drupalci/aws_dispatcher_snapshot/aws_trigger_ebs_delete_snapshots.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-source /usr/local/drupal-infrastructure/drupalci/aws_dispatcher_snapshot/aws_common.sh
-
-date31=$(date --date="31 days ago" +'%Y%m%d%H%M')
-
-descriptor="auto_dispatcher_/dev"
-
-delete_old_ebs_snapshots "${date31}" "${descriptor}"
