f69a35439b556e32696696b0cc1b006dc9b32f80
Neil Drumm <drumm@delocalizedham.com>
2018-12-21 12:26:19 -0500

    Add drupalorg_gitlab_project_backlog table

--- sanitize/whitelists/drupalorg.py
+++ sanitize/whitelists/drupalorg.py
@@ -769,6 +769,14 @@ whitelist.add(
     ])
 
 whitelist.add(
+    table="drupalorg_gitlab_project_backlog",
+    columns=[
+        "nid",
+        "sync",
+        "timestamp",
+    ])
+
+whitelist.add(
     table="drupalorg_ind_civimembership",
     columns=[
         "user_name",
