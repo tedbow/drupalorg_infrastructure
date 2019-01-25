30211c3495e66682ef0e7049f1fff487a3f04b3c
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-01-25 12:05:27 -0800

    Adds the backup script so jenkins can execute it

---
+++ gitlab/backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+
+# Have gitlab take a backup
+sudo /opt/gitlab/bin/gitlab-rake gitlab:backup:create | tee -a /tmp/backupoutput.log
+GITLAB_BACKUP=`grep 'Creating backup archive:' /tmp/backupoutput.log |awk {print $4}`
+echo ${GITLAB_BACKUP%_gitlab_backup.tar}
+
+rsync -avPz /var/opt/gitlab/backups/${GITLAB_BACKUP} gitlabstg1.drupal.bak::backups
