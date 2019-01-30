374139c33025a162e431efdfdee076d948abe07d
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-01-30 12:43:57 -0800

    Updates the backup script to clear out the daily backup log and to use sudo to do the rsync

--- gitlab/backup.sh
+++ gitlab/backup.sh
@@ -2,8 +2,9 @@
 set -eux
 
 # Have gitlab take a backup
+rm /tmp/backupoutput.log
 sudo /opt/gitlab/bin/gitlab-rake gitlab:backup:create | tee -a /tmp/backupoutput.log
 GITLAB_BACKUP=`grep 'Creating backup archive:' /tmp/backupoutput.log |awk '{print $4}'`
 echo ${GITLAB_BACKUP%_gitlab_backup.tar}
 
-rsync -avPz /var/opt/gitlab/backups/${GITLAB_BACKUP} gitlabstg1.drupal.bak::backups
+sudo rsync -avPz /var/opt/gitlab/backups/${GITLAB_BACKUP} gitlabstg1.drupal.bak::backups
