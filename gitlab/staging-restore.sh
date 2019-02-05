f9d6234958e09219a6153bda315e3f92f69579da
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-02-05 15:43:21 -0800

    Updates the script to actually do the work

--- gitlab/staging-restore.sh
+++ gitlab/staging-restore.sh
@@ -6,17 +6,14 @@ sudo gitlab-ctl stop unicorn
 sudo gitlab-ctl stop sidekiq
 
 # Restore the backup on gitlabstg1
-# XXX gitlab-rake gitlab:backup:restore BACKUP=$1
-echo $1
-echo $2
+gitlab-rake gitlab:backup:restore BACKUP=$1
 
 # Reconfigure the geo settings for staging urls.
-# XXX gitlab-rails runner "eval(File.read '/usr/local/drupal-infrastructure/gitlab/geo-reconfigure.rb')"
+gitlab-rails runner "eval(File.read '/usr/local/drupal-infrastructure/gitlab/geo-reconfigure.rb')"
 GITLAB_URL=gitlab.drupalcode.org
 
 # Reset the authorized_keys setting to use the db
-# XXX curl -s -g --request PUT --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" "https://${GITLAB_URL}/api/v4/application/settings?authorized_keys_enabled=false
-curl -k -s -v -g --request GET --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" https://gitlab.drupalcode.org/api/v4/application/settings
+curl -s -g --request PUT --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" "https://${GITLAB_URL}/api/v4/application/settings?authorized_keys_enabled=false
 
 # Restart gitlab
 sudo gitlab-ctl start unicorn
