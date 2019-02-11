5b48ba99627f8a0ba99cb639c784500d02197e13
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-02-10 22:02:41 -0800

    need this variable inside the script too.

--- gitlab/backup.sh
+++ gitlab/backup.sh
@@ -6,4 +6,6 @@ rm /tmp/backupoutput.log
 # Have gitlab take a backup
 sudo /opt/gitlab/bin/gitlab-rake gitlab:backup:create | tee -a /tmp/backupoutput.log
 # Transfer the backup to staging.
+GITLAB_BACKUP=`grep 'Creating backup archive:' /tmp/backupoutput.log |awk '{print $4}'`
+
 sudo rsync -avPz /var/opt/gitlab/backups/${GITLAB_BACKUP} gitlabstg1.drupal.bak::backups
