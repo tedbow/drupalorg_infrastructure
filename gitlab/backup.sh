1f3741bd11acf65497b758848454bdb1fe0024e6
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-02-11 09:43:41 -0800

    so many safety belts

--- gitlab/backup.sh
+++ gitlab/backup.sh
@@ -2,7 +2,7 @@
 set -eux
 
 # Ensure we've got a clean log
-rm /tmp/backupoutput.log
+rm /tmp/backupoutput.log || true
 # Have gitlab take a backup
 sudo /opt/gitlab/bin/gitlab-rake gitlab:backup:create | tee -a /tmp/backupoutput.log
 # Transfer the backup to staging.
