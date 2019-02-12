75450c5842b8da9133c42eade44b126a8ed24297
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-02-12 08:54:38 -0800

    Had the wrong host, too

--- gitlab/staging-restore.sh
+++ gitlab/staging-restore.sh
@@ -12,14 +12,14 @@ rm -rf /var/opt/gitlab/backups/repositories /var/opt/gitlab/backups/tmp /var/opt
 # Restore the backup on gitlabstg1
 gitlab-rake gitlab:backup:restore force=yes BACKUP=${1%_gitlab_backup.tar} |tee -a backupoutput.txt
 # Remove the backup file
-rm -rf /var/opt/gitlab/backups/$1
+rm -rf /var/opt/gitlab/backups/$1 || true
 
 # Reconfigure the geo settings for staging urls.
 gitlab-rails runner "eval(File.read '/usr/local/drupal-infrastructure/gitlab/geo-reconfigure.rb')"
-GITLAB_URL=gitlab.drupalcode.org
+GITLAB_HOST=gitlab.code-staging.devdrupal.org
 
 # Reset the authorized_keys setting to use the db
-curl -s -g --request PUT --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" https://${GITLAB_URL}/api/v4/application/settings?authorized_keys_enabled=false
+curl -s -g --request PUT --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" https://${GITLAB_HOST}/api/v4/application/settings?authorized_keys_enabled=false
 
 # Restart gitlab
 gitlab-ctl start unicorn
