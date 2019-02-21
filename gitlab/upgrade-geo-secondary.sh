207387ab606849226548b97e90cc9b5c5b36c56b
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-02-21 14:44:23 -0800

    makes sure that the geo db is also migrated and also not running while we try and upgrade

--- gitlab/upgrade-geo-secondary.sh
+++ gitlab/upgrade-geo-secondary.sh
@@ -3,10 +3,13 @@ set -eux
 # Make sure everything is in a proper state
 gitlab-ctl reconfigure
 # upgrade
+gitlab-ctl stop sidekiq
+gitlab-ctl stop geo-logcursor
 apt-get update && apt-get install gitlab-ee
 # post upgrade reconfigure
 SKIP_POST_DEPLOYMENT_MIGRATIONS=true gitlab-ctl reconfigure
 gitlab-rake db:migrate
+gitlab-rake geo:db:migrate
 gitlab-ctl hup unicorn
 gitlab-ctl hup sidekiq
 gitlab-ctl restart geo-logcursor
