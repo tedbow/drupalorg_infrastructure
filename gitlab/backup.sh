729cac590968861d8526dfe981484c0bdddd53bc
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-02-26 08:43:54 -0800

    Lets move the rsync out of the backup script so that we can disable it easier

--- gitlab/backup.sh
+++ gitlab/backup.sh
@@ -5,7 +5,3 @@ set -eux
 rm /tmp/backupoutput.log || true
 # Have gitlab take a backup
 sudo /opt/gitlab/bin/gitlab-rake gitlab:backup:create | tee -a /tmp/backupoutput.log
-# Transfer the backup to staging.
-GITLAB_BACKUP=`grep 'Creating backup archive:' /tmp/backupoutput.log |awk '{print $4}'`
-
-sudo rsync -avPz /var/opt/gitlab/backups/${GITLAB_BACKUP} gitlabstg1.drupal.bak::backups
