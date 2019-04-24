cad7659e6fd4c599ac661f129012c65768b4bad9
Neil Drumm <drumm@delocalizedham.com>
2019-04-24 12:32:28 -0400

    New service name on upgraded server

--- staging/fabfile.py
+++ staging/fabfile.py
@@ -44,7 +44,10 @@ def deploy():
             run("git checkout %s" % (env.branch))
             run("git branch --set-upstream-to=origin/%s %s" % (env.branch, env.branch))
         run("git pull")
-        run("sudo /usr/sbin/service php5-fpm reload")
+        if env.host = "wwwstg2.drupal.bak"
+            run("sudo /usr/sbin/service php5.6-fpm reload")
+        else
+            run("sudo /usr/sbin/service php5-fpm reload")
 
 @parallel
 def clone():
