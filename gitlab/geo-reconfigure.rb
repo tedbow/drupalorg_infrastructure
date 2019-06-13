c99af2d89efdeee4cae2c5ce902e4d5ecf99f4f0
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2019-06-13 10:02:20 -0700

    Geo added a new name thing that also needs to be renamed

--- gitlab/geo-reconfigure.rb
+++ gitlab/geo-reconfigure.rb
@@ -1,3 +1,3 @@
 # This reconfigures staging geo via a script
-Gitlab::Geo.primary_node.update!(url: "https://git.code-staging.devdrupal.org/")
-Gitlab::Geo.secondary_nodes.first.update!(url: "https://git2.code-staging.devdrupal.org/", oauth_application_id: nil)
+Gitlab::Geo.primary_node.update!(url: "https://git.code-staging.devdrupal.org/", name: "https://git.code-staging.devdrupal.org")
+Gitlab::Geo.secondary_nodes.first.update!(url: "https://git2.code-staging.devdrupal.org/", name: "https://git2.code-staging.devdrupal.org/", oauth_application_id: nil)
