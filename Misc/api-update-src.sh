dd3eaa7659a20afd205b6d508889ffcb5f12243e
Neil Drumm <drumm@delocalizedham.com>
2019-08-06 15:54:06 -0400

    Remove deprecated option

--- Misc/api-update-src.sh
+++ Misc/api-update-src.sh
@@ -5,7 +5,7 @@ for d in $(ls); do
   cd $d
   git pull
   if echo $d | grep -q '^drupal-8' && [ -f 'composer.json' ]; then
-    composer install --prefer-dist --no-interaction --ignore-platform-reqs --dev
+    composer install --prefer-dist --no-interaction --ignore-platform-reqs
   fi
   cd ..
 done
