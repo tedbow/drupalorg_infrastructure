1215f7968467c48921b04bc368d03d6b44ef4816
Neil Drumm <drumm@delocalizedham.com>
2019-08-22 17:29:18 -0400

    Update service name to the correct one

--- dev/common.sh
+++ dev/common.sh
@@ -4,7 +4,7 @@ set -uex
 
 function restart_apache {
   sudo service apache2 restart
-  sudo service php5-fpm restart
+  sudo service php5.6-fpm restart
 }
 
 # Set common variables.
@@ -12,4 +12,3 @@ vhost_path="/etc/apache2/automated-jenkins/${name}-${site}.conf"
 web_path="/var/www/dev/${name}-${site}.dev.devdrupal.org"
 # Clean site name, no dots or dashes, truncate to 16 characters.
 db_name=$(echo "${name}_${site}" | sed -e "s/[-.]/_/g;s/^\(.\{16\}\).*/\1/")
-
