3464dfbe32aff64172803ec6bbc32b328b5b4593
Neil Drumm <drumm@delocalizedham.com>
2019-08-29 10:51:09 -0400

    Coder keeps ending up with local changes, get rid of them

--- Misc/api-update-src.sh
+++ Misc/api-update-src.sh
@@ -4,6 +4,12 @@ cd /usr/local/api.drupal.org-src
 for d in $(ls); do
   cd $d
   git pull
+  # Coder keeps ending up with local changes, get rid of them.
+  if [ -d 'vendor/drupal/coder' ]; then
+    pushd 'vendor/drupal/coder'
+    git diff-index --quiet HEAD -- || git reset --hard
+    popd
+  fi
   if echo $d | grep -q '^drupal-8' && [ -f 'composer.json' ]; then
     composer install --prefer-dist --no-interaction --ignore-platform-reqs
   fi
