6c84aae25893b5fdb59bc62cd37f84d9d59ef1e9
Neil Drumm <drumm@delocalizedham.com>
2020-04-15 10:11:13 -0400

    Contact module was uninstalled

--- snapshot/drupal_association.staging.sql
+++ snapshot/drupal_association.staging.sql
@@ -1,3 +1,2 @@
 UPDATE comment SET mail = CONCAT(name, '@sanitized.invalid');
 UPDATE donations SET mail = CONCAT(did, '@sanitized.invalid');
-UPDATE contact SET recipients = 'noreply@sanitized.invalid';
