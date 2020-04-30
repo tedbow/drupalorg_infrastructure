4299d0d2a15dd234b2f1a523f35621cbb2da4a5c
Ted Bowman <ted@tedbow.com>
2020-04-30 17:39:49 -0400

    use full path of phpstan

--- stats/project_analysis/analyzer.sh
+++ stats/project_analysis/analyzer.sh
@@ -7,7 +7,8 @@ cd /var/lib/drupalci/workspace/drupal-checkouts/drupal$5
 COMPOSER_CACHE_DIR=/tmp/cache$5 composer --no-interaction --no-progress require drupal/$2 $3 2>> /var/lib/drupalci/workspace/phpstan-results/$1.$3.phpstan_stderr
 php -d memory_limit=2G -d sys_temp_dir=/var/lib/drupalci/workspace/drupal-checkouts/drupal$5 ./vendor/bin/phpstan analyse --no-progress --error-format checkstyle -c ./phpstan.neon  ./${4#project_}s/contrib/$2 > /var/lib/drupalci/workspace/phpstan-results/$1.$3.phpstan_results.xml 2>> /var/lib/drupalci/workspace/phpstan-results/$1.$3.phpstan_stderr
 
-rm phpstan.neon
+# Remove phpstan.neon because it is not needed for rector and causes some modules to fail.
+rm /var/lib/drupalci/workspace/drupal-checkouts/drupal$5/phpstan.neon
 # Only run rector if we have some file messages in the XML.
   # Create a git commit for the current state of the project
 cd ${4#project_}s/contrib/$2
