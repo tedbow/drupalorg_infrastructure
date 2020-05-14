771c8e9f780e7c6e4657313fe2138368cacf298e
Ted Bowman <ted@tedbow.com>
2020-05-14 17:54:49 -0400

    update core_version_requirement if doesn't satisfiy 9.0.0

--- stats/project_analysis/info_updater/src/InfoUpdater.php
+++ stats/project_analysis/info_updater/src/InfoUpdater.php
@@ -2,13 +2,24 @@
 
 namespace InfoUpdater;
 
+use Composer\Semver\Semver;
 use Symfony\Component\Yaml\Yaml;
 
 class InfoUpdater {
   public static function updateInfo($file) {
     $info = Yaml::parseFile($file);
+    $update_info = FALSE;
     if (!isset($info['core_version_requirement'])) {
       $info['core_version_requirement'] = '^8 || ^9';
+      $update_info = TRUE;
+    }
+    else {
+      if (!Semver::satisfies('9.0.0', $info['core_version_requirement'])) {
+        $info['core_version_requirement'] .= ' || ^9';
+        $update_info = TRUE;
+      }
+    }
+    if ($update_info) {
       $yml = Yaml::dump($info);
       return file_put_contents($file, $yml) !== FALSE;
     }
