367cd1ea6592e2fe1b94eff268bed8b4e46297f9
Ted Bowman <ted@tedbow.com>
2020-05-27 08:58:35 -0400

    install in install_phpstan.sh

--- stats/project_analysis/install_phpstan.sh
+++ stats/project_analysis/install_phpstan.sh
@@ -5,4 +5,8 @@ cd /var/lib/drupalci/workspace/drupal-checkouts
 mkdir -p /var/lib/drupalci/workspace/phpstan-results
 git clone -s /var/lib/drupalci/drupal-checkout drupal$1
 cd /var/lib/drupalci/workspace/drupal-checkouts/drupal$1
+COMPOSER_CACHE_DIR=/tmp/cache$1 sudo php -d sys_temp_dir=/var/lib/drupalci/workspace/drupal-checkouts/drupal$1 ./vendor/bin/drush si --db-url=sqlite://sites/default/files/.ht.sqlite -y
+COMPOSER_CACHE_DIR=/tmp/cache$1 sudo php -d sys_temp_dir=/var/lib/drupalci/workspace/drupal-checkouts/drupal$1 ./vendor/bin/drush ./vendor/bin/drush en upgrade_status -y
+git add sites/default/files/.ht.sqlite
+git add .;git commit -q -m "add sqlite"
 COMPOSER_CACHE_DIR=/tmp/cache$1 composer config prefer-stable false
