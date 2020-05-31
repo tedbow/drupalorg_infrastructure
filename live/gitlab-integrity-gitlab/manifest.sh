b4a9809ecac33d377aa0eef40b8a9f239aa72b44
Neil Drumm <drumm@delocalizedham.com>
2020-05-31 11:30:15 -0400

    Issue #3046642: Remove unnecessary join

--- live/gitlab-integrity-gitlab/manifest.sh
+++ live/gitlab-integrity-gitlab/manifest.sh
@@ -11,7 +11,7 @@ sudo gitlab-psql -d gitlabhq_production -c "COPY (SELECT u.username, e.email, e.
 sudo gitlab-psql -d gitlabhq_production -c "COPY (SELECT u.username, replace(k.fingerprint, ':', '') fingerprint FROM keys k INNER JOIN users u ON u.id = k.user_id AND u.email LIKE '%.no-reply.drupal.org') TO STDOUT" | sort > keys.tsv
 
 # Projects.
-sudo gitlab-psql -d gitlabhq_production -c "COPY (SELECT p.name, p.id, lower(p.path), p.description, n.path FROM projects p LEFT JOIN project_mirror_data pmd ON pmd.project_id = p.id LEFT JOIN namespaces n ON n.id = p.namespace_id WHERE p.marked_for_deletion_by_user_id IS NULL) TO STDOUT" | sort > projects.tsv
+sudo gitlab-psql -d gitlabhq_production -c "COPY (SELECT p.name, p.id, lower(p.path), p.description, n.path FROM projects p LEFT JOIN namespaces n ON n.id = p.namespace_id WHERE p.marked_for_deletion_by_user_id IS NULL) TO STDOUT" | sort > projects.tsv
 
 # Maintainers.
 sudo gitlab-psql -d gitlabhq_production -c "COPY (SELECT p.name, u.username, m.access_level FROM members m INNER JOIN users u ON u.id = m.user_id INNER JOIN projects p ON p.id = m.source_id AND p.marked_for_deletion_by_user_id IS NULL WHERE m.type = 'ProjectMember') TO STDOUT" | sort > maintainers.tsv
