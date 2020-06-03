631fb41844dad6e5b90b6164e85c6b5ed4b2bd40
Ted Bowman <ted@tedbow.com>
2020-06-02 21:30:01 -0400

    filter and test

--- stats/project_analysis/project_analysis_utils/src/GitHelper.php
+++ stats/project_analysis/project_analysis_utils/src/GitHelper.php
@@ -41,13 +41,38 @@ class GitHelper {
     // Match any line that starts with + or - and has no other characters
     // except space, * , / or {
     $pattern = '/^[\+\-].*[^\/* {}].*/';
+    $use_replacements = [];
     foreach ($files as $file) {
       $diff = $this->shellExecSplit("git diff $file");
       $diff = array_splice($diff, 5);
 
       $rector_change = FALSE;
-      foreach ($diff as $diff_line) {
+      $skip_lines = [];
+      foreach ($diff as $index => $diff_line) {
+        if (in_array($index, $skip_lines)) {
+          continue;
+        }
+        if (strpos($diff_line, "+use ") === 0) {
+          $imported_class = str_replace(['+use ', ';'], ['', ''], $diff_line);
+          $parts = explode('\\', $imported_class);
+          $use_replacements[] = [
+            'full' => "\\$imported_class",
+            'short' => array_pop($parts),
+          ];
+          continue;
+        }
         if (preg_match($pattern, $diff_line)) {
+          if (strpos($diff_line, '-') === 0 && isset($diff[$index + 1]) && strpos($diff[$index + 1] , '+') === 0) {
+            $remove_line = str_replace('- ', '', $diff_line);
+            $add_line = str_replace('+ ', '', $diff[$index + 1]);
+            foreach ($use_replacements as $use_replacement) {
+              if (str_replace($use_replacement['full'], $use_replacement['short'], $remove_line) === $add_line) {
+                // The add an remove lines are exactly the same except or the FQCN.
+                $skip_lines[] = $index + 1;
+                continue 2;
+              }
+            }
+          }
           $rector_change = TRUE;
           break;
         }
