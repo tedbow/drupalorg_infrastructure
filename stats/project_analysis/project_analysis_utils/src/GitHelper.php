3030b723284f64abe8597f0a3796a603893a2cd1
Ted Bowman <ted@tedbow.com>
2020-06-03 07:19:19 -0400

    more comments

--- stats/project_analysis/project_analysis_utils/src/GitHelper.php
+++ stats/project_analysis/project_analysis_utils/src/GitHelper.php
@@ -21,12 +21,16 @@ class GitHelper {
   }
 
   /**
-   * Restores any file that only has change not made by rector.
+   * Restores any file that only has change not made by rector deprecation rules.
    *
-   * Currently this method just looks for files where all the changes lines only
-   * contain spaces, { or }. Rector has a bug where it changes some lines
-   * that aren't related to any rules.
-   * https://github.com/rectorphp/rector/issues/3375
+   * Currently this method looks for files with where all the changes are of the
+   * following types:
+   *   - Lines that only ocontain spaces, *, {, or }. Rector has a bug where it
+   *     changes some lines that aren't related to any rules.
+   *     https://github.com/rectorphp/rector/issues/3375
+   *   - Changes that are only fully qualified class name replacements. This
+   *     are good changes but they are not deprecations.
+   *
    *
    * @param string $dir
    *   The working directory to use.
@@ -62,12 +66,16 @@ class GitHelper {
           continue;
         }
         if (preg_match($pattern, $diff_line)) {
+          // If this is not a line that just has space, *, / changes check to
+          // see if the current line and the next line are only a replacement of
+          // a FQCN. While this is good fix it is not a deprecation.
           if (strpos($diff_line, '-') === 0 && isset($diff[$index + 1]) && strpos($diff[$index + 1] , '+') === 0) {
             $remove_line = str_replace('- ', '', $diff_line);
             $add_line = str_replace('+ ', '', $diff[$index + 1]);
             foreach ($use_replacements as $use_replacement) {
               if (str_replace($use_replacement['full'], $use_replacement['short'], $remove_line) === $add_line) {
-                // The add an remove lines are exactly the same except or the FQCN.
+                // The add and remove lines are exactly the same except for the
+                // FQCN replacement. Skip this line and the next line
                 $skip_lines[] = $index + 1;
                 continue 2;
               }
